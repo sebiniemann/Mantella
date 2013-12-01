7fe527061fe6e8392a7f54d0651ae16321913cf5
SebastianNiemann <niemann@sra.uni-hannover.de>
2013-12-01 11:13:27 +0100

    Added AllTests.java

---
+++ Java/test/src/AllTests.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright 2013 Sebastian Niemann <niemann@sra.uni-hannover.de> and contributors.
+ * 
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://opensource.org/licenses/MIT
+ *******************************************************************************/
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Sebastian Niemann <niemann@sra.uni-hannovr.de>
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+})
+public class AllTests {
+
+}
