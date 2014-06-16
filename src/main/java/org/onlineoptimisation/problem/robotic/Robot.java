3ee423ac8830814f77fea2b058dc38f7cf92faf5
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-06-16 13:32:47 +0200

    Cleanup

--- src/main/java/org/onlineoptimisation/problem/robotic/Robot.java
+++
@@ -1,7 +0,0 @@
-package org.onlineoptimisation.problem.robotic;
-
-import org.armadillojava.Mat;
-
-public abstract class Robot {
-  public abstract Mat getDirectJacobian(Mat parameter, Mat trajectoryPoint, boolean homogeneous);
-}
