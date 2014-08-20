9b45e27b169637f5ceb43674a3d7ed7e94beb57b
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-08-20 18:41:59 +0200

    fix: Fixed errors in locating GNU Readline
    Signed-off-by: Sebastian Niemann <niemann@sra.uni-hannover.de>

--- ext/cmake/FindReadline.cmake
+++ ext/cmake/FindReadline.cmake
@@ -1,39 +1,39 @@
-find_path(Readline_INCLUDE_DIR readline/readline.h)
-find_library(Readline_LIBRARY NAMES readline) 
+find_path(READLINE_INCLUDE_DIRS readline/readline.h)
+find_library(READLINE_LIBRARIES NAMES readline)
 
-if (Readline_INCLUDE_DIR AND Readline_LIBRARY)
-  set (Readline_FOUND TRUE)
+if (READLINE_INCLUDE_DIRS AND READLINE_LIBRARIES)
+  set (READLINE_FOUND TRUE)
 endif ()
 
-if (Readline_INCLUDE_DIR AND EXISTS "${Readline_INCLUDE_DIR}/readline/readline.h")
-  file(STRINGS "${Readline_INCLUDE_DIR}/readline/readline.h" 
-               Readline_MAJOR_VERSION 
+if (READLINE_INCLUDE_DIRS AND EXISTS "${READLINE_INCLUDE_DIRS}/readline/readline.h")
+  file(STRINGS "${READLINE_INCLUDE_DIRS}/readline/readline.h"
+               READLINE_MAJOR_VERSION
        REGEX "^#define RL_VERSION_MAJOR.*[0-9]+$")
-  file(STRINGS "${Readline_INCLUDE_DIR}/readline/readline.h" 
-               Readline_MINOR_VERSION 
+  file(STRINGS "${READLINE_INCLUDE_DIRS}/readline/readline.h"
+               READLINE_MINOR_VERSION
        REGEX "^#define RL_VERSION_MINOR.*[0-9]+$")
 
   string(REGEX REPLACE "^.*RL_VERSION_MAJOR.*([0-9]+).*$" 
                        "\\1" 
-                       Readline_MAJOR_VERSION 
-                       "${Readline_MAJOR_VERSION}")
+                       READLINE_MAJOR_VERSION
+                       "${READLINE_MAJOR_VERSION}")
   string(REGEX REPLACE "^.*RL_VERSION_MINOR.*([0-9]+).*$" 
                        "\\1" 
-                       Readline_MINOR_VERSION 
-                       "${Readline_MINOR_VERSION}")
+                       READLINE_MINOR_VERSION
+                       "${READLINE_MINOR_VERSION}")
 
-  set(Readline_VERSION "${Readline_MAJOR_VERSION}.${Readline_MINOR_VERSION}")
+  set(READLINE_VERSION "${READLINE_MAJOR_VERSION}.${READLINE_MINOR_VERSION}")
 endif ()
 
-if (Readline_FOUND)
-  if (Readline_FIND_VERSION VERSION_GREATER Readline_VERSION)
-    message(FATAL_ERROR "Found version ${Readline_VERSION} of GNU Readline at ${Readline_LIBRARY}, but version ${Readline_FIND_VERSION} required")
+if (READLINE_FOUND)
+  if (READLINE_FIND_VERSION VERSION_GREATER READLINE_VERSION)
+    message(FATAL_ERROR "Found version ${READLINE_VERSION} of GNU Readline at ${READLINE_LIBRARIES}, but version ${READLINE_FIND_VERSION} required")
   endif ()
-  if (NOT Readline_FIND_QUIETLY)
-    message(STATUS "Found GNU Readline: ${Readline_LIBRARY} (found version \"${Readline_VERSION}\")")
+  if (NOT READLINE_FIND_QUIETLY)
+    message(STATUS "Found GNU Readline: ${READLINE_LIBRARIES} (found version \"${READLINE_VERSION}\")")
   endif ()
 else ()
-  if (Readline_FIND_REQUIRED)
+  if (READLINE_FIND_REQUIRED)
     message(FATAL_ERROR "Could not find GNU Readline")
   endif ()
 endif ()
