796586032a93b6b895ed5ee1e635561a3a39e720
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-08-27 23:39:00 +0200

    devel: Removed "using ..." notation and added namespaces directly
    Signed-off-by: Sebastian Niemann <niemann@sra.uni-hannover.de>

--- include/helper/random.hpp
+++ include/helper/random.hpp
@@ -1,15 +1,13 @@
 #pragma once
 
 #include <random>
-using std::random_device;
-using std::mt19937_64;
 
 namespace hop {
   class Random {
     public:
-      static mt19937_64 RNG;
+      static std::mt19937_64 RNG;
 
     protected:
-      static random_device RANDOM_DEVIDE;
+      static std::random_device RANDOM_DEVIDE;
   };
 }
