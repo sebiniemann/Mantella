d386d0f4cc3bde249d27b5dd9aab34ce3a4acbc4
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-08-28 22:26:14 +0200

    devel: Moved enum types to the GUI

--- cli/main.cpp
+++ cli/main.cpp
@@ -15,20 +15,20 @@ int main (const int argc, const char* argv[]) {
 
   }
 
-//  std::shared_ptr<Experiment> plot2DFitnessFunction = std::shared_ptr<Experiment>(new Plot2DFitnessFunction(std::shared_ptr<hop::OptimisationProblem>(new hop::SphereFunction(2))));
+  std::shared_ptr<Experiment> plot2DFitnessFunction = std::shared_ptr<Experiment>(new Plot2DFitnessFunction(std::shared_ptr<hop::OptimisationProblem>(new hop::SphereFunction(2))));
 
-//  std::ofstream output("output"); {
-//    cereal::JSONOutputArchive archive(output);
-//    archive(cereal::make_nvp("hop", plot2DFitnessFunction));
-//  } output.close();
+  std::ofstream output("output"); {
+    cereal::JSONOutputArchive archive(output);
+    archive(cereal::make_nvp("experiement", plot2DFitnessFunction));
+  } output.close();
 
-//  plot2DFitnessFunction->run();
-//  plot2DFitnessFunction->waitUntilFinished();
+  plot2DFitnessFunction->run();
+  plot2DFitnessFunction->waitUntilFinished();
 
   std::shared_ptr<Experiment> experiment;
   std::ifstream input(argv[1]); {
     cereal::JSONInputArchive archive(input);
-    archive(cereal::make_nvp("hop", experiment));
+    archive(cereal::make_nvp("experiement", experiment));
   } input.close();
 
   experiment->run();
