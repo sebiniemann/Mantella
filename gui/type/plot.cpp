a96d5bb7d41a2b5a922f3e35db5b4ff06af6ab80
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-10-03 13:37:03 +0200

    devel: Removed GUI feature
    We discussed at the last sprint planning, that for now no real benefit
    is gained by an additional GUI – besides the existing apps.

--- gui/type/plot.cpp
+++
@@ -1,9 +0,0 @@
-#include <plot.hpp>
-
-decltype(Plot::_names) Plot::_names = {
-  {Plot::Type::FitnessFunction, "Fitness function"}
-};
-
-std::string Plot::getName(const Type& type) const {
-  return _names.at(type);
-}
