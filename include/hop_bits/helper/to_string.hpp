50e03ae659ae7f811dfbeea3cf297230521d4861
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-10-03 13:39:37 +0200

    devel: Renamed to_string.* to string.*

--- include/hop_bits/helper/to_string.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <memory>
-
-#include <hop_bits/optimisationProblem.hpp>
-
-namespace hop {
-  std::string to_string(const std::shared_ptr<OptimisationProblem> optimisationProblem);
-}
--- src/helper/to_string.cpp
+++
@@ -1,7 +0,0 @@
-#include <hop_bits/helper/to_string.hpp>
-
-namespace hop {
-  std::string to_string(const std::shared_ptr<OptimisationProblem> optimisationProblem) {
-    return optimisationProblem->to_string();
-  }
-}
