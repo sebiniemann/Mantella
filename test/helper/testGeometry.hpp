d206333cb56a1608013e7234a188daf69801e48e
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-10-18 23:27:14 +0200

    devel: Added namespaces

--- test/helper/testGeometry.hpp
+++ test/helper/testGeometry.hpp
@@ -1,6 +1,7 @@
 #include <catch/catch.hpp>
 
 #include <cstdlib>
+#include <cmath>
 
 #include <armadillo>
 
@@ -12,21 +13,21 @@ TEST_CASE("Geometry helper", "[geometry]") {
 
   SECTION("Test impementation correctness") {
     arma::Mat<double>::fixed<2, 2> firstCenter({
-      -2, 3,
+      -2.0, 3.0,
       1.8, -2.5
     });
     arma::Row<double>::fixed<2> firstRadius({
       1.3,
-      4
+      4.0
     });
 
     arma::Mat<double>::fixed<2, 2> secondCenter({
       -1.5, 2.4,
-      -3, 2
+      -3.0, 2.0
     });
     arma::Row<double>::fixed<2> secondRadius({
       1.2,
-      3
+      3.0
     });
 
     arma::Mat<double>::fixed<2, 2> expected({
@@ -42,17 +43,33 @@ TEST_CASE("Geometry helper", "[geometry]") {
 
   SECTION("Test rotation matrix impementation 2D") {
 
-    double angle[] = {0,45,90,135,180,225,270,315,360,-0,-45,-90,-180,-225,-315};
+    double angle[] = {0.0,
+      45.0,
+      90.0,
+      135.0,
+      180.0,
+      225.0, 
+      270.0,
+      315.0,
+      360.0,
+      -0.0,
+      -45.0,
+      -90.0,
+      -180.0,
+      -225.0,
+      -315.0
+    };
 
     for (std::size_t n = 0; n != sizeof(angle); ++n){
         arma::Mat<double> result = hop::Geometry::get2DRotationMatrix(angle[n]);
 
         arma::Mat<double>::fixed<2, 2> expected({
-          cos(angle[n]), -sin(angle[n]),
-          sin(angle[n]), cos(angle[n])
+          std::cos(angle[n]), -std::sin(angle[n]),
+          std::sin(angle[n]), std::cos(angle[n])
         });
 
         for (std::size_t i = 0; i < expected.n_elem; ++i) {
+          std::cout << result.at(i) - expected.at(i) << std::endl;
           CHECK(result.at(i) == Approx(expected.at(i)));
         }
 
@@ -69,21 +86,21 @@ TEST_CASE("Geometry helper", "[geometry]") {
         arma::Mat<double> result = hop::Geometry::get3DRotationMatrix(rollAngle[n],pitchAngle[n],yawAngle[n]);
 
         arma::Mat<double>::fixed<3, 3> expectedRoll({
-          1,0,0,
-          0,cos(rollAngle[n]), -sin(rollAngle[n]),
-          0,sin(rollAngle[n]), cos(rollAngle[n]),
+          1, 0, 0,
+          0, std::cos(rollAngle[n]), -std::sin(rollAngle[n]),
+          0, std::sin(rollAngle[n]), std::cos(rollAngle[n]),
         });
 
         arma::Mat<double>::fixed<3, 3> expectedPitch({
-          cos(pitchAngle[n]),0, sin(pitchAngle[n]),
-          0,1,0,
-          -sin(pitchAngle[n]),0, cos(pitchAngle[n])
+          std::cos(pitchAngle[n]), 0, std::sin(pitchAngle[n]),
+          0, 1, 0,
+          -std::sin(pitchAngle[n]), 0, std::cos(pitchAngle[n])
         });
 
         arma::Mat<double>::fixed<3, 3> expectedYaw({
-          cos(yawAngle[n]), -sin(yawAngle[n]),0,
-          sin(yawAngle[n]), cos(yawAngle[n]),0,
-          0,0,1
+          std::cos(yawAngle[n]), -std::sin(yawAngle[n]), 0,
+          std::sin(yawAngle[n]), std::cos(yawAngle[n]), 0,
+          0, 0, 1
         });
         //arma::Mat<double>::fixed<3, 3> expected = expectedYaw*expectedPitch*expectedRoll;
         arma::Mat<double>::fixed<3, 3> expected = expectedRoll*expectedPitch*expectedYaw;
