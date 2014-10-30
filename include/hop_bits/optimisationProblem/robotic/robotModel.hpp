8fba16baaa0aa9e839cbb9ec5db81d99bc52a795
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-10-30 08:51:47 +0100

    feat: Added robot model base class

--- include/hop_bits/optimisationProblem/robotic/robotModel.hpp
+++ include/hop_bits/optimisationProblem/robotic/robotModel.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
+// C++ STL
+#include <vector>
+
+// Armadillo
 #include <armadillo>
 
 namespace hop {
   class RobotModel {
     public:
-      virtual arma::Mat<double> getJacobian(const arma::Col<double>& endEffectorPose, const arma::Col<double>& redundantActuationParameters) const = 0;
+
+      virtual std::vector<arma::Mat<double>> getModelCharacterisation(
+          const arma::Col<double>& endEffectorPose,
+          const arma::Mat<double>& redundantJointActuations) const = 0;
+
+      virtual arma::Mat<double> getActuation(
+          const arma::Col<double>& endEffectorPose,
+          const arma::Mat<double>& redundantJointActuations) const = 0;
+
+      virtual double getPositionError(
+          const arma::Col<double>& endEffectorPose,
+          const arma::Mat<double>& redundantJointActuations) const = 0;
   };
 }
