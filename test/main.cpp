8e114c2261a2186f9c4a16ec5378c071a5c97da5
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-10-30 08:53:26 +0100

    devel: Added exceptions to commandline arguments

--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,31 @@
+// Catch
 #define CATCH_CONFIG_RUNNER
 #include <catch/catch.hpp>
 
+// C++ STL
 #include <string>
+#include <stdexcept>
 
-std::string testDirectory;
+// Boost
+#include <boost/filesystem.hpp>
 
+boost::filesystem::path testDirectory;
+
+// Test cases
 #include <optimisationProblem/benchmark/testBlackBoxOptimisationBenchmark2013.hpp>
 #include <helper/testGeometry.hpp>
 #include <helper/testString.hpp>
 
 int main(const int argc, const char* argv[]) {
-  if(argc < 2) {
-    // TODO Add exception
+  if(argc != 2) {
+    throw std::invalid_argument("The number of arguments (" + std::to_string(argc) + ") provided must be exactly 2.");
   }
 
-  testDirectory = argv[1];
+  testDirectory = boost::filesystem::path(argv[1]);
+
+  if(!boost::filesystem::exists(testDirectory)) {
+    throw std::invalid_argument("The speficied test data directory (" + testDirectory.string() + ") does not exists.");
+  }
 
   return Catch::Session().run();
 }
