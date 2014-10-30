0a1aaea7ed64e13edf75e997e8fc9f954cfb2545
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-10-30 08:46:50 +0100

    devel: Added comments to some helper classes

--- include/hop_bits/helper/cereal.hpp
+++ include/hop_bits/helper/cereal.hpp
@@ -1,25 +1,33 @@
 #pragma once
 
+// C++ STL
 #include <cstdlib>
 #include <vector>
 
+// Armadillo
 #include <armadillo>
 
+// Cereal
 #include <cereal/cereal.hpp>
 #include <cereal/types/vector.hpp>
 
-#include <iostream>
-
 namespace cereal {
+  // Helper template to serialise matrices.
   template <class Archive, class Type>
-  void save(Archive& archive, const arma::Mat<Type>& matrix) {
+  void save(
+      // Archive to be filled.
+      Archive& archive,
+      // Matrix to be serialised.
+      const arma::Mat<Type>& matrix) {
     std::size_t n_rows = matrix.n_rows;
     std::size_t n_cols = matrix.n_cols;
     std::size_t n_elem = matrix.n_elem;
 
+    // Save the shape ...
     archive(cereal::make_nvp("numberOfRows", n_rows));
     archive(cereal::make_nvp("numberOfCols", n_cols));
 
+    // ... and the values of the matrix.
     std::vector<Type> data(n_elem);
     for(size_t n = 0; n < n_elem; ++n) {
       data.at(n) = matrix.at(n);
@@ -27,28 +35,46 @@ namespace cereal {
     archive(cereal::make_nvp("values", data));
   }
 
+  // Helper template to deserialise matrices
   template <class Archive, class Type>
-  void load(Archive& archive, arma::Mat<Type>& matrix) {
+  void load(
+      // Archive containing the serialised matrix.
+      Archive& archive,
+      // Output location
+      arma::Mat<Type>& matrix) {
     std::size_t n_rows;
     std::size_t n_cols;
 
+    // Extract the shape ...
     archive(cereal::make_nvp("numberOfRows", n_rows));
     archive(cereal::make_nvp("numberOfCols", n_cols));
 
+    // ... and the values ...
     std::vector<Type> data(n_rows * n_cols);
     archive(cereal::make_nvp("values", data));
 
+    // ... in order to fill a temporary matrix ...
     arma::Mat<Type> temp(data);
+    // ... and reshae it.
     temp.reshape(n_rows, n_cols);
+
+    // Replace the temporary matrix with the provided storage location.
     matrix.swap(temp);
   }
 
+  // Helper template to serialise column vectors.
   template <class Archive, class Type>
-  void save(Archive& archive, const arma::Col<Type>& column) {
+  void save(
+      // Archive to be filled.
+      Archive& archive,
+      // Column vector to be serialised.
+      const arma::Col<Type>& column) {
     std::size_t n_elem = column.n_elem;
 
+    // Save the leght ...
     archive(cereal::make_nvp("numberOfElements", n_elem));
 
+    // ... and the values of the column vector.
     std::vector<Type> data(n_elem);
     for(size_t n = 0; n < n_elem; ++n) {
       data.at(n) = column.at(n);
@@ -56,42 +82,69 @@ namespace cereal {
     archive(cereal::make_nvp("values", data));
   }
 
+  // Helper template to deserialise column vectors.
   template <class Archive, class Type>
-  void load(Archive& archive, arma::Col<Type>& column) {
+  void load(
+      // Archive containing the serialised column vector.
+      Archive& archive,
+      // Output location
+      arma::Col<Type>& column) {
     std::size_t n_elem;
 
+    // Extract the length ...
     archive(cereal::make_nvp("numberOfElements", n_elem));
 
+    // ... and the values ...
     std::vector<Type> data(n_elem);
     archive(cereal::make_nvp("values", data));
 
+    // ... in order to fill a temporary column vector.
     arma::Col<Type> temp(data);
+
+    // Replace the temporary column vector with the provided storage location.
     column.swap(temp);
   }
 
+  // Helper template to serialise row vectors.
   template <class Archive, class Type>
-  void save(Archive& archive, const arma::Row<Type>& row) {
+  void save(
+      // Archive to be filled.
+      Archive& archive,
+      // Row vector to be serialised.
+      const arma::Row<Type>& row) {
     std::size_t n_elem = row.n_elem;
 
+    // Save the length ...
     archive(cereal::make_nvp("numberOfElements", n_elem));
 
+    // ... and the values of the row vector.
     std::vector<Type> data(n_elem);
     for(size_t n = 0; n < n_elem; ++n) {
       data.at(n) = row.at(n);
     }
-    archive(cereal::make_nvp("values", data));
+   archive(cereal::make_nvp("values", data));
   }
 
+  // Helper template to deserialise row vectors.
   template <class Archive, class Type>
-  void load(Archive& archive, arma::Row<Type>& row) {
+  void load(
+      // Archive containing the serialised row vector.
+      Archive& archive,
+      // Output location
+      arma::Row<Type>& row) {
     std::size_t n_elem;
 
+    // Extract the length ...
     archive(cereal::make_nvp("numberOfElements", n_elem));
 
+    // ... and the values ...
     std::vector<Type> data(n_elem);
     archive(cereal::make_nvp("values", data));
 
+    // ... in order to fill a temporary row vector.
     arma::Row<Type> temp(data);
+
+    // Replace the temporary row vector with the provided storage location.
     row.swap(temp);
   }
 }
--- include/hop_bits/helper/printable.hpp
+++ include/hop_bits/helper/printable.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+// C++ STL
 #include <string>
 
 namespace hop {
+  // Single function helper class to require/provide a string reresentation.
   class Printable {
     public:
       virtual std::string to_string() const = 0;
