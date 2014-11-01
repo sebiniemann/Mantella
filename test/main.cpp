414d4ae1f1635d879caa4694ecd66e461383bea3
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-02 00:24:50 +0100

    fix: Uncaught exception

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,7 @@
 // C++ STL
 #include <string>
 #include <stdexcept>
+#include <iostream>
 
 // Boost
 #include <boost/filesystem.hpp>
@@ -15,17 +16,21 @@
 boost::filesystem::path testDirectory;
 
 int main(const int argc, const char* argv[]) {
-  if(argc != 2) {
-    throw std::invalid_argument("The number of arguments (" + std::to_string(argc) + ") provided must be exactly 2.");
+  try {
+    if(argc != 2) {
+      throw std::invalid_argument("The number of arguments (" + std::to_string(argc) + ") provided must be exactly 2.");
+    }
+
+    testDirectory = boost::filesystem::path(argv[1]);
+
+    if(!boost::filesystem::exists(testDirectory)) {
+      throw std::invalid_argument("The speficied test data directory (" + testDirectory.string() + ") does not exists.");
+    }
+
+    hop::Random::setSeed(1234567890);
+
+    return Catch::Session().run();
+  } catch(std::exception exception) {
+    std::cout << exception.what() << std::cout;
   }
-
-  testDirectory = boost::filesystem::path(argv[1]);
-
-  if(!boost::filesystem::exists(testDirectory)) {
-    throw std::invalid_argument("The speficied test data directory (" + testDirectory.string() + ") does not exists.");
-  }
-
-  hop::Random::setSeed(1234567890);
-
-  return Catch::Session().run();
 }
