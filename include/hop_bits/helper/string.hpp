6fe5bf4fe211d0709086c83fec2f7fdd2f438d85
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-06 22:09:03 +0100

    feat: Added class String
    to_string remains out of any class to be similar to std::to_string

--- include/hop_bits/helper/string.hpp
+++ include/hop_bits/helper/string.hpp
@@ -8,6 +8,8 @@
 
 namespace hop {
   // Returns the string representation of a class marked as printable;
+  // The underscore within the name is intended, in order to be constistent with Standard Library's
+  // std::to_string
   std::string to_string(
       // The class marked as printable.
       const Printable& printable);
@@ -15,11 +17,14 @@ namespace hop {
       // A shared pointer to the class marked as printable.
       const std::shared_ptr<Printable> printable);
 
-  // Checks whether the provided text ends with another string. This is usually used in this library
-  // to check/filter for file endings.
-  bool endsWith(
-      // The text to be checked.
-      const std::string& text,
-      // The suffix searched for.
-      const std::string& ending);
+  class String {
+    public:
+      // Checks whether the provided text ends with another string. This is usually used in this library
+      // to check/filter for file endings.
+      static bool endsWith(
+          // The text to be checked.
+          const std::string& text,
+          // The suffix searched for.
+          const std::string& ending);
+  }
 }
