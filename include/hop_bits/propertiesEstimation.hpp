838501ab546843c7c704ff61e8cefb87ba46599f
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-06 13:09:17 +0100

    feat: Added properties estimation

---
+++ include/hop_bits/propertiesEstimation.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+// C++ Standard Library
+#include <memory>
+#include <vector>
+
+// Armadillo
+#include <armadillo>
+
+// HOP
+#include <hop_bits/propertiesAnalysis.hpp>
+#include <hop_bits/optimisationProblem.hpp>
+
+namespace hop {
+  class PropertiesEstimation {
+    public:
+      explicit PropertiesEstimation();
+
+      // Copy constructors are not used in this library and deleted to avoid unintended/any usage.
+      PropertiesEstimation(const PropertiesEstimation&) = delete;
+      PropertiesEstimation& operator=(const PropertiesEstimation&) = delete;
+
+      virtual void estimate(
+          const std::shared_ptr<OptimisationProblem>& optimisationProblem) const = 0;
+
+      void addPropertiesAnalysis(
+          const std::shared_ptr<PropertiesAnalysis> propertiesAnalysis);
+
+      std::size_t getNumberOfPropertiesSets() const;
+
+      bool isLinear(
+          const std::size_t& propertiesSetIndex) const;
+      arma::Col<double> getLinearEstimator(
+          const std::size_t& propertiesSetIndex) const;
+
+      bool isQuadratic(
+          const std::size_t& propertiesSetIndex) const;
+      arma::Col<double> getQuadraticEstimator(
+          const std::size_t& propertiesSetIndex) const;
+
+      bool isLipschitzContinuous(
+          const std::size_t& propertiesSetIndex) const;
+      double getLipschitzConstant(
+          const std::size_t& propertiesSetIndex) const;
+
+      bool isAlphaHoelderContinuous(
+          const std::size_t& propertiesSetIndex) const;
+      double getAlphaHoelderConstant(
+          const std::size_t& propertiesSetIndex) const;
+
+      double getBelief(
+          const std::size_t& propertiesSetIndex) const;
+      double getPlausibility(
+          const std::size_t& propertiesSetIndex) const;
+    protected:
+      std::vector<std::shared_ptr<PropertiesAnalysis>> propertiesAnalyses_;
+
+      std::size_t numberOfPropertiesSets_;
+
+      std::vector<bool> isLinear_;
+      std::vector<arma::Col<double>> linearEstimator_;
+
+      std::vector<bool> isQuadratic_;
+      std::vector<arma::Col<double>> quadraticEstimator_;
+
+      std::vector<bool> isLipschitzContinuous_;
+      std::vector<double> lipschitzConstant_;
+
+      std::vector<bool> isAlphaHoelderContinuous_;
+      std::vector<double> alphaHoelderConstant_;
+  };
+}
---
+++ src/propertiesEstimation.cpp
@@ -0,0 +1,60 @@
+#include <hop_bits/propertiesEstimation.hpp>
+
+namespace hop {
+  PropertiesEstimation::PropertiesEstimation()
+    : isLinear_(false),
+      isQuadratic_(false),
+      isLipschitzContinuous_(false),
+      isAlphaHoelderContinuous_(false),
+      numberOfPropertiesSets_(0) {
+
+  }
+
+  void PropertiesEstimation::addPropertiesAnalysis(const std::shared_ptr<PropertiesAnalysis> propertiesAnalysis) {
+    propertiesAnalyses_.push_back(propertiesAnalysis);
+  }
+
+  std::size_t PropertiesEstimation::getNumberOfPropertiesSets() const {
+    return numberOfPropertiesSets_;
+  }
+
+  bool PropertiesEstimation::isLinear(
+      const std::size_t& propertiesSetIndex) const {
+    return isLinear_.at(propertiesSetIndex);
+  }
+
+  arma::Col<double> PropertiesEstimation::getLinearEstimator(
+      const std::size_t& propertiesSetIndex) const {
+    return linearEstimator_.at(propertiesSetIndex);
+  }
+
+  bool PropertiesEstimation::isQuadratic(
+      const std::size_t& propertiesSetIndex) const{
+    return isQuadratic_.at(propertiesSetIndex);
+  }
+
+  arma::Col<double> PropertiesEstimation::getQuadraticEstimator(
+      const std::size_t& propertiesSetIndex) const{
+    return quadraticEstimator_.at(propertiesSetIndex);
+  }
+
+  bool PropertiesEstimation::isLipschitzContinuous(
+      const std::size_t& propertiesSetIndex) const {
+    return isLipschitzContinuous_.at(propertiesSetIndex);
+  }
+
+  double PropertiesEstimation::getLipschitzConstant(
+      const std::size_t& propertiesSetIndex) const {
+    return lipschitzConstant_.at(propertiesSetIndex);
+  }
+
+  bool PropertiesEstimation::isAlphaHoelderContinuous(
+      const std::size_t& propertiesSetIndex) const {
+    return isAlphaHoelderContinuous_.at(propertiesSetIndex);
+  }
+
+  double PropertiesEstimation::getAlphaHoelderConstant(
+      const std::size_t& propertiesSetIndex) const {
+    return alphaHoelderConstant_.at(propertiesSetIndex);
+  }
+}
