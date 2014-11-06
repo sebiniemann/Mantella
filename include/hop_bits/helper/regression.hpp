539cd466512aec3769203edb632b361f643506f0
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-06 13:10:17 +0100

    feat: Added regression helper

--- include/hop_bits/helper/regression.hpp
+++ include/hop_bits/helper/regression.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+// C++ Standard Library
+#include <unordered_map>
+
 // Armadillo
 #include <armadillo>
+
+// HOP
+#include <hop_bits/helper/cache.hpp>
 
 namespace hop {
   class Regression {
     public:
-      arma::Col<double> getEstimator(
+      static arma::Col<double> getEstimator(
           const arma::Mat<double>& regressors,
           const arma::Col<double>& regressand);
   };
