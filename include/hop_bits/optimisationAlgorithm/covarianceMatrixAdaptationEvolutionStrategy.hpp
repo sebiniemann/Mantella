e2d147f974721cf37dcf87ee8812169516015787
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-13 20:27:30 +0100

    devel: Simple style changes

--- include/hop_bits/optimisationAlgorithm/covarianceMatrixAdaptationEvolutionStrategy.hpp
+++ include/hop_bits/optimisationAlgorithm/covarianceMatrixAdaptationEvolutionStrategy.hpp
@@ -6,7 +6,8 @@ namespace hop {
   class CovarianceMatrixAdaptationEvolutionStrategy : public OptimisationAlgorithm {
     public:
       //TODO: which input parameters should be added to the constructor?
-      explicit CovarianceMatrixAdaptationEvolutionStrategy(const std::shared_ptr<OptimisationProblem> optimisationProblem);
+      explicit CovarianceMatrixAdaptationEvolutionStrategy(
+      	const std::shared_ptr<OptimisationProblem> optimisationProblem);
 
       CovarianceMatrixAdaptationEvolutionStrategy(const CovarianceMatrixAdaptationEvolutionStrategy&) = delete;
       CovarianceMatrixAdaptationEvolutionStrategy& operator=(const CovarianceMatrixAdaptationEvolutionStrategy&) = delete;
@@ -15,34 +16,37 @@ namespace hop {
 
     protected:
       void optimiseImplementation() override;
-      //input parameters
-      int numberOfDimensions_; //N
-      arma::Col<double> objectiveValues_; //xmean
-	  double sigma_;
-      double stopValue_; //stopfitness
+      // input parameters
+      // N
+      unsigned int numberOfDimensions_; 
+      // xmean
+      arma::Col<double> objectiveValues_;
+      double sigma_;
+      // stopfitness
+      double stopValue_; 
 	  
-      //selection parameters
+      // selection parameters
       int lambda_;
-	  double mu_;
+      double mu_;
       arma::Col<double> weights_;
       double mueff_;
 	  
-      //adaptation parameters
-	  double cc_;
-	  double cs_;
-	  double c1_;
-	  double cmu_;
-	  double damps_;
+      // adaptation parameters
+      double cc_;
+      double cs_;
+      double c1_;
+      double cmu_;
+      double damps_;
 	  
-      //dynamic parameters and constants
-	  arma::Col<double> pc_;
-	  arma::Col<double> ps_;
-	  arma::Mat<double> B_;
+      // dynamic parameters and constants
+      arma::Col<double> pc_;
+      arma::Col<double> ps_;
+      arma::Mat<double> B_;
       arma::Mat<double> D_;
-	  arma::Mat<double> C_;
-	  arma::Mat<double> invsqrtC_;
-	  double eigeneval_;
-	  double chiN_;
+      arma::Mat<double> C_;
+      arma::Mat<double> invsqrtC_;
+      double eigeneval_;
+      double chiN_;
 	  
       //function to use
       double frosenbrock(arma::Col<double> x);
