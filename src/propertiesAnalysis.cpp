8ed6eee7ad562383df8b867e38c006ad8019ea36
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-14 14:18:35 +0100

    devel: Simplified properties analysis

--- src/propertiesAnalysis.cpp
+++ src/propertiesAnalysis.cpp
@@ -4,27 +4,16 @@
 #include <cstdlib>
 
 namespace hop {
-  void PropertiesAnalysis::analyse(
-      const std::shared_ptr<OptimisationProblem> optimisationProblem) {
-    arma::Mat<double> parameters(optimisationProblem->getCachedObjectiveValues().size(), optimisationProblem->getNumberOfDimensions());
-    arma::Col<double> objectiveValues(optimisationProblem->getNumberOfDimensions());
+  PropertiesAnalysis::PropertiesAnalysis()
+    : plausibility_(0) {
 
-    std::size_t n = 0;
-    for (const auto& entry : optimisationProblem->getCachedObjectiveValues()) {
-      parameters.col(n) = entry.first;
-      objectiveValues.at(n) = entry.second;
-
-      ++n;
-    }
-
-    belief_ = arma::datum::nan;
-    plausibility_ = arma::datum::nan;
-
-    analyseImplementation(parameters, objectiveValues);
   }
 
-  double PropertiesAnalysis::getBelief() const {
-    return belief_;
+  void PropertiesAnalysis::analyse(
+      const std::shared_ptr<OptimisationProblem> optimisationProblem) {
+    plausibility_ = 0;
+
+    analyseImplementation(optimisationProblem);
   }
 
   double PropertiesAnalysis::getPlausibility() const {
