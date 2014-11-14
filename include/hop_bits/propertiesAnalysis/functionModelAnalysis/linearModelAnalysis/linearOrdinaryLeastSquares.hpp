6a9ac9ee8fd8520e150a7168713114115f4ac0d1
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-14 14:15:00 +0100

    devel: Moved function model analyses into the subfolder functionModelAnalysis/

---
+++ include/hop_bits/propertiesAnalysis/functionModelAnalysis/linearModelAnalysis/linearOrdinaryLeastSquares.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// HOP
+#include <hop_bits/propertiesAnalysis/functionModelAnalysis/linearModelAnalysis.hpp>
+
+namespace hop {
+  class LinearOrdinaryLeastSquares : public LinearModelAnalysis {
+    protected:
+      void analyseImplementation(
+          const std::shared_ptr<OptimisationProblem> optimisationProblem) override;
+  };
+}
---
+++ src/propertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis/quadraticOrdinaryLeastSquares.cpp
@@ -0,0 +1,36 @@
+#include <hop_bits/propertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis/quadraticOrdinaryLeastSquares.hpp>
+
+// HOP
+#include <hop_bits/helper/regression.hpp>
+
+namespace hop {
+  void QuadraticOrdinaryLeastSquares::analyseImplementation(
+      const std::shared_ptr<OptimisationProblem> optimisationProblem) {
+    std::unordered_map<arma::Col<double>, double, CacheHasher, CacheKeyEqual> parameterToObjectiveValueMappings = optimisationProblem->getCachedObjectiveValues();
+
+    arma::Mat<double> parameters(optimisationProblem->getNumberOfDimensions() * (optimisationProblem->getNumberOfDimensions() + 3) / 2 + 1, parameterToObjectiveValueMappings.size());
+    arma::Col<double> objectiveValues(parameterToObjectiveValueMappings.size());
+
+    std::size_t n = 0;
+    for (const auto& parameterToObjectiveValueMapping : parameterToObjectiveValueMappings) {
+      arma::Col<double> parameter = parameterToObjectiveValueMapping.first;
+
+      parameters.submat(0, n, parameter.n_elem - 1, n) = parameter;
+      parameters.submat(parameter.n_elem, n, 2 * parameter.n_elem - 1,  n) = arma::square(parameter);
+
+      std::size_t k = 2 * parameter.n_elem;
+      for (std::size_t l = 0; l < parameter.n_elem; ++l) {
+        for (std::size_t m = l + 1; m < parameter.n_elem; ++m) {
+          parameters.at(k++, n) = parameter.at(l) * parameter.at(m);
+        }
+      }
+
+      objectiveValues.at(n) = parameterToObjectiveValueMapping.second;
+      ++n;
+    }
+    parameters.row(parameters.n_rows - 1).fill(1.0);
+
+    quadraticModelEstimator_ = getEstimator(parameters, objectiveValues);
+    residuals_ = objectiveValues - parameters.t() * quadraticModelEstimator_;
+  }
+}
