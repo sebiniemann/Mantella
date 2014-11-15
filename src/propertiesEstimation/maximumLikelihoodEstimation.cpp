8940525c8ff61fe0177d7a5fdd2364fdcfffc4a3
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-15 19:45:33 +0100

    devel: Removed cout (debug)

--- src/propertiesEstimation/maximumLikelihoodEstimation.cpp
+++ src/propertiesEstimation/maximumLikelihoodEstimation.cpp
@@ -1,19 +1,14 @@
 #include <hop_bits/propertiesEstimation/maximumLikelihoodEstimation.hpp>
 
-// C++ Standard Library
-#include <iostream>
-
 namespace hop {
   void MaximumLikelihoodEstimation::estimateImplementation(
       const std::shared_ptr<OptimisationProblem> optimisationProblem) {
 
      linearModelAnalysis_->analyse(optimisationProblem);
-     std::cout << linearModelAnalysis_->getResiduals() << std::endl;
      linearModelEstimators_.push_back(linearModelAnalysis_->getLinearModelEstimator());
      isLinear_.push_back(false);
 
      quadraticModelAnalysis_->analyse(optimisationProblem);
-     std::cout << quadraticModelAnalysis_->getResiduals() << std::endl;
      quadraticModelEstimators_.push_back(quadraticModelAnalysis_->getQuadraticModelEstimator());
      isQuadratic_.push_back(false);
 
