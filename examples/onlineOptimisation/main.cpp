279f00d629491fe728de19377130e3cf18559388
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-18 13:13:48 +0100

    devel: Added soft constraints value output into the example

--- examples/onlineOptimisation/main.cpp
+++ examples/onlineOptimisation/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
 
   std::shared_ptr<hop::OptimisationProblem> optimisationProblem(new hop::bbob2013::LinearSlope(2));
 
-  hop::StandardParticleSwarmOptimisation2011 optimisationAlgorithm(optimisationProblem, 10);
+  hop::HookeJeevesAlgorithm optimisationAlgorithm(optimisationProblem);
   optimisationAlgorithm.optimise();
 
   std::cout << "isFinished: " << optimisationAlgorithm.isFinished() << std::endl;
@@ -22,6 +22,7 @@ int main(int argc, char* argv[]) {
   std::cout << "numberOfIterations: " << optimisationAlgorithm.getNumberOfIterations() << std::endl;
   std::cout << "numberOfEvaluations: " << optimisationProblem->getNumberOfEvaluations() << std::endl;
   std::cout << "numberOfDistinctEvaluations: " << optimisationProblem->getNumberOfDistinctEvaluations() << std::endl;
+  std::cout << "bestSoftConstraintValue: " << optimisationAlgorithm.getBestSoftConstraintValue() << std::endl;
   std::cout << "bestObjectiveValueDelta: " << optimisationAlgorithm.getBestObjectiveValue() - optimisationProblem->getAcceptableObjectiveValue() << std::endl;
   std::cout << "bestParameter: " << optimisationAlgorithm.getBestParameter() << std::endl;
 
@@ -30,16 +31,16 @@ int main(int argc, char* argv[]) {
 
   for(std::size_t n = 0; n < propertiesEstimation.getNumberOfPropertySets(); ++n) {
     std::cout << "isLinear: " << propertiesEstimation.isLinear(n) << std::endl;
-    std::cout << "getLinearModelEstimator: " << propertiesEstimation.getLinearModelEstimator(n) << std::endl;
+    std::cout << "linearModelEstimator: " << propertiesEstimation.getLinearModelEstimator(n) << std::endl;
     std::cout << "isQuaratic:" << propertiesEstimation.isQuadratic(n) << std::endl;
-    std::cout << "getQuadraticModelEstimator" << propertiesEstimation.getQuadraticModelEstimator(n) << std::endl;
+    std::cout << "quadraticModelEstimator" << propertiesEstimation.getQuadraticModelEstimator(n) << std::endl;
     std::cout << "isLipschitzContinuous: " << propertiesEstimation.isLipschitzContinuous(n) << std::endl;
-    std::cout << "getLipschitzConstant: " << propertiesEstimation.getLipschitzConstant(n) << std::endl;
+    std::cout << "lipschitzConstant: " << propertiesEstimation.getLipschitzConstant(n) << std::endl;
   }
 
   std::shared_ptr<hop::OptimisationProblem> optimisationProblem2(new hop::bbob2013::SphereFunction(2));
 
-  hop::StandardParticleSwarmOptimisation2011 optimisationAlgorithm2(optimisationProblem2, 10);
+  hop::HookeJeevesAlgorithm optimisationAlgorithm2(optimisationProblem2);
   optimisationAlgorithm2.optimise();
 
   std::cout << "isFinished: " << optimisationAlgorithm2.isFinished() << std::endl;
@@ -47,6 +48,7 @@ int main(int argc, char* argv[]) {
   std::cout << "numberOfIterations: " << optimisationAlgorithm2.getNumberOfIterations() << std::endl;
   std::cout << "numberOfEvaluations: " << optimisationProblem2->getNumberOfEvaluations() << std::endl;
   std::cout << "numberOfDistinctEvaluations: " << optimisationProblem2->getNumberOfDistinctEvaluations() << std::endl;
+  std::cout << "bestSoftConstraintValue: " << optimisationAlgorithm2.getBestSoftConstraintValue() << std::endl;
   std::cout << "bestObjectiveValueDelta: " << optimisationAlgorithm2.getBestObjectiveValue() - optimisationProblem->getAcceptableObjectiveValue() << std::endl;
   std::cout << "bestParameter: " << optimisationAlgorithm2.getBestParameter() << std::endl;
 
@@ -55,11 +57,11 @@ int main(int argc, char* argv[]) {
 
   for(std::size_t n = 0; n < propertiesEstimation.getNumberOfPropertySets(); ++n) {
     std::cout << "isLinear: " << propertiesEstimation.isLinear(n) << std::endl;
-    std::cout << "getLinearModelEstimator: " << propertiesEstimation.getLinearModelEstimator(n) << std::endl;
+    std::cout << "linearModelEstimator: " << propertiesEstimation.getLinearModelEstimator(n) << std::endl;
     std::cout << "isQuaratic:" << propertiesEstimation.isQuadratic(n) << std::endl;
-    std::cout << "getQuadraticModelEstimator" << propertiesEstimation.getQuadraticModelEstimator(n) << std::endl;
+    std::cout << "quadraticModelEstimator" << propertiesEstimation.getQuadraticModelEstimator(n) << std::endl;
     std::cout << "isLipschitzContinuous: " << propertiesEstimation.isLipschitzContinuous(n) << std::endl;
-    std::cout << "getLipschitzConstant: " << propertiesEstimation.getLipschitzConstant(n) << std::endl;
+    std::cout << "lipschitzConstant: " << propertiesEstimation.getLipschitzConstant(n) << std::endl;
   }
 
   return EXIT_SUCCESS;
