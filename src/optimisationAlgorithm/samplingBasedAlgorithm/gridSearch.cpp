e88922123cc97401a5d95f0228d7f59536fc7354
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-19 22:50:16 +0100

    devel: Added sampling factor parameterisation

--- src/optimisationAlgorithm/samplingBasedAlgorithm/gridSearch.cpp
+++ src/optimisationAlgorithm/samplingBasedAlgorithm/gridSearch.cpp
@@ -1,16 +1,19 @@
 #include <hop_bits/optimisationAlgorithm/samplingBasedAlgorithm/gridSearch.hpp>
 
+#include <iostream>
+
 namespace hop {
   GridSearch::GridSearch(
       const std::shared_ptr<OptimisationProblem> optimisationProblem)
     : SamplingBasedAlgorithm(optimisationProblem),
       candidateObjectiveValue_(std::numeric_limits<double>::infinity()),
       candidateSoftConstraintValue_(std::numeric_limits<double>::infinity()) {
-
-  }
+    setSamplingFactors(arma::ones(optimisationProblem_->getNumberOfDimensions()) / static_cast<double>(optimisationProblem_->getNumberOfDimensions()));
+}
 
   void GridSearch::optimiseImplementation() {
-    numberOfSamples_ = arma::conv_to<arma::Col<arma::uword>>::from(arma::ceil(maximalNumberOfIterations_ * (optimisationProblem_->getUpperBounds() - optimisationProblem_->getLowerBounds()) / arma::sum(optimisationProblem_->getUpperBounds() - optimisationProblem_->getLowerBounds())));
+    arma::Col<double> scaledSamplingFactors = samplingFactors_.at(0) / samplingFactors_;
+    numberOfSamples_ = arma::conv_to<arma::Col<arma::uword>>::from(scaledSamplingFactors * std::pow(maximalNumberOfIterations_ / arma::prod(scaledSamplingFactors), 1.0 / static_cast<double>(optimisationProblem_->getNumberOfDimensions())));
 
     sampleParameters_.clear();
     for (std::size_t n = 0; n < optimisationProblem_->getNumberOfDimensions(); ++n) {
@@ -21,6 +24,7 @@ namespace hop {
     bestObjectiveValue_ = std::numeric_limits<double>::infinity();
 
     sampleIndicies_ = arma::zeros<arma::Col<arma::uword>>(sampleParameters_.size());
+    candidateParameter_.set_size(optimisationProblem_->getNumberOfDimensions());
     do {
       ++numberOfIterations_;
 
@@ -31,11 +35,11 @@ namespace hop {
       ++sampleIndicies_.at(0);
       for(std::size_t n = 0; n < sampleIndicies_.n_elem - 1; ++n) {
         if(sampleIndicies_.at(n) >= numberOfSamples_.at(n)) {
+          sampleIndicies_.at(n) = 0;
            ++sampleIndicies_.at(n + 1);
         }
       }
 
-      candidateParameter_ = candidateParameter_;
       candidateSoftConstraintValue_ = optimisationProblem_->getSoftConstraintsValue(candidateParameter_);
       candidateObjectiveValue_ = optimisationProblem_->getObjectiveValue(candidateParameter_);
 
@@ -45,6 +49,17 @@ namespace hop {
         bestObjectiveValue_ = candidateObjectiveValue_;
       }
     } while(!isFinished() && !isTerminated());
+  }
+
+  void GridSearch::setSamplingFactors(
+      const arma::Col<double>& samplingFactors) {
+    if(samplingFactors.n_elem != optimisationProblem_->getNumberOfDimensions()) {
+
+    } else if(arma::sum(samplingFactors) != 1) {
+
+    }
+
+    samplingFactors_ = samplingFactors;
   }
 
   std::string GridSearch::to_string() const noexcept {
