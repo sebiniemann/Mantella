499a59374b3aadaf3b34b0f33a7a6feadaad8a24
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-21 17:49:51 +0100

    devel: Initialised lipschitz constant (no sweet cake for coverity_scan)

--- src/propertiesAnalysis/continuityAnalysis/lipschitzContinuityAnalysis.cpp
+++ src/propertiesAnalysis/continuityAnalysis/lipschitzContinuityAnalysis.cpp
@@ -1,6 +1,11 @@
 #include <hop_bits/propertiesAnalysis/continuityAnalysis/lipschitzContinuityAnalysis.hpp>
 
 namespace hop {
+  LipschitzContinuityAnalysis::LipschitzContinuityAnalysis()
+    : lipschitzConstant_(0) {
+
+  }
+
   double LipschitzContinuityAnalysis::getLipschitzConstant() const noexcept {
     return lipschitzConstant_;
   }
