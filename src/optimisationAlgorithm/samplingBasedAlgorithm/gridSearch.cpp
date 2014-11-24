6b4fa3f248669c5712cbfb4713f9e07472b52047
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-11-24 10:17:03 +0100

    fix: Coverted the sum of all sampling factor to string within the error message

--- src/optimisationAlgorithm/samplingBasedAlgorithm/gridSearch.cpp
+++ src/optimisationAlgorithm/samplingBasedAlgorithm/gridSearch.cpp
@@ -56,7 +56,7 @@ namespace hop {
     if(samplingFactors.n_elem != optimisationProblem_->getNumberOfDimensions()) {
       throw std::logic_error("The number of dimensions of the sampling factors (" + std::to_string(samplingFactors.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(optimisationProblem_->getNumberOfDimensions()) + ").");
     } else if(arma::sum(samplingFactors) != 1.0) {
-      throw std::logic_error("The sum of all sampling factors (" + arma::sum(samplingFactors) + ") must be 1.");
+      throw std::logic_error("The sum of all sampling factors (" + std::to_string(arma::sum(samplingFactors)) + ") must be 1.");
     }
 
     samplingFactors_ = samplingFactors;
