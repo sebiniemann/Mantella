134033b495f2728ceeb101e2d5cbaa0d1cb85c94
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-12-01 07:17:47 +0100

    devel: Simplified multiple divisions and set all immediate values explicitly to double precision

--- src/optimisationAlgorithm/populationBasedAlgorithm/covarianceMatrixAdaptationEvolutionStrategy.cpp
+++ src/optimisationAlgorithm/populationBasedAlgorithm/covarianceMatrixAdaptationEvolutionStrategy.cpp
@@ -13,17 +13,17 @@ namespace hop {
 
     //init selection parameters
     //unsigned int lambda_ = 4 + std::floor(3 * std::log(numberOfDimensions)); //overwritten by constructor value
-    unsigned int numberOfParents = populationSize_ / 2;
+    unsigned int numberOfParents = populationSize_ / 2.0;
 
-    arma::Col<double> weights = arma::normalise(std::log((populationSize_ + 1) / 2) - arma::log(arma::linspace(1, std::floor(numberOfParents), std::floor(numberOfParents))), 1);
+    arma::Col<double> weights = arma::normalise(std::log((populationSize_ + 1.0) / 2.0) - arma::log(arma::linspace(1, std::floor(numberOfParents), std::floor(numberOfParents))), 1);
 
-    double varianceEffectiveness = std::pow(arma::sum(weights), 2) / arma::sum(arma::square(weights));
+    double varianceEffectiveness = std::pow(arma::sum(weights), 2.0) / std::pow(arma::norm(weights), 2.0);
     //init adaptation parameters
-    double cc = (4 + varianceEffectiveness / numberOfDimensions) / (numberOfDimensions + 4 + 2 * varianceEffectiveness / numberOfDimensions); //time constant for cumulation for C
-    double cs = (varianceEffectiveness + 2) / (numberOfDimensions + varianceEffectiveness + 5); //t-const for cumulation for sigma control
-    double c1 = 2 / (std::pow(numberOfDimensions + 1.3, 2) + varianceEffectiveness); //learning rate for rank-one update of C
-    double cmu = std::min(1 - c1, 2 * (varianceEffectiveness - 2 + 1 / varianceEffectiveness) / (std::pow(numberOfDimensions + 2, 2) + varianceEffectiveness)); //and for rank-mu update
-    double damps = 1 + 2 * std::max(0.0, std::sqrt((varianceEffectiveness - 1) / (numberOfDimensions + 1)) - 1) + cs; //damping for sigma 
+    double cc = (4.0 + varianceEffectiveness) / (numberOfDimensions + 4.0 + 2.0 * varianceEffectiveness); //time constant for cumulation for C
+    double cs = (varianceEffectiveness + 2.0) / (numberOfDimensions + varianceEffectiveness + 5.0); //t-const for cumulation for sigma control
+    double c1 = 2.0 / (std::pow(numberOfDimensions + 1.3, 2.0) + varianceEffectiveness); //learning rate for rank-one update of C
+    double cmu = std::min(1 - c1, 2 * (varianceEffectiveness - 2.0 + 1.0) / (varianceEffectiveness * (std::pow(numberOfDimensions + 2.0, 2.0) + 1.0))); //and for rank-mu update
+    double damps = 1.0 + 2.0 * std::max(0.0, std::sqrt((varianceEffectiveness - 1.0) / (numberOfDimensions + 1.0)) - 1.0) + cs; //damping for sigma
     //init dynamic parameters and constants
     arma::Col<double> pc(numberOfDimensions, arma::fill::zeros);
     arma::Col<double> ps(numberOfDimensions, arma::fill::zeros);
@@ -31,7 +31,7 @@ namespace hop {
     arma::Col<double> D(numberOfDimensions, arma::fill::ones);
     arma::Mat<double> C(numberOfDimensions, numberOfDimensions, arma::fill::eye);
     arma::Mat<double> invsqrtC(numberOfDimensions, numberOfDimensions, arma::fill::eye);
-    double chiN = std::sqrt(numberOfDimensions) * (1.0 - (1.0 / (4.0 * numberOfDimensions)) + (1.0 / (21.0 * std::pow(numberOfDimensions, 2))));
+    double chiN = std::sqrt(numberOfDimensions) * (1.0 - (1.0 / (4.0 * numberOfDimensions)) + (1.0 / (21.0 * std::pow(numberOfDimensions, 2.0))));
 
     //LOOP START
     while (!isFinished() && !isTerminated()) {
@@ -48,7 +48,7 @@ namespace hop {
           bestObjectiveValue_ = arfitness(n);
           bestParameter_ = arx.col(n);
           //otherwise it's always infinity, so isFinished() doesn't work ;)
-          bestSoftConstraintValue_ = 0;
+          bestSoftConstraintValue_ = 0.0;
         }
 
         if (isFinished() || isTerminated()) {
@@ -62,27 +62,27 @@ namespace hop {
       //TODO: i don't know how to do a "cool" matlab style select like 1:lambda over arindex, so this is it
       // mind usage later on in adapt covariance
       arma::Col<arma::uword> shortArindex = arma::Col<arma::uword>(numberOfParents);
-      for (int i = 0; i < numberOfParents; i++) {
-        shortArindex.at(i) = arindex.at(i);
+      for (std::size_t n = 0; n < numberOfParents; ++n) {
+        shortArindex.at(n) = arindex.at(n);
       }
       
       objectiveValues = arx.cols(shortArindex) * weights;
       
       //cumulation: update evolution paths
-      ps = (1 - cs) * ps + std::sqrt(cs * (2 - cs) * varianceEffectiveness) * invsqrtC * (objectiveValues - oldObjectiveValues) / stepSize_;
+      ps = (1.0 - cs) * ps + std::sqrt(cs * (2.0 - cs) * varianceEffectiveness) * invsqrtC * (objectiveValues - oldObjectiveValues) / stepSize_;
       //original matlab line: hsig = norm(ps)/sqrt(1-(1-cs)^(2*numberOfIterations_/lambda))/chiN < 1.4 + 2/(numberOfDimensions+1);
       double hsigLeftSide = (arma::norm(ps) / std::sqrt(1 - std::pow((1 - cs), 2 * numberOfIterations_ / populationSize_))) / chiN;
       double hsigRightSide = 1.4 + 2 / (numberOfDimensions + 1.0);
       //TODO: hsig cannot be an int (will always be zero), although its a bool operation happening. Must be an arma thing. Maybe even a bug?
       double hsig = hsigLeftSide < hsigRightSide;
 
-      pc = (1 - cc) * pc + hsig * std::sqrt(cc * (2 - cc) * varianceEffectiveness) * (objectiveValues - oldObjectiveValues) / stepSize_;
+      pc = (1.0 - cc) * pc + hsig * std::sqrt(cc * (2.0 - cc) * varianceEffectiveness) * (objectiveValues - oldObjectiveValues) / stepSize_;
 
       //adapt covariance matrix C
-      arma::Mat<double> artmp = (1 / stepSize_) * arx.cols(shortArindex) - arma::repmat(oldObjectiveValues, 1, numberOfParents);
-      C = (1 - c1 - cmu) * C + //old matrix
+      arma::Mat<double> artmp = (1.0 / stepSize_) * arx.cols(shortArindex) - arma::repmat(oldObjectiveValues, 1, numberOfParents);
+      C = (1.0 - c1 - cmu) * C + //old matrix
           c1 * (pc * pc.t() + //rank one update
-          (1 - hsig) * cc * (2 - cc) * C) + //correction for hsig==0
+          (1.0 - hsig) * cc * (2.0 - cc) * C) + //correction for hsig==0
           cmu * artmp * arma::diagmat(weights) * artmp.t(); // rank mu update
 
       //adapt stepsize sigma
@@ -91,13 +91,13 @@ namespace hop {
       //decomposition of C into B*diag(D.^2)*B' (diagonalization)
       //the first comparator element is actually numberOfIterations - eigeneval, but eigeneval only ever holds NOI - NOI last turn, which is the same as popSize
       //this only might get calculated when it shouldn't is if a acceptable value is found before we reach popSize. But this gets called every time with atleast dim>=30 anyway
-      if (populationSize_ > (populationSize_ / (c1 + cmu) / (numberOfDimensions*1.0) / 10.0)) {
+      if (populationSize_ > (populationSize_ / (c1 + cmu) / (numberOfDimensions * 1.0) / 10.0)) {
         C = arma::symmatu(C);
         //matlab code is [B,D] = eig(C); arma uses switched arguments
         arma::Col<double> tempD;
         arma::eig_sym(tempD, B, C);
         D = arma::sqrt(tempD);
-        invsqrtC = B * arma::diagmat(1 / D) * B.t();
+        invsqrtC = B * arma::diagmat(1.0 / D) * B.t();
       }
     }
     //LOOP END
