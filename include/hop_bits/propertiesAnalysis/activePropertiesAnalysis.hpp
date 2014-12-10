dc81133a1d0420d1ee77ecf74fb9d337745a7b6d
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-12-10 16:11:31 +0100

    devel: Added active property analysis

---
+++ include/hop_bits/propertiesAnalysis/activePropertiesAnalysis.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// HOP
+#include <hop_bits/propertiesAnalysis.hpp>
+
+namespace hop {
+  class ActivePropertiesAnalysis : public PropertiesAnalysis {
+    public:
+      using PropertiesAnalysis::PropertiesAnalysis;
+  };
+}
