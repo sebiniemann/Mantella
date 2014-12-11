1b246aa198210ac95c27033bd0886025be67a263
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-12-11 09:40:20 +0100

    devel: Added quadratic model residuals threshold
    Still missing normalisation of residuals

--- include/hop_bits/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis.hpp
+++ include/hop_bits/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis.hpp
@@ -6,11 +6,20 @@
 namespace hop {
   class QuadraticModelAnalysis : public FunctionModelAnalysis {
     public:
-      using FunctionModelAnalysis::FunctionModelAnalysis;
+      explicit QuadraticModelAnalysis() noexcept;
+
+      // Copy constructors are not used in this library and deleted to avoid unintended/any usage.
+      QuadraticModelAnalysis(const QuadraticModelAnalysis&) = delete;
+      QuadraticModelAnalysis& operator=(const QuadraticModelAnalysis&) = delete;
+
+      void setQuadraticModelResidualsThreshold(
+          const double& quadraticModelResidualsThreshold);
 
       arma::Col<double> getQuadraticModelEstimator() const noexcept;
 
     protected:
       arma::Col<double> quadraticModelEstimator_;
+
+      double quadraticModelResidualsThreshold_;
   };
 }
--- src/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis.cpp
+++ src/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis.cpp
@@ -1,7 +1,20 @@
 #include <hop_bits/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/quadraticModelAnalysis.hpp>
 
 namespace hop {
+  QuadraticModelAnalysis::QuadraticModelAnalysis() noexcept {
+    setQuadraticModelResidualsThreshold(0.75);
+  }
+
   arma::Col<double> QuadraticModelAnalysis::getQuadraticModelEstimator() const noexcept {
     return quadraticModelEstimator_;
+  }
+
+  void QuadraticModelAnalysis::setQuadraticModelResidualsThreshold(
+      const double& quadraticModelResidualsThreshold) {
+    if(quadraticModelResidualsThreshold < 0 || quadraticModelResidualsThreshold > 1) {
+      throw std::runtime_error("The quadratic model residuals threshold (" + std::to_string(quadraticModelResidualsThreshold) + ") must be within 0 and 1.");
+    }
+
+    quadraticModelResidualsThreshold_ = quadraticModelResidualsThreshold;
   }
 }
