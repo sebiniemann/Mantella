559a78e5698e4da43e1c9fb82304a2e58efd4e3d
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-12-11 09:40:25 +0100

    devel: Added linear model residuals threshold

--- include/hop_bits/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/linearModelAnalysis.hpp
+++ include/hop_bits/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/linearModelAnalysis.hpp
@@ -6,11 +6,20 @@
 namespace hop {
   class LinearModelAnalysis : public FunctionModelAnalysis {
     public:
-      using FunctionModelAnalysis::FunctionModelAnalysis;
+      explicit LinearModelAnalysis() noexcept;
+
+      // Copy constructors are not used in this library and deleted to avoid unintended/any usage.
+      LinearModelAnalysis(const LinearModelAnalysis&) = delete;
+      LinearModelAnalysis& operator=(const LinearModelAnalysis&) = delete;
+
+      void setLinearModelResidualsThreshold(
+          const double& linearModelResidualsThreshold);
 
       arma::Col<double> getLinearModelEstimator() const noexcept;
 
     protected:
       arma::Col<double> linearModelEstimator_;
+
+      double linearModelResidualsThreshold_;
   };
 }
--- src/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/linearModelAnalysis.cpp
+++ src/propertiesAnalysis/passivePropertiesAnalysis/functionModelAnalysis/linearModelAnalysis.cpp
@@ -4,4 +4,13 @@ namespace hop {
   arma::Col<double> LinearModelAnalysis::getLinearModelEstimator() const noexcept {
     return linearModelEstimator_;
   }
+
+  void LinearModelAnalysis::setLinearModelResidualsThreshold(
+      const double& linearModelResidualsThreshold) {
+    if(linearModelResidualsThreshold < 0 || linearModelResidualsThreshold > 1) {
+      throw std::runtime_error("The quadratic model residuals threshold (" + std::to_string(linearModelResidualsThreshold) + ") must be within 0 and 1.");
+    }
+
+    linearModelResidualsThreshold_ = linearModelResidualsThreshold;
+  }
 }
