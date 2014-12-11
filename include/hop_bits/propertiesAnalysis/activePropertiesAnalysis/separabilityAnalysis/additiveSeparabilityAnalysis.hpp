fb6dc3cf1792c93cdb5f57d536722bfb559fb77c
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-12-11 09:39:50 +0100

    devel: Added additive separability threshold setter

--- include/hop_bits/propertiesAnalysis/activePropertiesAnalysis/separabilityAnalysis/additiveSeparabilityAnalysis.hpp
+++ include/hop_bits/propertiesAnalysis/activePropertiesAnalysis/separabilityAnalysis/additiveSeparabilityAnalysis.hpp
@@ -6,6 +6,16 @@
 namespace hop {
   class AdditiveSeparabilityAnalysis : public SeparabilityAnalysis {
     public:
-      using SeparabilityAnalysis::SeparabilityAnalysis;
+      explicit AdditiveSeparabilityAnalysis() noexcept;
+
+      // Copy constructors are not used in this library and deleted to avoid unintended/any usage.
+      AdditiveSeparabilityAnalysis(const AdditiveSeparabilityAnalysis&) = delete;
+      AdditiveSeparabilityAnalysis& operator=(const AdditiveSeparabilityAnalysis&) = delete;
+
+      void setAdditiveSeparabilityThreshold(
+          const double& additiveSeparabilityThreshold);
+
+    protected:
+      double additiveSeparabilityThreshold_;
   };
 }
--- src/propertiesAnalysis/activePropertiesAnalysis/separabilityAnalysis/additiveSeparabilityAnalysis.cpp
+++ src/propertiesAnalysis/activePropertiesAnalysis/separabilityAnalysis/additiveSeparabilityAnalysis.cpp
@@ -1,2 +1,16 @@
 #include <hop_bits/propertiesAnalysis/activePropertiesAnalysis/separabilityAnalysis/additiveSeparabilityAnalysis.hpp>
 
+namespace hop {
+  AdditiveSeparabilityAnalysis::AdditiveSeparabilityAnalysis() noexcept {
+    setAdditiveSeparabilityThreshold(0.75);
+  }
+
+  void AdditiveSeparabilityAnalysis::setAdditiveSeparabilityThreshold(
+      const double& additiveSeparabilityThreshold) {
+    if(additiveSeparabilityThreshold < 0 || additiveSeparabilityThreshold > 1) {
+      throw std::runtime_error("The additive separability threshold (" + std::to_string(additiveSeparabilityThreshold) + ") must be within 0 and 1.");
+    }
+
+    additiveSeparabilityThreshold_ = additiveSeparabilityThreshold;
+  }
+}
