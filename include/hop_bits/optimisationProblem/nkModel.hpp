607b342aed4724d4d5f2b20d9f9d3b36ff0d7dac
Sebastian Niemann <niemann@sra.uni-hannover.de>
2014-12-29 12:22:21 +0100

    devel: Renamed to NKModel

---
+++ include/hop_bits/optimisationProblem/nkModel.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// HOP
+#include <hop_bits/optimisationProblem.hpp>
+
+namespace hop {
+  class NKModel : public OptimisationProblem<unsigned int> {
+    public:
+      explicit NKModel(
+          const unsigned int& numberOfNeighbours,
+          const unsigned int& numberOfDimensions) noexcept;
+
+      // Copy constructors are not used in this library and deleted to avoid unintended/any usage.
+      NKModel(const NKModel&) = delete;
+      NKModel& operator=(const NKModel&) = delete;
+
+    protected:
+      double getObjectiveValueImplementation(
+          const arma::Col<unsigned int>& parameter) const noexcept override;
+  };
+}
