55dd3f02c474b47ff03766b0da841c4d5033cd2b
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-06 08:03:28 +0100

    devel: Added BBOB 2010 problems

---
+++ include/hop_bits/optimisationProblem/blackBoxOptimisationBenchmark2010/differentPowersFunction.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+// HOP
+#include <hop_bits/optimisationProblem/blackBoxOptimisationBenchmark2010.hpp>
+
+namespace hop {
+  namespace bbob2010 {
+    class DifferentPowersFunction : public BlackBoxOptimisationBenchmark2010 {
+      public:
+        using BlackBoxOptimisationBenchmark2010::BlackBoxOptimisationBenchmark2010;
+
+        DifferentPowersFunction(const DifferentPowersFunction&) = delete;
+        DifferentPowersFunction& operator=(const DifferentPowersFunction&) = delete;
+
+        std::string to_string() const noexcept override;
+
+      protected:
+        double getObjectiveValueImplementation(
+            const arma::Col<double>& parameter) const noexcept override;
+
+        friend class cereal::access;
+
+        template <typename Archive>
+        void serialize(
+            Archive& archive) noexcept {
+          archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2010", cereal::base_class<BlackBoxOptimisationBenchmark2010>(this)));
+          archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
+          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("rotationR", rotationR_));
+        }
+
+        template <typename Archive>
+        static void load_and_construct(
+            Archive& archive,
+            cereal::construct<DifferentPowersFunction>& construct) noexcept {
+          unsigned int numberOfDimensions;
+          archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions));
+          construct(numberOfDimensions);
+
+          archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2010", cereal::base_class<BlackBoxOptimisationBenchmark2010>(construct.ptr())));
+          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("rotationR", construct->rotationR_));
+        }
+    };
+  }
+}
+
---
+++ include/hop_bits/optimisationProblem/blackBoxOptimisationBenchmark2010/weierstrassFunction.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+// C++ Standard Library
+#include <cmath>
+
+// HOP
+#include <hop_bits/optimisationProblem/blackBoxOptimisationBenchmark2010.hpp>
+
+namespace hop {
+  namespace bbob2010 {
+    class WeierstrassFunction : public BlackBoxOptimisationBenchmark2010 {
+      public:
+        explicit WeierstrassFunction(
+            const unsigned int& numberOfDimensions) noexcept;
+
+        WeierstrassFunction(const WeierstrassFunction&) = delete;
+        WeierstrassFunction& operator=(const WeierstrassFunction&) = delete;
+
+        std::string to_string() const noexcept override;
+
+      protected:
+        double f0_;
+        const arma::Col<double> delta_ = getScaling(std::sqrt(0.01));
+
+        double getObjectiveValueImplementation(
+            const arma::Col<double>& parameter) const noexcept override;
+
+        friend class cereal::access;
+
+        template <typename Archive>
+        void serialize(
+            Archive& archive) noexcept {
+          archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2010", cereal::base_class<BlackBoxOptimisationBenchmark2010>(this)));
+          archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
+          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("rotationR", rotationR_));
+          archive(cereal::make_nvp("rotationQ", rotationQ_));
+          archive(cereal::make_nvp("f0", f0_));
+        }
+
+        template <typename Archive>
+        static void load_and_construct(
+            Archive& archive,
+            cereal::construct<WeierstrassFunction>& construct) noexcept {
+          unsigned int numberOfDimensions;
+          archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions));
+          construct(numberOfDimensions);
+
+          archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2010", cereal::base_class<BlackBoxOptimisationBenchmark2010>(construct.ptr())));
+          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("rotationR", construct->rotationR_));
+          archive(cereal::make_nvp("rotationQ", construct->rotationQ_));
+          archive(cereal::make_nvp("f0", construct->f0_));
+        }
+    };
+  }
+}
