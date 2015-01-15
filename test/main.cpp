989bc068fa09e0173b73c967f98a5429aae1ec83
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-15 09:45:48 +0100

    devel: Even better error messages

--- test/main.cpp
+++ test/main.cpp
@@ -18,13 +18,13 @@ boost::filesystem::path testDirectory;
 int main(const int argc, const char* argv[]) {
   try {
     if (argc != 2) {
-      throw std::invalid_argument("The location of the test data directory need to be added to the command line.");
+      throw std::invalid_argument("The location of the test directory must be added to the command line.");
     }
 
     testDirectory = boost::filesystem::path(argv[1]);
 
     if (!boost::filesystem::exists(testDirectory)) {
-      throw std::invalid_argument("The speficied test data directory (" + testDirectory.string() + ") does not exists.");
+      throw std::invalid_argument("The speficied test directory (" + testDirectory.string() + ") does not exists.");
     }
 
     mant::Rng::setSeed(1234567890);
