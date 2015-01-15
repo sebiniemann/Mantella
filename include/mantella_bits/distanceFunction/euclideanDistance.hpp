404de75c3d69aa3b232788679228f31f7d9cf2b2
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-15 18:53:59 +0100

    devel: Realised the new methods for the eucledian distance

--- include/mantella_bits/distanceFunction/euclideanDistance.hpp
+++ include/mantella_bits/distanceFunction/euclideanDistance.hpp
@@ -8,6 +8,11 @@ namespace mant {
     protected:
       double getDistanceImplementation(
           const arma::Col<double>& firstParameter,
-          const arma::Col<double>& secondParameter) const override;
+          const arma::Col<double>& secondParameter) const noexcept override;
+
+      arma::Col<double> getNeighourImplementation(
+          const arma::Col<double>& parameter,
+          const arma::Col<double>& minimalDistance,
+          const arma::Col<double>& maximalDistance) const noexcept override;
   };
 }
--- src/distanceFunction/euclideanDistance.cpp
+++ src/distanceFunction/euclideanDistance.cpp
@@ -1,9 +1,20 @@
 #include <mantella_bits/distanceFunction/euclideanDistance.hpp>
 
+// Mantella
+#include <mantella_bits/helper/rng.hpp>
+
 namespace mant {
   double EuclideanDistance::getDistanceImplementation(
       const arma::Col<double>& firstParameter,
-      const arma::Col<double>& secondParameter) const {
+      const arma::Col<double>& secondParameter) const noexcept {
     return arma::norm(secondParameter - firstParameter);
+  }
+
+  arma::Col<double> EuclideanDistance::getNeighourImplementation(
+      const arma::Col<double>& parameter,
+      const arma::Col<double>& minimalDistance,
+      const arma::Col<double>& maximalDistance) const noexcept {
+    const arma::Col<double>& velocity = arma::normalise(arma::randn<arma::Col<double>>(parameter.n_elem)) * std::uniform_real_distribution<double>(0.0, 1.0)(Rng::generator);
+    return parameter + arma::sign(velocity) % minimalDistance + (maximalDistance - minimalDistance) % velocity;
   }
 }
