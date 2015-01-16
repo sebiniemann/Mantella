77fefdb5deb11d03d6bb6fa4c52a9f737a285fb1
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-16 12:13:38 +0100

    devel: Templated random search

--- include/mantella_bits/optimisationAlgorithm/samplingBasedAlgorithm/randomSearch.hpp
+++ include/mantella_bits/optimisationAlgorithm/samplingBasedAlgorithm/randomSearch.hpp
@@ -2,13 +2,13 @@
 
 // Mantella
 #include <mantella_bits/optimisationAlgorithm/samplingBasedAlgorithm.hpp>
-#include <mantella_bits/distanceFunction/euclideanDistance.hpp>
 
 namespace mant {
-  class RandomSearch : public SamplingBasedAlgorithm<double, EuclideanDistance> {
+  template <typename ParameterType, class DistanceFunction>
+  class RandomSearch : public SamplingBasedAlgorithm<ParameterType, DistanceFunction> {
     public:
       explicit RandomSearch(
-          const std::shared_ptr<OptimisationProblem<double>> optimisationProblem) noexcept;
+          const std::shared_ptr<OptimisationProblem<ParameterType>> optimisationProblem) noexcept;
 
       RandomSearch(const RandomSearch&) = delete;
       RandomSearch& operator=(const RandomSearch&) = delete;
@@ -18,4 +18,33 @@ namespace mant {
     protected:
       void optimiseImplementation() noexcept override;
   };
+
+  template <typename ParameterType, class DistanceFunction>
+  RandomSearch<ParameterType, DistanceFunction>::RandomSearch(
+      const std::shared_ptr<OptimisationProblem<ParameterType>> optimisationProblem) noexcept
+    : SamplingBasedAlgorithm<ParameterType, DistanceFunction>(optimisationProblem) {
+
+  }
+
+  template <typename ParameterType, class DistanceFunction>
+  void RandomSearch<ParameterType, DistanceFunction>::optimiseImplementation() noexcept {
+    while(!this->isFinished() && !this->isTerminated()) {
+      ++this->numberOfIterations_;
+
+      const arma::Col<ParameterType>& candidateParameter = this->distanceFunction_.getNeighour(this->optimisationProblem_->getLowerBounds(), this->optimisationProblem_->getUpperBounds() - this->optimisationProblem_->getLowerBounds());
+      const double& candidateSoftConstraintsValue = this->optimisationProblem_->getSoftConstraintsValue(candidateParameter);
+      const double& candidateObjectiveValue = this->optimisationProblem_->getObjectiveValue(candidateParameter);
+
+      if(candidateSoftConstraintsValue < this->bestSoftConstraintsValue_ || (candidateSoftConstraintsValue == this->bestSoftConstraintsValue_ && candidateObjectiveValue < this->bestObjectiveValue_)) {
+        this->bestParameter_ = candidateParameter;
+        this->bestSoftConstraintsValue_ = candidateSoftConstraintsValue;
+        this->bestObjectiveValue_ = candidateObjectiveValue;
+      }
+    };
+  }
+
+  template <typename ParameterType, class DistanceFunction>
+  std::string RandomSearch<ParameterType, DistanceFunction>::to_string() const noexcept {
+    return "RandomSearch";
+  }
 }
