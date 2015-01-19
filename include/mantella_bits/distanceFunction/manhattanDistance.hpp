8ef11ac4911bdf5b6acf15c57d3c3ea1545d8e3e
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-19 15:11:25 +0100

    devel: Added the manhattan distance (not yet fully for discrete values)

--- include/mantella_bits/distanceFunction/manhattanDistance.hpp
+++ include/mantella_bits/distanceFunction/manhattanDistance.hpp
@@ -2,17 +2,63 @@
 
 // Mantella
 #include <mantella_bits/distanceFunction.hpp>
+#include <mantella_bits/helper/rng.hpp>
 
 namespace mant {
-  class ManhattanDistance : public DistanceFunction<double> {
+  template <typename ParameterType>
+  class ManhattanDistance : public DistanceFunction<ParameterType> {
     protected:
-      double getDistanceImplementation(
-          const arma::Col<double>& firstParameter,
-          const arma::Col<double>& secondParameter) const noexcept override;
+      ParameterType getDistanceImplementation(
+          const arma::Col<ParameterType>& parameter) const noexcept override;
 
-      arma::Col<double> getNeighourImplementation(
-          const arma::Col<double>& parameter,
-          const arma::Col<double>& minimalDistance,
-          const arma::Col<double>& maximalDistance) const noexcept override;
+      arma::Col<ParameterType> getNeighbourImplementation(
+          const arma::Col<ParameterType>& parameter,
+          const ParameterType& minimalDistance,
+          const ParameterType& maximalDistance) const noexcept override;
+
+      arma::Col<ParameterType> getNeighbourImplementation(
+          const arma::Col<ParameterType>& parameter,
+          const ParameterType& minimalDistance,
+          const ParameterType& maximalDistance,
+          std::true_type) const noexcept;
+
+      arma::Col<ParameterType> getNeighbourImplementation(
+          const arma::Col<ParameterType>& parameter,
+          const ParameterType& minimalDistance,
+          const ParameterType& maximalDistance,
+          std::false_type) const noexcept;
   };
+
+  template <typename ParameterType>
+  ParameterType ManhattanDistance<ParameterType>::getDistanceImplementation(
+      const arma::Col<ParameterType>& parameter) const noexcept {
+    return arma::norm(parameter, 1);
+  }
+
+  // TODO Minus / int / unsigned int
+  template <typename ParameterType>
+  arma::Col<ParameterType> ManhattanDistance<ParameterType>::getNeighbourImplementation(
+      const arma::Col<ParameterType>& parameter,
+      const ParameterType& minimalDistance,
+      const ParameterType& maximalDistance) const noexcept {
+    return getNeighbourImplementation(parameter, minimalDistance, maximalDistance, std::is_floating_point<ParameterType>());
+  }
+
+  template <typename ParameterType>
+  arma::Col<ParameterType> ManhattanDistance<ParameterType>::getNeighbourImplementation(
+      const arma::Col<ParameterType>& parameter,
+      const ParameterType& minimalDistance,
+      const ParameterType& maximalDistance,
+      std::true_type) const noexcept {
+    return arma::normalise(2.0 * arma::randu(parameter.n_elem) - 1.0, 1) * std::uniform_real_distribution<ParameterType>(minimalDistance, maximalDistance)(Rng::generator);
+  }
+
+  template <typename ParameterType>
+  arma::Col<ParameterType> ManhattanDistance<ParameterType>::getNeighbourImplementation(
+      const arma::Col<ParameterType>& parameter,
+      const ParameterType& minimalDistance,
+      const ParameterType& maximalDistance,
+      std::false_type) const noexcept {
+
+  }
 }
--- src/distanceFunction/manhattanDistance.cpp
+++ src/distanceFunction/manhattanDistance.cpp
@@ -1,19 +1,1 @@
 #include <mantella_bits/distanceFunction/manhattanDistance.hpp>
-
-// Mantella
-#include <mantella_bits/helper/rng.hpp>
-
-namespace mant {
-  double ManhattanDistance::getDistanceImplementation(
-      const arma::Col<double>& firstParameter,
-      const arma::Col<double>& secondParameter) const noexcept {
-    return arma::norm(secondParameter - firstParameter, 1);
-  }
-
-  arma::Col<double> ManhattanDistance::getNeighourImplementation(
-      const arma::Col<double>& parameter,
-      const arma::Col<double>& minimalDistance,
-      const arma::Col<double>& maximalDistance) const noexcept {
-    // TODO implement
-  }
-}
