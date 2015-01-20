b904409fd23de150007bab010dc87b24e1f5ece3
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-20 11:42:46 +0100

    devel: Removed testing namespace and simplified test case names

--- test/helper/testString.cpp
+++ test/helper/testString.cpp
@@ -4,28 +4,26 @@
 // Mantella
 #include <mantella>
 
-namespace testing {
-  class TestPrintable : public mant::Printable {
-    public:
-      std::string to_string() const noexcept override {
-        return "ThisIsTestPrintable";
-      }
-  };
-}
+class TestPrintable : public mant::Printable {
+  public:
+    std::string to_string() const noexcept override {
+      return "ThisIsTestPrintable";
+    }
+};
 
 TEST_CASE("string: to_string(Printable)", "") {
-  testing::TestPrintable testPrintable;
+  TestPrintable testPrintable;
 
   CHECK(mant::to_string(testPrintable) == "ThisIsTestPrintable");
 }
 
 TEST_CASE("string: to_string(shared_ptr<Printable>)", "") {
-  std::shared_ptr<mant::Printable> testPrintable = std::shared_ptr<mant::Printable>(new testing::TestPrintable);
+  std::shared_ptr<mant::Printable> testPrintable = std::shared_ptr<mant::Printable>(new TestPrintable);
 
   CHECK(mant::to_string(testPrintable) == "ThisIsTestPrintable");
 }
 
-TEST_CASE("string: endsWith(string, string)", "") {
+TEST_CASE("string: endsWith", "") {
   CHECK(mant::endsWith("input.serialised", ".serialised") == true);
   CHECK(mant::endsWith("input.serialised", "put.serialised") == true);
   CHECK(mant::endsWith("input.serialised", "input.serialised") == true);
