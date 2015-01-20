e7b9be1af05b0668e568cba0c4840864c22d019c
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-20 16:39:47 +0100

    devel: Trying to debug something on Travis

--- test/optimisationProblem/blackBoxOptimisationBenchmark2009/testAttractiveSectorFunction.cpp
+++ test/optimisationProblem/blackBoxOptimisationBenchmark2009/testAttractiveSectorFunction.cpp
@@ -14,33 +14,55 @@
 // Mantella
 #include <mantella>
 
+#include <iostream>
+
 extern boost::filesystem::path testDirectory;
 
 TEST_CASE("BBOB2009-AttractiveSectorFunction", "") {
   for (const auto& numberOfDimensions : {2, 40}) {
     mant::bbob2013::AttractiveSectorFunction attractiveSectorFunction(numberOfDimensions);
 
+    std::cout << "AAA" << numberOfDimensions << std::endl;
+
     arma::Mat<double> parameters;
     parameters.load(testDirectory.string() + "/data/optimisationProblem/blackBoxOptimisationBenchmark2013/parameters,dim" + std::to_string(numberOfDimensions) +".mat");
+
+    std::cout << "BBB" << numberOfDimensions << std::endl;
 
     arma::Col<double> translation;
     translation.load(testDirectory.string() + "/data/optimisationProblem/blackBoxOptimisationBenchmark2013/translation,dim" + std::to_string(numberOfDimensions) +".mat");
 
+    std::cout << "CCC" << numberOfDimensions << std::endl;
+
     arma::Mat<double> rotationR;
     rotationR.load(testDirectory.string() + "/data/optimisationProblem/blackBoxOptimisationBenchmark2013/rotationR,dim" + std::to_string(numberOfDimensions) +".mat");
+
+    std::cout << "DDD" << numberOfDimensions << std::endl;
 
     arma::Mat<double> rotationQ;
     rotationQ.load(testDirectory.string() + "/data/optimisationProblem/blackBoxOptimisationBenchmark2013/rotationQ,dim" + std::to_string(numberOfDimensions) +".mat");
 
+    std::cout << "EEE" << numberOfDimensions << std::endl;
+
     arma::Col<double> expected;
     expected.load(testDirectory.string() + "/data/optimisationProblem/blackBoxOptimisationBenchmark2013/expectedAttractiveSectorFunction,dim" + std::to_string(numberOfDimensions) +".mat");
 
+    std::cout << "FFF" << numberOfDimensions << std::endl;
+
     attractiveSectorFunction.setObjectiveValueTranslation(0);
+
+    std::cout << "GGG" << numberOfDimensions << std::endl;
     attractiveSectorFunction.setTranslation(translation);
+
+    std::cout << "HHH" << numberOfDimensions << std::endl;
     attractiveSectorFunction.setRotationR(rotationR);
+
+    std::cout << "III" << numberOfDimensions << std::endl;
     attractiveSectorFunction.setRotationQ(rotationQ);
 
     for (std::size_t n = 0; n < parameters.n_cols; ++n) {
+
+      std::cout << "JJJ" << numberOfDimensions << "###" << n << std::endl;
       CHECK(attractiveSectorFunction.getObjectiveValue(parameters.col(n)) == Approx(expected.at(n)));
     }
   }
