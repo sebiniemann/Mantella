ebce422c1bfcff90529a0d4c02ecf8c25110ca63
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-20 11:40:36 +0100

    test: Added tests for the Printable class

---
+++ test/helper/testPrintable.cpp
@@ -0,0 +1,18 @@
+// Catch
+#include <catch.hpp>
+
+// Mantella
+#include <mantella>
+
+class TestPrintable : public mant::Printable {
+  public:
+    std::string to_string() const noexcept override {
+      return "ThisIsTestPrintable";
+    }
+};
+
+TEST_CASE("printable: Printable", "") {
+  TestPrintable testPrintable;
+
+  CHECK(testPrintable.to_string() == "ThisIsTestPrintable");
+}
