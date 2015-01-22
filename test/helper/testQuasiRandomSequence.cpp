744dbd5999376aef4c15eb72a1fff1c8512f717c
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-22 11:40:00 +0100

    test: Reorganised quasiRandomSequence.hpp test cases

--- test/helper/testQuasiRandomSequence.cpp
+++ test/helper/testQuasiRandomSequence.cpp
@@ -5,7 +5,7 @@
 // Mantella
 #include <mantella>
 
-TEST_CASE("quasiRandomSequence: getHaltonSequence", "") {
+TEST_CASE("quasiRandomSequence: getHaltonSequence(...)", "") {
   arma::Mat<double>::fixed<2, 5> expected;
 
   expected = {
@@ -30,7 +30,7 @@ TEST_CASE("quasiRandomSequence: getHaltonSequence", "") {
   CHECK_THROWS_AS(mant::getHaltonSequence({4, 5}, {3}, 6), std::logic_error);
 }
 
-TEST_CASE("quasiRandomSequence: getVanDerCorputSequence", "") {
+TEST_CASE("quasiRandomSequence: getVanDerCorputSequence(...)", "") {
   compare(mant::getVanDerCorputSequence(2, 0, 5), {0.0, 1.0/2.0, 1.0/4.0, 3.0/4.0, 1.0/8.0});
   compare(mant::getVanDerCorputSequence(3, 3, 5), {1.0/9.0, 4.0/9.0, 7.0/9.0, 2.0/9.0, 5.0/9.0});
 }
