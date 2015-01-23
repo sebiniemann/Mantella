d2b0b68e2d05eaba113763ebcd507291f745efd2
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-01-23 14:53:22 +0100

    test: Reorganised tests for string.hpp

--- test/helper/testString.cpp
+++ test/helper/testString.cpp
@@ -12,25 +12,42 @@ class TestPrintable : public mant::Printable {
 };
 
 TEST_CASE("string: to_string(Printable)", "") {
-  TestPrintable testPrintable;
+  SECTION("Returns the specified class name.") {
+    TestPrintable testPrintable;
 
-  CHECK(mant::to_string(testPrintable) == "ThisIsTestPrintable");
+    CHECK(mant::to_string(testPrintable) == "ThisIsTestPrintable");
+  }
 }
 
 TEST_CASE("string: to_string(shared_ptr<Printable>)", "") {
-  std::shared_ptr<mant::Printable> testPrintable = std::shared_ptr<mant::Printable>(new TestPrintable);
+  SECTION("Returns the specified class name.") {
+    std::shared_ptr<mant::Printable> testPrintable = std::shared_ptr<mant::Printable>(new TestPrintable);
 
-  CHECK(mant::to_string(testPrintable) == "ThisIsTestPrintable");
+    CHECK(mant::to_string(testPrintable) == "ThisIsTestPrintable");
+  }
 }
 
 TEST_CASE("string: endsWith", "") {
-  CHECK(mant::endsWith("input.serialised", ".serialised") == true);
-  CHECK(mant::endsWith("input.serialised", "put.serialised") == true);
-  CHECK(mant::endsWith("input.serialised", "input.serialised") == true);
-  CHECK(mant::endsWith("input.serialised", "serialised") == true);
-  CHECK(mant::endsWith("input.serialised", ".serialise") == false);
-  CHECK(mant::endsWith("input.serialised", "erialise") == false);
-  CHECK(mant::endsWith("input.serialised", ".mat") == false);
+  SECTION("Finds various subsets of endings.") {
+    CHECK(mant::endsWith("input.serialised", ".serialised") == true);
+    CHECK(mant::endsWith("input.serialised", "d") == true);
+    CHECK(mant::endsWith("input.serialised", "put.serialised") == true);
+    CHECK(mant::endsWith("input.serialised", "serialised") == true);
+  }
+
+  SECTION("Recognised the input itself as an ending.") {
+    CHECK(mant::endsWith("input.serialised", "input.serialised") == true);
+  }
+
+  SECTION("Ignores substrings of the input, if something from the ending is missing.") {
+    CHECK(mant::endsWith("input.serialised", ".serialise") == false);
+    CHECK(mant::endsWith("input.serialised", "erialise") == false);
+  }
+
+  SECTION("Ignores any string not occuring within the input.") {
+    CHECK(mant::endsWith("input.serialised", ".mat") == false);
+  }
+
 }
 
 
