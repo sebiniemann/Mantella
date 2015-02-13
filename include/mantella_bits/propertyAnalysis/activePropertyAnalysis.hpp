f3ecba0ec277d6f2d7bb010fafe24871851c48b5
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-02-13 17:30:40 +0100

    devel: Require an optimisation problem for the active property analysis

--- include/mantella_bits/propertyAnalysis/activePropertyAnalysis.hpp
+++ include/mantella_bits/propertyAnalysis/activePropertyAnalysis.hpp
@@ -3,5 +3,18 @@ namespace mant {
   class ActivePropertyAnalysis : public PropertyAnalysis<ParameterType> {
     public:
       using PropertyAnalysis<ParameterType>::PropertyAnalysis;
+
+      void analyse(
+          const std::shared_ptr<OptimisationProblem<ParameterType>>) noexcept;
+
+    protected:
+      virtual void analyseImplementation(
+          const std::shared_ptr<OptimisationProblem<ParameterType>>) noexcept = 0;
   };
+
+  template <typename ParameterType>
+  void ActivePropertyAnalysis<ParameterType>::analyse(
+      const std::shared_ptr<OptimisationProblem<ParameterType>> optimisationProblem) noexcept {
+    analyseImplementation(optimisationProblem);
+  }
 }
