b7b0e6d363e9591f37025baacae2cf97438f0e51
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-02-14 23:57:00 +0100

    devel: Returning the linear proportionality porperty

--- include/mantella_bits/propertyAnalysis/activePropertyAnalysis/proportionalityAnalysis/linearProportionalityAnalysis.hpp
+++ include/mantella_bits/propertyAnalysis/activePropertyAnalysis/proportionalityAnalysis/linearProportionalityAnalysis.hpp
@@ -3,5 +3,19 @@ namespace mant {
   class LinearProportionalityAnalysis : public ProportionalityAnalysis<ParameterType> {
     public:
       using ProportionalityAnalysis<ParameterType>::ProportionalityAnalysis;
+
+      LinearProportionalityProperty<ParameterType> getProperty() const noexcept;
+
+    protected:
+      LinearProportionalityProperty<ParameterType> property_;
   };
+
+  //
+  // Implementation
+  //
+
+  template <typename ParameterType>
+  LinearProportionalityProperty<ParameterType> LinearProportionalityAnalysis<ParameterType>::getProperty() const noexcept {
+    return property_;
+  }
 }
