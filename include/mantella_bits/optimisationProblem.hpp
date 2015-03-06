4ae56502ee2469ecdd5f680a8c259c3ee2fd4f78
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-06 16:16:20 +0100

    devel: Removed comments, which can now be found in the user documentation

--- include/mantella_bits/optimisationProblem.hpp
+++ include/mantella_bits/optimisationProblem.hpp
@@ -1,206 +1,101 @@
 namespace mant {
-  // Base class of all optimisation problems in this library. Provides access to all usually
-  // needed informations.
-  //
-  // Within this library, optiomisation problems are viewed as contiunous, real-valued minimisation
-  // problems with interval and/or soft-constraints. Support for hard-constraints is not yet
-  // implemented.
-  //
-  // The parameter space and objective values can be translated (shifted), rotated and scaled to
-  // avoid biases while evaluating optimisation algorithms.
-  //
-  // Accessing constraints and the objective value makes use of caches to avoid redundant
-  // computations.
   template <typename ParameterType>
   class OptimisationProblem : public Printable {
     public:
-      // The number of dimensions to be optimised
       const unsigned int numberOfDimensions_;
-
-      // Constructs an optimisation problem with the given number of dimensions to be optimised.
+.
       explicit OptimisationProblem(
-        // The number of dimensions
         const unsigned int& numberOfDimensions) noexcept;
 
-      // Checks for each dimension whether the parameter is greater than or equal to the lower
-      // bound.
-      // Returns 0 for each dimension where the constraint is violated and 1 otherwise.
-      // Note: Computes the result only once and caches it afterwards.
       arma::Col<unsigned int> isSatisfyingLowerBounds(
-        // The parameter to be tested
         const arma::Col<ParameterType>& parameter);
 
-      // Checks for each dimension whether the parameter is lower than or equal to the upper bound.
-      // Returns 0 for each dimension where the constraint is violated and 1 otherwise.
-      // Note: Computes the result only once and caches it afterwards.
       arma::Col<unsigned int> isSatisfyingUpperBounds(
-        // The parameter to be tested
         const arma::Col<ParameterType>& parameter);
 
-      // Checks whether the soft-constraint is non-zero.
-      // Returns false if the constraint is violated and true otherwise.
-      // Note: Computes the result only once and caches it afterwards.
       bool isSatisfyingSoftConstraints(
-        // The parameter to be tested
         const arma::Col<ParameterType>& parameter);
 
-      // Checks whether any interval or soft-constraint is violated.
-      // Returns false if the constraint is violated and true otherwise.
-      // Note: Computes the results only once per constraint and caches them afterwards.
       bool isSatisfyingConstraints(
-        // The parameter to be tested
         const arma::Col<ParameterType>& parameter);
 
-      // Returns the sum of all soft-constraint values.
-      // The value must always be positive and only 0 if no constraint is violated.
-      // Note: Computes the result only once and caches it afterwards.
       double getSoftConstraintsValue(
-        // The parameter to be evaluated
         const arma::Col<ParameterType>& parameter);
 
-      // Returns the objective value to be minimised
-      // Better solutions must have a lower objective value.
-      // Note: Computes the result only once and caches it afterwards.
       double getObjectiveValue(
-        // The parameter to be evaluated
         const arma::Col<ParameterType>& parameter);
 
-      // Returns the lower bounds of the search space.
       arma::Col<ParameterType> getLowerBounds() const noexcept;
 
-      // Sets the lower bounds of the search space.
-      // Note: It is not checked, whether it is strictly greater than the lower bound or not.
       void setLowerBounds(
-        // The new lower bounds
         const arma::Col<ParameterType> lowerBounds);
 
-      // Returns the upper bounds of the search space
       arma::Col<ParameterType> getUpperBounds() const noexcept;
 
-      // Sets the upper bounds of the search space.
-      // Note: It is not checked, whether it is strictly greater than the lower bound or not.
       void setUpperBounds(
-        // The new upper bounds
         const arma::Col<ParameterType> upperBounds);
 
-      // Sets the translation (i.e. shift) which is added (not substracted) to the parameter before
-      // any other computation.
-      // Note: The translation is always based on the same origin and will not add up is multiple
-      // set.
       void setParameterTranslation(
-        // The new translation of the parameter space
           const arma::Col<ParameterType> parameterTranslation);
 
-      // Sets the scaling which is multiplied to the parameter after its translation.
-      // Reflections due to negative values are permitted.
-      // Note: The scaling is always based on the same origin and will not add up is multiple set.
       void setParameterScale(
-        // The new scaling of the parameter space
         const arma::Col<ParameterType> parameterScale);
 
-      // Sets the rotation which is multiplied to the parameter after the scaling.
-      // Note: The rotation is always based on the same origin and will not add up is multiple set.
       void setParameterRotation(
-        // The new rotation of the parameter space
         const arma::Mat<ParameterType> parameterRotation);
 
-      // Sets the translation (i.e. shift) which is added to the objective value after its
-      // computation.
-      // Note: The translation is always based on the same origin and will not add up is multiple
-      // set.
       void setObjectiveValueTranslation(
-        // The new translation of the objective value space
         const double objectiveValueTranslation) noexcept;
 
-      // Sets the scaling which is multiplied to the objective value after its translation.
-      // Note: The scaling is always based on the same origin and will not add up is multiple set.
       void setObjectiveValueScale(
-        // The new scaling of the objective value space
         const double objectiveValueScale) noexcept;
-
-      // Returns the upper limit for an objective value to be acceptable. The optimisation process
-      // is usually finished after reaching this threshold.
-      // Note: The objective value must not be reachable.
+        
       double getAcceptableObjectiveValue() const noexcept;
-
-      // Sets the upper limit for an objective value to be acceptable. The optimisation process
-      // is usually finished after reaching this threshold.
-      // Note: The objective value must not be reachable.
+      
       void setAcceptableObjectiveValue(
           const double acceptableObjectiveValue) noexcept;
 
-      // Returns the number of objective function evaluations.
-      // Note: The value includes retrival from cache and actual computation.
       unsigned int getNumberOfEvaluations() const noexcept;
 
-      // Returns the number of unique objective function evaluations.
-      // Note: This will only include actual computations and no cache retrival.
       unsigned int getNumberOfDistinctEvaluations() const noexcept;
 
-      // Resets the counter by setting the number of (distinct) function evaluations to zero and
-      // clearing the caches.
       void reset() noexcept;
 
       std::unordered_map<arma::Col<ParameterType>, double, Hash, IsEqual> getCachedObjectiveValues() const noexcept;
 
-      // Provides a default deconstructor.
       virtual ~OptimisationProblem() = default;
 
     protected:
-      // The lower bound of tthe search space
       arma::Col<ParameterType> lowerBounds_;
-      // The upper bound of the search space
       arma::Col<ParameterType> upperBounds_;
 
-      // The translation (shift) of the parameter space
       arma::Col<double> parameterTranslation_;
-      // The scaling of the parameter space
       arma::Col<double> parameterScale_;
-      // The rotation of the parameter space
       arma::Mat<double> parameterRotation_;
 
-      // The translation (shift) of the objective value
       double objectiveValueTranslation_;
-      // The scaling of the objective value
       double objectiveValueScale_;
 
-      // The upper limit for an objective value to be acceptable.
       double acceptableObjectiveValue_;
 
-      // The number of objective function evaluations so far (retrived from cache or computated).
       unsigned int numberOfEvaluations_;
-      // The number of distinct objective function evaluations so far (computated only).
       unsigned int numberOfDistinctEvaluations_;
 
-      // Returns the rotated, scaled and translated (shifted) parameter.
       arma::Col<ParameterType> getScaledCongruentParameter(
-        // The parameter to be rotated, scaled and translated (shifted)
         const arma::Col<ParameterType>& parameter) const noexcept;
 
-      // Actual implementaion of the soft-constraints function.
-      // Returns 0.0 if not overloaded.
       virtual double getSoftConstraintsValueImplementation(
-        // The parameter to be evaluated
         const arma::Col<ParameterType>& parameter) const noexcept;
 
-      // Actual implementaion of the objective function.
-      // Note: the number of dimensions of the parameter is checked beforehand.
       virtual double getObjectiveValueImplementation(
-        // The parameter to be evaluated
         const arma::Col<ParameterType>& parameter) const noexcept = 0;
 
-      // Several caches used to avoid redundant computations.
       std::unordered_map<arma::Col<ParameterType>, double, Hash, IsEqual> cachedObjectiveValues_;
 
  #if defined(MANTELLA_USE_PARALLEL)
-      // Gives cereal access to otherwise protected constructors, functions and fields.
       friend class cereal::access;
-      // Cereal uses a default constructor and will afterwards update the fields directly.
       OptimisationProblem() = default;
-
-      // The fields to be saved (serialised) and their corresponding name.
-      // The given name should always match the WHOLE parameter name (expect the suffix "_") to
-      // lessen confusion later on.
+      
       template <typename Archive>
       void serialize(
           Archive& archive) noexcept {
