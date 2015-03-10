a9236c2fbfc5ce2453e3c61ad0dc840d73d12e43
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-10 14:59:59 +0100

    Moved parameterisation into the katsuura function

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/katsuuraFunction.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/katsuuraFunction.hpp
@@ -2,12 +2,26 @@ namespace mant {
   namespace bbob2009 {
     class KatsuuraFunction : public BlackBoxOptimisationBenchmark2009 {
       public:
-        using BlackBoxOptimisationBenchmark2009::BlackBoxOptimisationBenchmark2009;
+        inline explicit KatsuuraFunction(
+            const unsigned int& numberOfDimensions) noexcept;
+
+        inline void setLocalParameterTranslation(
+            const arma::Col<double>& localParameterTranslation);
+
+        inline void setRotationR(
+            const arma::Mat<double>& rotationR);
+
+        inline void setRotationQ(
+            const arma::Mat<double>& rotationQ);
 
         inline std::string toString() const noexcept override;
 
       protected:
-        const arma::Col<double> delta_ = getScaling(std::sqrt(100.0));
+        const arma::Col<double> scaling_;
+
+        arma::Col<double> localParameterTranslation_;
+        arma::Mat<double> rotationR_;
+        arma::Mat<double> rotationQ_;
 
         inline double getObjectiveValueImplementation(
             const arma::Col<double>& parameter) const noexcept override;
@@ -20,7 +34,7 @@ namespace mant {
             Archive& archive) noexcept {
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(this)));
           archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
-          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("localParameterTranslation", localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", rotationR_));
           archive(cereal::make_nvp("rotationQ", rotationQ_));
         }
@@ -34,7 +48,7 @@ namespace mant {
           construct(numberOfDimensions);
 
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(construct.ptr())));
-          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("localParameterTranslation", construct->localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", construct->rotationR_));
           archive(cereal::make_nvp("rotationQ", construct->rotationQ_));
         }
@@ -45,9 +59,57 @@ namespace mant {
     // Implementation
     //
 
+    inline KatsuuraFunction::KatsuuraFunction(
+        const unsigned int& numberOfDimensions) noexcept
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions),
+        scaling_(getScaledTransformation(10.0)) {
+      setLocalParameterTranslation(getRandomLocalParameterTranslation());
+      setRotationR(getRandomRotationMatrix(numberOfDimensions_));
+      setRotationQ(getRandomRotationMatrix(numberOfDimensions_));
+    }
+
+    inline void KatsuuraFunction::setLocalParameterTranslation(
+        const arma::Col<double>& localParameterTranslation) {
+      if (localParameterTranslation.n_elem != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the local translation (" + std::to_string(localParameterTranslation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      }
+
+      localParameterTranslation_ = localParameterTranslation;
+    }
+
+    inline void KatsuuraFunction::setRotationR(
+        const arma::Mat<double>& rotationR) {
+      if (!rotationR.is_square()) {
+        throw std::logic_error("The rotation matrix's shape (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must be square.");
+      } else if (rotationR.n_rows != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the parameter rotation maxtrix (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      } else if(arma::any(arma::vectorise(arma::abs(rotationR.i() - rotationR.t()) > 1.0e-12 * std::max(1.0, std::abs(arma::median(arma::vectorise(rotationR))))))) {
+        throw std::logic_error("The rotation matrix must be orthonormal.");
+      } else if(std::abs(std::abs(arma::det(rotationR)) - 1.0) > 1.0e-12) {
+        throw std::logic_error("The rotation matrix's determinant (" + std::to_string(arma::det(rotationR)) + ") must be either 1 or -1.");
+      }
+
+      rotationR_ = rotationR;
+    }
+
+    inline void KatsuuraFunction::setRotationQ(
+        const arma::Mat<double>& rotationQ) {
+      if (!rotationQ.is_square()) {
+        throw std::logic_error("The rotation matrix's shape (" + std::to_string(rotationQ.n_rows) + ", " + std::to_string(rotationQ.n_cols) + ") must be square.");
+      } else if (rotationQ.n_rows != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the parameter rotation maxtrix (" + std::to_string(rotationQ.n_rows) + ", " + std::to_string(rotationQ.n_cols) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      } else if(arma::any(arma::vectorise(arma::abs(rotationQ.i() - rotationQ.t()) > 1.0e-12 * std::max(1.0, std::abs(arma::median(arma::vectorise(rotationQ))))))) {
+        throw std::logic_error("The rotation matrix must be orthonormal.");
+      } else if(std::abs(std::abs(arma::det(rotationQ)) - 1.0) > 1.0e-12) {
+        throw std::logic_error("The rotation matrix's determinant (" + std::to_string(arma::det(rotationQ)) + ") must be either 1 or -1.");
+      }
+
+      rotationQ_ = rotationQ;
+    }
+
     inline double KatsuuraFunction::getObjectiveValueImplementation(
         const arma::Col<double>& parameter) const noexcept {
-      arma::Col<double> z = rotationQ_ * (delta_ % (rotationR_ * (parameter - translation_)));
+      arma::Col<double> z = rotationQ_ * (scaling_ % (rotationR_ * (parameter - localParameterTranslation_)));
 
       double product = 1.0;
       for (std::size_t n = 0; n < z.n_elem; ++n) {
@@ -66,7 +128,7 @@ namespace mant {
     }
 
     inline std::string KatsuuraFunction::toString() const noexcept {
-      return "KatsuuraFunction";
+      return "katsuura-function";
     }
   }
 }
