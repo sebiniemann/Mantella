1816c99d3d69f4642d66c2229c16b9e45c52ed04
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-10 15:00:36 +0100

    Moved parameterisation into the schaffers f7 function  (ill conditioned)

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/schaffersF7FunctionIllConditioned.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/schaffersF7FunctionIllConditioned.hpp
@@ -2,12 +2,26 @@ namespace mant {
   namespace bbob2009 {
     class SchaffersF7FunctionIllConditioned : public BlackBoxOptimisationBenchmark2009 {
       public:
-        using BlackBoxOptimisationBenchmark2009::BlackBoxOptimisationBenchmark2009;
+        inline explicit SchaffersF7FunctionIllConditioned(
+            const unsigned int& numberOfDimensions) noexcept;
+
+        inline void setLocalParameterTranslation(
+            const arma::Col<double>& localParameterTranslation);
+
+        inline void setRotationR(
+            const arma::Mat<double>& rotationR);
+
+        inline void setRotationQ(
+            const arma::Mat<double>& rotationQ);
 
         inline std::string toString() const noexcept override;
 
       protected:
-        const arma::Col<double> delta_ = getScaling(std::sqrt(1000.0));
+        arma::Col<double> localParameterTranslation_;
+        arma::Mat<double> rotationR_;
+        arma::Mat<double> rotationQ_;
+
+        const arma::Col<double> scaling_;
 
         inline double getObjectiveValueImplementation(
             const arma::Col<double>& parameter) const noexcept override;
@@ -20,7 +34,7 @@ namespace mant {
             Archive& archive) noexcept {
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(this)));
           archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
-          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("localParameterTranslation", localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", rotationR_));
           archive(cereal::make_nvp("rotationQ", rotationQ_));
         }
@@ -34,7 +48,7 @@ namespace mant {
           construct(numberOfDimensions);
 
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(construct.ptr())));
-          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("localParameterTranslation", construct->localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", construct->rotationR_));
           archive(cereal::make_nvp("rotationQ", construct->rotationQ_));
         }
@@ -45,16 +59,64 @@ namespace mant {
     // Implementation
     //
 
+    inline SchaffersF7FunctionIllConditioned::SchaffersF7FunctionIllConditioned(
+        const unsigned int& numberOfDimensions) noexcept
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions),
+        scaling_(getScaledTransformation(std::sqrt(1000.0))) {
+      setLocalParameterTranslation(getRandomLocalParameterTranslation());
+      setRotationR(getRandomRotationMatrix(numberOfDimensions_));
+      setRotationQ(getRandomRotationMatrix(numberOfDimensions_));
+    }
+
+    inline void SchaffersF7FunctionIllConditioned::setLocalParameterTranslation(
+        const arma::Col<double>& localParameterTranslation) {
+      if (localParameterTranslation.n_elem != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the local translation (" + std::to_string(localParameterTranslation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      }
+
+      localParameterTranslation_ = localParameterTranslation;
+    }
+
+    inline void SchaffersF7FunctionIllConditioned::setRotationR(
+        const arma::Mat<double>& rotationR) {
+      if (!rotationR.is_square()) {
+        throw std::logic_error("The rotation matrix's shape (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must be square.");
+      } else if (rotationR.n_rows != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the parameter rotation maxtrix (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      } else if(arma::any(arma::vectorise(arma::abs(rotationR.i() - rotationR.t()) > 1.0e-12 * std::max(1.0, std::abs(arma::median(arma::vectorise(rotationR))))))) {
+        throw std::logic_error("The rotation matrix must be orthonormal.");
+      } else if(std::abs(std::abs(arma::det(rotationR)) - 1.0) > 1.0e-12) {
+        throw std::logic_error("The rotation matrix's determinant (" + std::to_string(arma::det(rotationR)) + ") must be either 1 or -1.");
+      }
+
+      rotationR_ = rotationR;
+    }
+
+    inline void SchaffersF7FunctionIllConditioned::setRotationQ(
+        const arma::Mat<double>& rotationQ) {
+      if (!rotationQ.is_square()) {
+        throw std::logic_error("The rotation matrix's shape (" + std::to_string(rotationQ.n_rows) + ", " + std::to_string(rotationQ.n_cols) + ") must be square.");
+      } else if (rotationQ.n_rows != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the parameter rotation maxtrix (" + std::to_string(rotationQ.n_rows) + ", " + std::to_string(rotationQ.n_cols) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      } else if(arma::any(arma::vectorise(arma::abs(rotationQ.i() - rotationQ.t()) > 1.0e-12 * std::max(1.0, std::abs(arma::median(arma::vectorise(rotationQ))))))) {
+        throw std::logic_error("The rotation matrix must be orthonormal.");
+      } else if(std::abs(std::abs(arma::det(rotationQ)) - 1.0) > 1.0e-12) {
+        throw std::logic_error("The rotation matrix's determinant (" + std::to_string(arma::det(rotationQ)) + ") must be either 1 or -1.");
+      }
+
+      rotationQ_ = rotationQ;
+    }
+
     inline double SchaffersF7FunctionIllConditioned::getObjectiveValueImplementation(
         const arma::Col<double>& parameter) const noexcept {
-      const arma::Col<double>& z = arma::square(delta_ % (rotationQ_ * getAsymmetricTransformation(0.5, rotationR_ * (parameter - translation_))));
+      const arma::Col<double>& z = arma::square(scaling_ % (rotationQ_ * getAsymmetricTransformation(0.5, rotationR_ * (parameter - localParameterTranslation_))));
       const arma::Col<double>& s = arma::pow(z.head(z.n_elem - 1) + z.tail(z.n_elem - 1), 0.25);
 
       return std::pow(arma::mean(s % (1.0 + arma::square(arma::sin(50.0 * arma::pow(s, 0.4))))), 2.0) + 10.0 * getPenality(parameter);
     }
 
     inline std::string SchaffersF7FunctionIllConditioned::toString() const noexcept {
-      return "SchaffersF7FunctionIllConditioned";
+      return "schaffers-f7-function-ill-conditioned";
     }
   }
 }
