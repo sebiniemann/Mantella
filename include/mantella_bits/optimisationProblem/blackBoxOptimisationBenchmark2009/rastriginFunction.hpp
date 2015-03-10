fcb63f8da5e00ea33c8f0f934e10920e98e42408
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-10 14:57:20 +0100

    Moved parameterisation into the rastrigin function

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/rastriginFunction.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/rastriginFunction.hpp
@@ -2,12 +2,18 @@ namespace mant {
   namespace bbob2009 {
     class RastriginFunction : public BlackBoxOptimisationBenchmark2009 {
       public:
-        using BlackBoxOptimisationBenchmark2009::BlackBoxOptimisationBenchmark2009;
+        inline explicit RastriginFunction(
+            const unsigned int& numberOfDimensions) noexcept;
+
+        inline void setLocalParameterTranslation(
+            const arma::Col<double>& localParameterTranslation);
 
         inline std::string toString() const noexcept override;
 
       protected:
-        const arma::Col<double> delta_ = getScaling(std::sqrt(10.0));
+        const arma::Col<double> scaling_;
+
+        arma::Col<double> localParameterTranslation_;
 
         inline double getObjectiveValueImplementation(
             const arma::Col<double>& parameter) const noexcept override;
@@ -41,15 +47,31 @@ namespace mant {
     // Implementation
     //
 
+    inline RastriginFunction::RastriginFunction(
+        const unsigned int& numberOfDimensions) noexcept
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions),
+        scaling_(getScaledTransformation(std::sqrt(10.0))) {
+      setLocalParameterTranslation(getRandomLocalParameterTranslation());
+    }
+
+    inline void RastriginFunction::setLocalParameterTranslation(
+        const arma::Col<double>& localParameterTranslation) {
+      if (localParameterTranslation.n_elem != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the local translation (" + std::to_string(localParameterTranslation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      }
+
+      localParameterTranslation_ = localParameterTranslation;
+    }
+
     inline double RastriginFunction::getObjectiveValueImplementation(
         const arma::Col<double>& parameter) const noexcept {
-      const arma::Col<double>& z = delta_ % getAsymmetricTransformation(0.2, getOscillationTransformation(parameter - translation_));
+      const arma::Col<double>& z = scaling_ % getAsymmetricTransformation(0.2, getOscillatedTransformation(parameter - localParameterTranslation_));
 
       return 10.0 * (static_cast<double>(numberOfDimensions_) - arma::accu(arma::cos(2.0 * arma::datum::pi * z))) + std::pow(arma::norm(z), 2.0);
     }
 
     inline std::string RastriginFunction::toString() const noexcept {
-      return "RastriginFunction";
+      return "rastrigin-function";
     }
   }
 }
