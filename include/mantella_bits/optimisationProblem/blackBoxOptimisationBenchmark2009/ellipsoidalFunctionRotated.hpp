7c4e76e9679df15211ba48211e96f2b3713d77d0
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-10 14:59:16 +0100

    Moved parameterisation into the ellipsoidal function (rotated)

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/ellipsoidalFunctionRotated.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/ellipsoidalFunctionRotated.hpp
@@ -2,12 +2,22 @@ namespace mant {
   namespace bbob2009 {
     class EllipsoidalFunctionRotated : public BlackBoxOptimisationBenchmark2009 {
       public:
-        using BlackBoxOptimisationBenchmark2009::BlackBoxOptimisationBenchmark2009;
+        inline explicit EllipsoidalFunctionRotated(
+            const unsigned int& numberOfDimensions) noexcept;
+
+        inline void setLocalParameterTranslation(
+            const arma::Col<double>& localParameterTranslation);
+
+        inline void setRotationR(
+            const arma::Mat<double>& rotationR);
 
         inline std::string toString() const noexcept override;
 
       protected:
-        const arma::Col<double> scaling_ = getScaling(1000000.0);
+        const arma::Col<double> scaling_;
+
+        arma::Col<double> localParameterTranslation_;
+        arma::Mat<double> rotationR_;
 
         inline double getObjectiveValueImplementation(
             const arma::Col<double>& parameter) const noexcept override;
@@ -20,7 +30,7 @@ namespace mant {
             Archive& archive) noexcept {
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(this)));
           archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
-          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("localParameterTranslation", localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", rotationR_));
         }
 
@@ -33,7 +43,7 @@ namespace mant {
           construct(numberOfDimensions);
 
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(construct.ptr())));
-          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("localParameterTranslation", construct->localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", construct->rotationR_));
         }
 #endif
@@ -43,13 +53,45 @@ namespace mant {
     // Implementation
     //
 
+    inline EllipsoidalFunctionRotated::EllipsoidalFunctionRotated(
+        const unsigned int& numberOfDimensions) noexcept
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions),
+        scaling_(getScaledTransformation(1000000.0)) {
+      setLocalParameterTranslation(getRandomLocalParameterTranslation());
+      setRotationR(getRandomRotationMatrix(numberOfDimensions_));
+    }
+
+    inline void EllipsoidalFunctionRotated::setLocalParameterTranslation(
+        const arma::Col<double>& localParameterTranslation) {
+      if (localParameterTranslation.n_elem != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the local translation (" + std::to_string(localParameterTranslation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      }
+
+      localParameterTranslation_ = localParameterTranslation;
+    }
+
+    inline void EllipsoidalFunctionRotated::setRotationR(
+        const arma::Mat<double>& rotationR) {
+      if (!rotationR.is_square()) {
+        throw std::logic_error("The rotation matrix's shape (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must be square.");
+      } else if (rotationR.n_rows != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the parameter rotation maxtrix (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      } else if(arma::any(arma::vectorise(arma::abs(rotationR.i() - rotationR.t()) > 1.0e-12 * std::max(1.0, std::abs(arma::median(arma::vectorise(rotationR))))))) {
+        throw std::logic_error("The rotation matrix must be orthonormal.");
+      } else if(std::abs(std::abs(arma::det(rotationR)) - 1.0) > 1.0e-12) {
+        throw std::logic_error("The rotation matrix's determinant (" + std::to_string(arma::det(rotationR)) + ") must be either 1 or -1.");
+      }
+
+      rotationR_ = rotationR;
+    }
+
     inline double EllipsoidalFunctionRotated::getObjectiveValueImplementation(
         const arma::Col<double>& parameter) const noexcept {
-      return arma::dot(scaling_, arma::square(getOscillationTransformation(rotationR_ * (parameter - translation_))));
+      return arma::dot(scaling_, arma::square(getOscillatedTransformation(rotationR_ * (parameter - localParameterTranslation_))));
     }
 
     inline std::string EllipsoidalFunctionRotated::toString() const noexcept {
-      return "EllipsoidalFunctionRotated";
+      return "ellipsoidal-function-rotated";
     }
   }
 }
