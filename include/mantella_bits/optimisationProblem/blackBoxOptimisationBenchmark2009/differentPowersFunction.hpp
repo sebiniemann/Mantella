ada9521a3c1ea955f010cbf6802406de2942da07
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-10 14:58:45 +0100

    Moved parameterisation into the different powers function

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/differentPowersFunction.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/differentPowersFunction.hpp
@@ -2,11 +2,21 @@ namespace mant {
   namespace bbob2009 {
     class DifferentPowersFunction : public BlackBoxOptimisationBenchmark2009 {
       public:
-        using BlackBoxOptimisationBenchmark2009::BlackBoxOptimisationBenchmark2009;
+        inline explicit DifferentPowersFunction(
+            const unsigned int& numberOfDimensions) noexcept;
+
+        inline void setLocalParameterTranslation(
+            const arma::Col<double>& localParameterTranslation);
+
+        inline void setRotationR(
+            const arma::Mat<double>& rotationR);
 
         inline std::string toString() const noexcept override;
 
       protected:
+        arma::Col<double> localParameterTranslation_;
+        arma::Mat<double> rotationR_;
+
         inline double getObjectiveValueImplementation(
             const arma::Col<double>& parameter) const noexcept override;
 
@@ -18,7 +28,7 @@ namespace mant {
             Archive& archive) noexcept {
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(this)));
           archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
-          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("localParameterTranslation", localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", rotationR_));
         }
 
@@ -31,7 +41,7 @@ namespace mant {
           construct(numberOfDimensions);
 
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(construct.ptr())));
-          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("localParameterTranslation", construct->localParameterTranslation_));
           archive(cereal::make_nvp("rotationR", construct->rotationR_));
         }
 #endif
@@ -41,14 +51,45 @@ namespace mant {
     // Implementation
     //
 
+    inline DifferentPowersFunction::DifferentPowersFunction(
+        const unsigned int& numberOfDimensions) noexcept
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions) {
+      setLocalParameterTranslation(getRandomLocalParameterTranslation());
+      setRotationR(getRandomRotationMatrix(numberOfDimensions_));
+    }
+
+    inline void DifferentPowersFunction::setLocalParameterTranslation(
+        const arma::Col<double>& localParameterTranslation) {
+      if (localParameterTranslation.n_elem != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the local translation (" + std::to_string(localParameterTranslation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      }
+
+      localParameterTranslation_ = localParameterTranslation;
+    }
+
+    inline void DifferentPowersFunction::setRotationR(
+        const arma::Mat<double>& rotationR) {
+      if (!rotationR.is_square()) {
+        throw std::logic_error("The rotation matrix's shape (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must be square.");
+      } else if (rotationR.n_rows != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the parameter rotation maxtrix (" + std::to_string(rotationR.n_rows) + ", " + std::to_string(rotationR.n_cols) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+      } else if(arma::any(arma::vectorise(arma::abs(rotationR.i() - rotationR.t()) > 1.0e-12 * std::max(1.0, std::abs(arma::median(arma::vectorise(rotationR))))))) {
+        throw std::logic_error("The rotation matrix must be orthonormal.");
+      } else if(std::abs(std::abs(arma::det(rotationR)) - 1.0) > 1.0e-12) {
+        throw std::logic_error("The rotation matrix's determinant (" + std::to_string(arma::det(rotationR)) + ") must be either 1 or -1.");
+      }
+
+      rotationR_ = rotationR;
+    }
+
     inline double DifferentPowersFunction::getObjectiveValueImplementation(
         const arma::Col<double>& parameter) const noexcept {
-      const arma::Col<double>& z = arma::abs(rotationR_ * (parameter - translation_));
-      return arma::norm(z % getScaling(arma::square(z)));
+      const arma::Col<double>& z = arma::abs(rotationR_ * (parameter - localParameterTranslation_));
+      return arma::norm(z % getScaledTransformation(arma::square(z)));
     }
 
     inline std::string DifferentPowersFunction::toString() const noexcept {
-      return "DifferentPowersFunction";
+      return "different-powers-function";
     }
   }
 }
