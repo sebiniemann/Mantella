1d82879ea03a658d0ce0c49d677940456919614a
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-10 14:58:27 +0100

    Moved parameterisation into the büche rastrigin function

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/buecheRastriginFunction.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/buecheRastriginFunction.hpp
@@ -7,11 +7,13 @@ namespace mant {
 
         inline std::string toString() const noexcept override;
 
-        inline void setTranslation(
-            const arma::Col<double> translation) override;
+        inline void setLocalParameterTranslation(
+            const arma::Col<double>& localParameterTranslation);
 
       protected:
-        const arma::Col<double> scaling_ = getScaling(std::sqrt(10.0));
+        const arma::Col<double> scaling_;
+
+        arma::Col<double> localParameterTranslation_;
 
         inline double getObjectiveValueImplementation(
             const arma::Col<double>& parameter) const noexcept override;
@@ -23,7 +25,7 @@ namespace mant {
             Archive& archive) noexcept {
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(this)));
           archive(cereal::make_nvp("numberOfDimensions", numberOfDimensions_));
-          archive(cereal::make_nvp("translation", translation_));
+          archive(cereal::make_nvp("localParameterTranslation", localParameterTranslation_));
         }
 
         template <typename Archive>
@@ -35,7 +37,7 @@ namespace mant {
           construct(numberOfDimensions);
 
           archive(cereal::make_nvp("BlackBoxOptimisationBenchmark2009", cereal::base_class<BlackBoxOptimisationBenchmark2009>(construct.ptr())));
-          archive(cereal::make_nvp("translation", construct->translation_));
+          archive(cereal::make_nvp("localParameterTranslation", construct->localParameterTranslation_));
         }
 #endif
     };
@@ -46,25 +48,26 @@ namespace mant {
 
     inline BuecheRastriginFunction::BuecheRastriginFunction(
         const unsigned int& numberOfDimensions) noexcept
-      : BlackBoxOptimisationBenchmark2009(numberOfDimensions) {
-      setTranslation(translation_);
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions),
+        scaling_(getScaledTransformation(std::sqrt(10.0))) {
+      setLocalParameterTranslation(getRandomLocalParameterTranslation());
     }
 
-    inline void BuecheRastriginFunction::setTranslation(
-        const arma::Col<double> translation) {
-      if (translation.n_elem != numberOfDimensions_) {
-        throw std::logic_error("The number of dimensions of the translation (" + std::to_string(translation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
+    inline void BuecheRastriginFunction::setLocalParameterTranslation(
+        const arma::Col<double>& localParameterTranslation) {
+      if (localParameterTranslation.n_elem != numberOfDimensions_) {
+        throw std::logic_error("The number of dimensions of the local translation (" + std::to_string(localParameterTranslation.n_elem) + ") must match the number of dimensions of the optimisation problem (" + std::to_string(numberOfDimensions_) + ").");
       }
 
-      translation_ = translation;
-      for (std::size_t n = 0; n < translation_.n_elem; n += 2) {
-        translation_(n) = std::abs(translation_(n));
+      localParameterTranslation_ = localParameterTranslation;
+      for (std::size_t n = 0; n < localParameterTranslation.n_elem; n += 2) {
+        localParameterTranslation_(n) = std::abs(localParameterTranslation(n));
       }
     }
 
     inline double BuecheRastriginFunction::getObjectiveValueImplementation(
         const arma::Col<double>& parameter) const noexcept {
-      arma::Col<double> z = scaling_ % getOscillationTransformation(parameter - translation_);
+      arma::Col<double> z = scaling_ % getOscillatedTransformation(parameter - localParameterTranslation_);
       for (std::size_t n = 0; n < z.n_elem; n += 2) {
         if (z(n) > 0.0) {
           z(n) *= 10.0;
@@ -75,7 +78,7 @@ namespace mant {
     }
 
     inline std::string BuecheRastriginFunction::toString() const noexcept {
-      return "BuecheRastriginFunction";
+      return "bueche-rastrigin-function";
     }
   }
 }
