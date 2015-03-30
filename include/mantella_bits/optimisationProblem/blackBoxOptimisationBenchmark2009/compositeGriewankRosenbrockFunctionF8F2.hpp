82e5255fec914bc37bd2b9da6706214994127479
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-03-30 15:19:38 +0200

    Use scaling and translation within the composite Griewank-Rosenbrock function f8f2

--- include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/compositeGriewankRosenbrockFunctionF8F2.hpp
+++ include/mantella_bits/optimisationProblem/blackBoxOptimisationBenchmark2009/compositeGriewankRosenbrockFunctionF8F2.hpp
@@ -40,8 +40,11 @@ namespace mant {
 
     inline CompositeGriewankRosenbrockFunctionF8F2::CompositeGriewankRosenbrockFunctionF8F2(
         const unsigned int numberOfDimensions) noexcept
-      : BlackBoxOptimisationBenchmark2009(numberOfDimensions),
-        max_(std::max(1.0, std::sqrt(static_cast<double>(numberOfDimensions_)) / 8.0)) {
+      : BlackBoxOptimisationBenchmark2009(numberOfDimensions) {
+      // A vector with all elements set to max(1, numberOfDimensions / 8).
+      setParameterScaling(arma::zeros<arma::Col<ParameterType>>(numberOfDimensions_) + std::max(1.0, std::sqrt(static_cast<double>(numberOfDimensions_)) / 8.0));
+      // A vector with all elements set to 0.5.
+      setParameterTranslation(arma::zeros<arma::Col<ParameterType>>(numberOfDimensions_) + 0.5);
       setParameterRotation(getRandomRotationMatrix(numberOfDimensions_));
     }
 
