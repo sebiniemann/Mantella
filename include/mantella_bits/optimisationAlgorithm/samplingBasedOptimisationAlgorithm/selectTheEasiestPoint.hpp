2e5d4ab49b818a23fe5d14022f063e9e5da5ae56
Wolff <wlfbck@gmail.com>
2015-04-12 15:42:58 +0200

    devel: formatting, renamed variables, added comments

--- include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm/selectTheEasiestPoint.hpp
+++ include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm/selectTheEasiestPoint.hpp
@@ -1,10 +1,10 @@
 namespace mant {
 
   template <typename ParameterType>
-  class selectTheEasiestPoint : public SamplingBasedOptimisationAlgorithm<ParameterType> {
+  class SelectTheEasiestPoint : public SamplingBasedOptimisationAlgorithm<ParameterType> {
   public:
-    explicit selectTheEasiestPoint(
-        const std::shared_ptr<OptimisationProblem<ParameterType>> optimisationProblem, unsigned int solvingDimension) noexcept;
+    explicit SelectTheEasiestPoint(
+        const std::shared_ptr<OptimisationProblem<double>> optimisationProblem, const unsigned int solvingDimension) noexcept;
 
     std::string toString() const noexcept override;
 
@@ -15,11 +15,11 @@ namespace mant {
 
     double lowerBound; //TODO: HCMA sets this to -5 in all cases... (xacmes.m 90)
     double upperBound; //TODO: HCMA sets this to 5 in all cases... (xacmes.m 90)
-    unsigned int solvingDimension; //ix in matlab. gets set to i-1 in mystepinit, than incremented by 1 in mystep initialization...
+    unsigned int solvingDimension; //ix in HCMA. gets set to i-1 in mystepinit, than incremented by 1 in mystep initialization...
     arma::Col<double> sol;
-    unsigned int nt = 1;
-    arma::Col<double> t;
-    arma::Col<double> ft;
+    unsigned int parameterCount = 1;
+    arma::Col<double> parameterHistory;
+    arma::Col<double> functionValueHistory;
   };
 
   //
@@ -30,93 +30,96 @@ namespace mant {
   //if a result is found, the specific value for the solvingDimension can be found in bestParameter(solvingDimension)
 
   template <typename ParameterType>
-  selectTheEasiestPoint<ParameterType>::selectTheEasiestPoint(
-      const std::shared_ptr<OptimisationProblem<ParameterType>> optimisationProblem, unsigned int solvingDimension) noexcept
+  SelectTheEasiestPoint<ParameterType>::SelectTheEasiestPoint(
+      const std::shared_ptr<OptimisationProblem<double>> optimisationProblem, const unsigned int solvingDimension) noexcept
   : SamplingBasedOptimisationAlgorithm<ParameterType>(optimisationProblem) {
+    std::cout << "we have lift off!" << std::endl;
     setSolvingDimension(solvingDimension);
 
     sol = arma::Col<double>(this->numberOfDimensions_);
-    t = arma::Col<double>(200);
-    ft = arma::Col<double>(200);
+    parameterHistory = arma::Col<double>(this->numberOfIterations_); //NOI should suffice as size, 
+    functionValueHistory = arma::Col<double>(this->numberOfIterations_);
 
     lowerBound = optimisationProblem->getLowerBounds()(solvingDimension);
     upperBound = optimisationProblem->getUpperBounds()(solvingDimension);
 
     sol = (lowerBound + (upperBound - lowerBound)) * arma::randu(this->numberOfDimensions_); //mySTEPinit 18
 
-    t(0) = lowerBound;
+    parameterHistory(0) = lowerBound;
     sol(solvingDimension) = lowerBound;
-    ft(0) = -this->getObjectiveValue(sol);
+    functionValueHistory(0) = -this->getObjectiveValue(sol);
     this->numberOfIterations_++;
     //this never gets done in "normal" STEP, but we need to do it
-    if (arma::max(ft) < this->bestObjectiveValue_) {
-      this->bestObjectiveValue_ = arma::max(ft);
+    if (arma::max(functionValueHistory) < this->bestObjectiveValue_) {
+      this->bestObjectiveValue_ = arma::max(functionValueHistory);
       this->bestParameter_ = sol;
     }
 
-    t(1) = upperBound;
+    parameterHistory(1) = upperBound;
     sol(solvingDimension) = upperBound;
-    ft(1) = -this->getObjectiveValue(sol);
+    functionValueHistory(1) = -this->getObjectiveValue(sol);
     this->numberOfIterations_++;
     //this never gets done in "normal" STEP, but we need to do it
-    if (arma::max(ft) < this->bestObjectiveValue_) {
-      this->bestObjectiveValue_ = arma::max(ft);
+    if (arma::max(functionValueHistory) < this->bestObjectiveValue_) {
+      this->bestObjectiveValue_ = arma::max(functionValueHistory);
       this->bestParameter_ = sol;
     }
   }
 
   template <typename ParameterType>
-  void selectTheEasiestPoint<ParameterType>::optimiseImplementation() noexcept {
+  void SelectTheEasiestPoint<ParameterType>::optimiseImplementation() noexcept {
+    unsigned int i1 = 0; //i1 and i2 are both used as indexes so their value is 1 less than in matlab
+    unsigned int i2 = 0;
+
     while (!this->isFinished() & !this->isTerminated()) {
-
-      unsigned int i1 = 0; //i1 and i2 are both used and indexes so their value is 1 less than in matlab
-      unsigned int i2 = 0;
-
-      double ftarget = arma::max(ft);
-      if (nt == 1) {
+      double ftarget = arma::max(functionValueHistory);
+      if (parameterCount == 1) {
         i1 = 0;
         i2 = 1;
       } else {
         //arma::Col<double> dx = t.rows(1, nt) - t.rows(0, nt - 1);
-        arma::Col<double> dy = ft.rows(1, nt) - ft.rows(0, nt - 1);
-        arma::Col<double> yhat = ftarget - ft.rows(0, nt - 1); //TODO: t/feps handling!!
+        arma::Col<double> dy = functionValueHistory.rows(1, parameterCount) - functionValueHistory.rows(0, parameterCount - 1);
+        arma::Col<double> yhat = ftarget - functionValueHistory.rows(0, parameterCount - 1); //TODO: t/feps handling!!
         arma::Col<double> D = 4 * yhat - 2 * dy + 4 * arma::sqrt(yhat % yhat - yhat % dy); //dividing everything by dx² omitted since pointless.
         i1 = arma::sort_index(D)(0);
         i2 = i1 + 1;
       }
-      nt++;
+      parameterCount++;
+      
       //may have to enlargen vectors
-      if (nt >= t.n_elem) {
-        t.resize(t.n_elem + 100);
-        ft.resize(ft.n_elem + 100);
+      if (parameterCount >= parameterHistory.n_elem) {
+        parameterHistory.resize(parameterHistory.n_elem + 100);
+        functionValueHistory.resize(functionValueHistory.n_elem + 100);
       }
 
-      t(nt) = t(i1) + 0.5 * (t(i2) - t(i1));
-      sol(solvingDimension) = t(nt);
-      ft(nt) = -this->getObjectiveValue(sol);
+      //setting up and calcing next vector
+      parameterHistory(parameterCount) = parameterHistory(i1) + 0.5 * (parameterHistory(i2) - parameterHistory(i1));
+      sol(solvingDimension) = parameterHistory(parameterCount);
+      functionValueHistory(parameterCount) = -this->getObjectiveValue(sol);
       this->numberOfIterations_++;
       //this never gets done in "normal" STEP, but we need to do it
-      if (arma::max(ft) < this->bestObjectiveValue_) {
-        this->bestObjectiveValue_ = arma::max(ft);
+      if (arma::max(functionValueHistory) < this->bestObjectiveValue_) {
+        this->bestObjectiveValue_ = arma::max(functionValueHistory);
         this->bestParameter_ = sol;
       }
 
-      arma::uvec sortIndexT = arma::sort_index(t);
-      t = arma::sort(t);
-      ft = ft.rows(sortIndexT);
+      //parameters (and thous function values) have to be sorted so dy calculation works in next iteration
+      arma::uvec sortIndexT = arma::sort_index(parameterHistory);
+      parameterHistory = arma::sort(parameterHistory);
+      functionValueHistory = functionValueHistory.rows(sortIndexT);
 
 
     }
   }
 
   template <typename ParameterType>
-  void selectTheEasiestPoint<ParameterType>::setSolvingDimension(const unsigned int solvingDimension) {
+  void SelectTheEasiestPoint<ParameterType>::setSolvingDimension(const unsigned int solvingDimension) {
     this->solvingDimension = solvingDimension;
   }
 
   template <typename ParameterType>
-  std::string selectTheEasiestPoint<ParameterType>::toString() const noexcept {
-    return "selectTheEasiestPoint";
+  std::string SelectTheEasiestPoint<ParameterType>::toString() const noexcept {
+    return "SelectTheEasiestPoint";
   }
 }
 
