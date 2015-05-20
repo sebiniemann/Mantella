d80e42e2ad7dea328fef3afa02bdd492e73e2407
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-05-20 15:37:41 +0200

    Fixed tests

--- test/helper/testUnorderedContainer.cpp
+++ test/helper/testUnorderedContainer.cpp
@@ -12,7 +12,7 @@ TEST_CASE("unorderedContainer: Hash(arma::Col<double>)", "") {
     std::unordered_set<std::size_t> hashes;
 
     unsigned int numberOfCollisions = 0;
-    mant::Hash hash;
+    mant::Hash<double> hash;
     for(unsigned int n = 0; n < 10000; ++n) {
       auto result = hashes.insert(hash(arma::randu<arma::Col<double>>(5)));
       if(!result.second) {
@@ -29,7 +29,7 @@ TEST_CASE("unorderedContainer: Hash(arma::Col<unsigned int>)", "") {
     std::unordered_set<std::size_t> hashes;
 
     unsigned int numberOfCollisions = 0;
-    mant::Hash hash;
+    mant::Hash<unsigned int> hash;
     for(unsigned int n = 0; n < 10000; ++n) {
       auto result = hashes.insert(hash(arma::randi<arma::Col<unsigned int>>(5)));
       if(!result.second) {
@@ -43,36 +43,36 @@ TEST_CASE("unorderedContainer: Hash(arma::Col<unsigned int>)", "") {
 
 TEST_CASE("unorderedContainer: IsKeyEqual(arma::Col<double>)", "") {
   SECTION("Returns true if both vectors are equal.") {
-    mant::IsEqual isKeyEqual;
-    CHECK(isKeyEqual(arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4})) == true);
+    mant::IsEqual<double> isEqual;
+    CHECK(isEqual(arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4})) == true);
   }
 
   SECTION("Returns false if not all elements within the two vectors are equal.") {
-    mant::IsEqual isKeyEqual;
-    CHECK(isKeyEqual(arma::Col<double>::fixed<5>({1.0, 3.9, 5.5, -4.4}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4})) == false);
+    mant::IsEqual<double> isEqual;
+    CHECK(isEqual(arma::Col<double>::fixed<5>({1.0, 3.9, 5.5, -4.4}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4})) == false);
   }
 
   SECTION("Returns false if the size of the two vectors differ.") {
-    mant::IsEqual isKeyEqual;
-    CHECK(isKeyEqual(arma::Col<double>::fixed<5>({1.0, 0.0, 6.5}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4})) == false);
-    CHECK(isKeyEqual(arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5})) == false);
+    mant::IsEqual<double> isEqual;
+    CHECK(isEqual(arma::Col<double>::fixed<5>({1.0, 0.0, 6.5}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4})) == false);
+    CHECK(isEqual(arma::Col<double>::fixed<5>({1.0, 0.0, 6.5, -312.4}), arma::Col<double>::fixed<5>({1.0, 0.0, 6.5})) == false);
   }
 }
 
 TEST_CASE("unorderedContainer: IsKeyEqual(arma::Col<unsigned int>)", "") {
   SECTION("Returns true if both vectors are equal.") {
-    mant::IsEqual isKeyEqual;
-    CHECK(isKeyEqual(arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203})) == true);
+    mant::IsEqual<unsigned int> isEqual;
+    CHECK(isEqual(arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203})) == true);
   }
 
   SECTION("Returns false if not all elements within the two vectors are equal.") {
-    mant::IsEqual isKeyEqual;
-    CHECK(isKeyEqual(arma::Col<unsigned int>::fixed<5>({41, 2, 34, 82, 78}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203})) == false);
+    mant::IsEqual<unsigned int> isEqual;
+    CHECK(isEqual(arma::Col<unsigned int>::fixed<5>({41, 2, 34, 82, 78}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203})) == false);
   }
 
   SECTION("Returns false if the size of the two vectors differ.") {
-    mant::IsEqual isKeyEqual;
-    CHECK(isKeyEqual(arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203})) == false);
-    CHECK(isKeyEqual(arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8})) == false);
+    mant::IsEqual<unsigned int> isEqual;
+    CHECK(isEqual(arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203})) == false);
+    CHECK(isEqual(arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8, 1203}), arma::Col<unsigned int>::fixed<5>({1, 234, 5, 8})) == false);
   }
 }
--- test/testDistanceFunction.cpp
+++ test/testDistanceFunction.cpp
@@ -9,91 +9,32 @@
 
 #include "./helper.hpp"
 
-template <typename ParameterType>
-class MockDistanceFunction : public mant::DistanceFunction<ParameterType>{
+template <typename T>
+class MockDistanceFunction : public mant::DistanceFunction<T>{
   public:
-    ParameterType getDistanceImplementation(
-        const arma::Col<ParameterType>& parameter) const noexcept override;
+    T getLengthImplementation(
+        const arma::Col<T>& parameter) const noexcept override;
 
-    arma::Col<ParameterType> getRandomNeighbourImplementation(
-        const arma::Col<ParameterType>& parameter,
-        const ParameterType minimalDistance,
-        const ParameterType maximalDistance) const noexcept override;
+    arma::Col<T> getRandomNeighbourImplementation(
+        const arma::Col<T>& parameter,
+        const T minimalDistance,
+        const T maximalDistance) const noexcept override;
 };
 
-template <>
-double MockDistanceFunction<double>::getDistanceImplementation(
-    const arma::Col<double>& parameter) const noexcept {
-  return arma::norm(parameter, 1);
-}
-
-template <>
-unsigned int MockDistanceFunction<unsigned int>::getDistanceImplementation(
-    const arma::Col<unsigned int>& parameter) const noexcept {
+template <typename T>
+T MockDistanceFunction<T>::getLengthImplementation(
+    const arma::Col<T>& parameter) const noexcept {
   return arma::accu(parameter);
 }
 
-template <>
-arma::Col<double> MockDistanceFunction<double>::getRandomNeighbourImplementation(
-    const arma::Col<double>& parameter,
-    const double minimalDistance,
-    const double maximalDistance) const noexcept {
-  arma::Col<double> result = parameter;
-  result.at(0) += (maximalDistance + minimalDistance) / 2.0;
-  return result;
-}
-
-template <>
-arma::Col<unsigned int> MockDistanceFunction<unsigned int>::getRandomNeighbourImplementation(
-    const arma::Col<unsigned int>& parameter,
-    const unsigned int minimalDistance,
-    const unsigned int maximalDistance) const noexcept {
-  arma::Col<unsigned int> result = parameter;
+template <typename T>
+arma::Col<T> MockDistanceFunction<T>::getRandomNeighbourImplementation(
+    const arma::Col<T>& parameter,
+    const T minimalDistance,
+    const T maximalDistance) const noexcept {
+  arma::Col<T> result = parameter;
   result.at(0) += std::floor((maximalDistance + minimalDistance) / 2.0);
   return result;
-}
-
-TEST_CASE("DistanceFunction<double>", "") {
-  MockDistanceFunction<double> distanceFunction;
-
-  SECTION("Testing getRandomNeighbour(..)") {
-    SECTION("Selected random neighbour remains unchanged, considering an unrestricted minimal distance.") {
-      compare(distanceFunction.getRandomNeighbour(arma::zeros<arma::Col<double>>(1), 0.0, 2.3), distanceFunction.getRandomNeighbourImplementation(arma::zeros<arma::Col<double>>(1), 0.0, 2.3));
-      compare(distanceFunction.getRandomNeighbour(arma::ones<arma::Col<double>>(5), 0.0, 11.32), distanceFunction.getRandomNeighbourImplementation(arma::ones<arma::Col<double>>(5), 0.0, 11.32));
-    }
-
-    SECTION("Selected random neighbour remains unchanged, considering an unrestricted minimal distance.") {
-      compare(distanceFunction.getRandomNeighbour(arma::zeros<arma::Col<double>>(1), 0.5, 2.3), distanceFunction.getRandomNeighbourImplementation(arma::zeros<arma::Col<double>>(1), 0.5, 2.3));
-      compare(distanceFunction.getRandomNeighbour(arma::ones<arma::Col<double>>(5), 5.4, 9.31), distanceFunction.getRandomNeighbourImplementation(arma::ones<arma::Col<double>>(5), 5.4, 9.31));
-    }
-
-    SECTION("Selected random neighbour remains unchanged, considering an equal minimal and maximal distance.") {
-      compare(distanceFunction.getRandomNeighbour(arma::zeros<arma::Col<double>>(1), 3.0, 3.0), distanceFunction.getRandomNeighbourImplementation(arma::zeros<arma::Col<double>>(1), 3.0, 3.0));
-      compare(distanceFunction.getRandomNeighbour(arma::ones<arma::Col<double>>(5), 6.14, 6.14), distanceFunction.getRandomNeighbourImplementation(arma::ones<arma::Col<double>>(5), 6.14, 6.14));
-    }
-
-    SECTION("Throws an exception, if minimal distance < 0.") {
-      arma::Col<double>::fixed<5> parameter = arma::zeros<arma::Col<double>>(5);
-      CHECK_THROWS_AS(distanceFunction.getRandomNeighbour(parameter, -10.3, 4.0), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if maximal distance < 0.") {
-      arma::Col<double>::fixed<5> parameter = arma::zeros<arma::Col<double>>(5);
-      CHECK_THROWS_AS(distanceFunction.getRandomNeighbour(parameter, -8.3, -4.0), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if minimal distance > maximal distance.") {
-      arma::Col<double>::fixed<5> parameter = arma::zeros<arma::Col<double>>(5);
-      CHECK_THROWS_AS(distanceFunction.getRandomNeighbour(parameter, 10.3, 4.0), std::logic_error);
-    }
-  }
-
-  SECTION("Testing getDistance(...)") {
-    SECTION("Distance value remains unchanged.") {
-      CHECK(distanceFunction.getDistance(arma::zeros<arma::Col<double>>(1)) == distanceFunction.getDistanceImplementation(arma::zeros<arma::Col<double>>(1)));
-      CHECK(distanceFunction.getDistance(arma::Col<double>::fixed<5>({-1.2, 3.0, 1.0, -4.0, 0.0})) == distanceFunction.getDistanceImplementation(arma::Col<double>::fixed<5>({-1.2, 3.0, 1.0, -4.0, 0.0})));
-    }
-  }
 }
 
 TEST_CASE("DistanceFunction<unsigned int>", "") {
@@ -117,14 +58,14 @@ TEST_CASE("DistanceFunction<unsigned int>", "") {
 
     SECTION("Throws an exception, if minimal distance > maximal distance.") {
       arma::Col<unsigned int>::fixed<5> parameter = arma::zeros<arma::Col<unsigned int>>(5);
-      CHECK_THROWS_AS(distanceFunction.getRandomNeighbour(parameter, 10, 4), std::logic_error);
+      CHECK_THROWS_AS(distanceFunction.getRandomNeighbour(parameter, 10, 4), std::runtime_error);
     }
   }
 
   SECTION("Testing getDistance(...)") {
     SECTION("Distance value remains unchanged.") {
-      CHECK(distanceFunction.getDistance(arma::zeros<arma::Col<unsigned int>>(1)) == distanceFunction.getDistanceImplementation(arma::zeros<arma::Col<unsigned int>>(1)));
-      CHECK(distanceFunction.getDistance(arma::Col<unsigned int>::fixed<5>({1, 3, 1, 4, 0})) == distanceFunction.getDistanceImplementation(arma::Col<unsigned int>::fixed<5>({1, 3, 1, 4, 0})));
+      CHECK(distanceFunction.getLength(arma::zeros<arma::Col<unsigned int>>(1)) == 0);
+      CHECK(distanceFunction.getLength(arma::Col<unsigned int>::fixed<5>({1, 3, 1, 4, 0})) == 9);
     }
   }
 }
