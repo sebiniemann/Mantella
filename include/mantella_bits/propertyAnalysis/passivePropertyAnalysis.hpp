5aad9f5584b906aeb3c53bf7a4f35e45eaf45554
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-05-29 12:55:36 +0200

    Templating the codomain

--- include/mantella_bits/propertyAnalysis/passivePropertyAnalysis.hpp
+++ include/mantella_bits/propertyAnalysis/passivePropertyAnalysis.hpp
@@ -1,24 +1,25 @@
 namespace mant {
-  template <typename T>
-  class PassivePropertyAnalysis : public PropertyAnalysis<T> {
+  template <typename T, typename U = double>
+  class PassivePropertyAnalysis : public PropertyAnalysis<T, U> {
+    
     public:
-      using PropertyAnalysis<T>::PropertyAnalysis;
+      using PropertyAnalysis<T, U>::PropertyAnalysis;
 
       void analyse(
-          const std::unordered_map<arma::Col<double>, double, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMapping) noexcept;
+          const std::unordered_map<arma::Col<T>, U, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMapping) noexcept;
 
     protected:
       virtual void analyseImplementation(
-          const std::unordered_map<arma::Col<double>, double, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMappings) noexcept = 0;
+          const std::unordered_map<arma::Col<T>, U, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMappings) noexcept = 0;
   };
 
   //
   // Implementation
   //
 
-  template <typename T>
-  void PassivePropertyAnalysis<T>::analyse(
-      const std::unordered_map<arma::Col<double>, double, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMapping) noexcept {
+  template <typename T, typename U>
+  void PassivePropertyAnalysis<T, U>::analyse(
+      const std::unordered_map<arma::Col<T>, U, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMapping) noexcept {
     analyseImplementation(parameterToObjectiveValueMapping);
   }
 }
