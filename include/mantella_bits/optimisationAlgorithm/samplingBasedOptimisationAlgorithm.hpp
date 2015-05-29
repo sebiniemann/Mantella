5699f3c1d7a4a14ca4ba85b3f4c935bfe10073d1
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-05-29 13:13:30 +0200

    WIP

--- include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm.hpp
+++ include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm.hpp
@@ -1,7 +1,10 @@
 namespace mant {
-  template <typename T>
-  class SamplingBasedOptimisationAlgorithm : public OptimisationAlgorithm<T> {
+  template <typename T, typename U = double>
+  class SamplingBasedOptimisationAlgorithm : public OptimisationAlgorithm<T, U> {
+    static_assert(std::is_arithmetic<T>::value, "T must be an arithmetic type.");
+    static_assert(std::is_arithmetic<U>::value, "U must be an arithmetic type.");
+    
     public:
-      using OptimisationAlgorithm<T>::OptimisationAlgorithm;
+      using OptimisationAlgorithm<T, U>::OptimisationAlgorithm;
   };
 }
--- include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm/randomSearch.hpp
+++ include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm/randomSearch.hpp
@@ -1,9 +1,8 @@
 namespace mant {
-  template <typename T>
-  class RandomSearch : public SamplingBasedOptimisationAlgorithm<T> {
+  template <typename T = double>
+  class RandomSearch : public SamplingBasedOptimisationAlgorithm<T, T> {
     public:
-      explicit RandomSearch(
-          const std::shared_ptr<OptimisationProblem<T>> optimisationProblem) noexcept;
+      using SamplingBasedOptimisationAlgorithm<T, T>::SamplingBasedOptimisationAlgorithm;
 
       std::string toString() const noexcept override;
 
@@ -16,31 +15,17 @@ namespace mant {
   //
 
   template <typename T>
-  RandomSearch<T>::RandomSearch(
-      const std::shared_ptr<OptimisationProblem<T>> optimisationProblem) noexcept
-    : SamplingBasedOptimisationAlgorithm<T>(optimisationProblem) {
-
-  }
-
-  template <typename T>
   void RandomSearch<T>::optimiseImplementation() noexcept {
     while(!this->isFinished() && !this->isTerminated()) {
       ++this->numberOfIterations_;
-
-      const arma::Col<T>& candidateParameter = this->distanceFunction_->getRandomNeighbour(arma::zeros<arma::Col<double>>(this->numberOfDimensions_), this->getLowerBounds(), this->getUpperBounds() - this->getLowerBounds());
-      const double& candidateSoftConstraintsValue = this->getSoftConstraintsValue(candidateParameter);
-      const double& candidateObjectiveValue = this->getObjectiveValue(candidateParameter);
-
-      if(candidateSoftConstraintsValue < this->bestSoftConstraintsValue_ || (candidateSoftConstraintsValue == this->bestSoftConstraintsValue_ && candidateObjectiveValue < this->bestObjectiveValue_)) {
-        this->bestParameter_ = candidateParameter;
-        this->bestSoftConstraintsValue_ = candidateSoftConstraintsValue;
-        this->bestObjectiveValue_ = candidateObjectiveValue;
-      }
+    
+      const arma::Col<T>& candidateParameter = this->getRandomParameter();
+      updateBestParameter(candidateParameter, this->getSoftConstraintsValue(candidateParameter), this->getObjectiveValue(candidateParameter));
     };
   }
 
   template <typename T>
   std::string RandomSearch<T>::toString() const noexcept {
-    return "RandomSearch";
+    return "random_search";
   }
 }
