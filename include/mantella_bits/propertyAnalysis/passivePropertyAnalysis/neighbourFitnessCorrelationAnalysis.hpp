5ce02a53fd9193abb0783d60319a4e0a3d839793
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-06-01 00:32:55 +0200

    Added support of NeighbourFitnessCorrelationAnalysis

--- include/mantella_bits/propertyAnalysis/passivePropertyAnalysis/neighbourFitnessCorrelationAnalysis.hpp
+++ include/mantella_bits/propertyAnalysis/passivePropertyAnalysis/neighbourFitnessCorrelationAnalysis.hpp
@@ -41,21 +41,36 @@ namespace mant {
   void NeighbourFitnessCorrelationAnalysis<T, U>::analyseImplementation(
       const std::unordered_map<arma::Col<T>, U, Hash<T>, IsEqual<T>>& parameterToObjectiveValueMappings) noexcept {
     arma::Mat<T> parameters(parameterToObjectiveValueMappings.cbegin()->first.n_elem, parameterToObjectiveValueMappings.size());
-    arma::Col<U> objectiveValues(parameterToObjectiveValueMappings.size());
-
-    std::size_t n = 0;
-    for (const auto& parameterToObjectiveValueMapping : parameterToObjectiveValueMappings) {
-      parameters.col(n) = parameterToObjectiveValueMapping.first;
-      objectiveValues(n) = parameterToObjectiveValueMapping.second;
-      ++n;
+    
+    arma::Col<U> objectiveValues(parameters.n_cols);
+    arma::Mat<unsigned int> neighbours(parameters.n_cols, parameters.n_cols);
+    
+    std::size_t nn = 0;
+    for (auto n = parameterToObjectiveValueMappings.cbegin(); n != parameterToObjectiveValueMappings.cend();) {
+    
+      const arma::Col<T>& parameter = n->first;
+      objectiveValues(nn) = n->second;
+      
+      std::size_t kk = 0;
+      for (auto k = ++n; k != parameterToObjectiveValueMappings.cend(); ++k) {
+        if (std::is_integral<T>::value) {
+          neighbours(nn, kk) = arma::find(this->distanceFunction_->getDistance(parameter, k->first) <= maximalNeighbourDistance_);
+        } else {
+          neighbours(nn, kk) = arma::find(arma::norm(k->first - parameter) <= maximalNeighbourDistance_); 
+        }
+        
+        ++kk;
+      }
+      ++nn;
+    }
+    neighbours = arma::symmatu(neighbours);
+    
+    arma::Col<U> medianNeighbourObjectiveValues(objectiveValues.n_elem);
+    for (std::size_t n = 0; n < objectiveValues.n_elem; ++n) {
+      medianNeighbourObjectiveValues(n) = arma::median(objectiveValues.elem(neighbours.col(n)));
     }
 
-    unsigned int bestParameterIndex;
-    objectiveValues.min(bestParameterIndex);
-
-    parameters.each_col() -= parameters.col(bestParameterIndex);
-
-    correlationCoefficient_ = arma::as_scalar(arma::cor(arma::sqrt(arma::sum(arma::square(parameters))), objectiveValues));
+    correlationCoefficient_ = arma::as_scalar(arma::cor(objectiveValues, medianNeighbourObjectiveValues));
   }
 
   template <typename T, typename U>
