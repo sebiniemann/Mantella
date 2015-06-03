5b3797dca07a034562d63b2ccd262fea1ecbbe13
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-06-04 01:38:00 +0200

    Rearranged the conditions and improved some comments

--- include/mantella_bits/helper/assert.hpp
+++ include/mantella_bits/helper/assert.hpp
@@ -27,13 +27,16 @@ namespace mant {
   template <typename T>
   bool isRotationMatrix(
       const arma::Mat<T>& parameter) noexcept {
-    // is suqare?
+    // Is the rotation matrix square?
     if (!parameter.is_square()) {
       return false;
-    // is orthonormal?
+    }
+      
+    // Is its determinant either 1 or -1?
+    if(std::abs(std::abs(arma::det(parameter)) - 1.0) > 1.0e-12) {
       return false;
-    // determinant is either 1 or -1?
-    } else if(std::abs(std::abs(arma::det(parameter)) - 1.0) > 1.0e-12) {
+    }
+      
     // Is the rotation matrix square?
     // For (nearly) singular matrices, the inversion might throw an exception.
     try {
