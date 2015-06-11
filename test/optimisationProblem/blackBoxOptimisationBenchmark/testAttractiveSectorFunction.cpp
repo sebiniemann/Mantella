989d1243582709d946915534784d5c869872a71b
dkiechle <kiechle@sra.uni-hannover.de>
2015-06-11 15:46:53 +0200

    Updated code format

--- test/optimisationProblem/blackBoxOptimisationBenchmark/testAttractiveSectorFunction.cpp
+++ test/optimisationProblem/blackBoxOptimisationBenchmark/testAttractiveSectorFunction.cpp
@@ -18,10 +18,10 @@ TEST_CASE("bbob::AttractiveSectorFunction", "") {
     mant::bbob::AttractiveSectorFunction<> attractiveSectorFunction(numberOfDimensions);
 
     arma::Mat<double> parameters;
-    CHECK( parameters.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/_parameters_" + std::to_string(numberOfDimensions) +"x10.input") );
+    CHECK( parameters.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/_parameters_" + std::to_string(numberOfDimensions) + "x10.input") );
 
     arma::Col<double> translation;
-    CHECK( translation.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/_translation_" + std::to_string(numberOfDimensions) +"x1.input") );
+    CHECK( translation.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/_translation_" + std::to_string(numberOfDimensions) + "x1.input") );
 
     arma::Mat<double> rotationR;
     CHECK( rotationR.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/_randomRotationMatrix_" + std::to_string(numberOfDimensions) + "x" + std::to_string(numberOfDimensions) + "_2.input") );
@@ -30,7 +30,7 @@ TEST_CASE("bbob::AttractiveSectorFunction", "") {
     CHECK( rotationQ.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/_randomRotationMatrix_" + std::to_string(numberOfDimensions) + "x" + std::to_string(numberOfDimensions) + "_1.input") );
 
     arma::Col<double> expected;
-    CHECK( expected.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/bbob_attractiveSectorFunction_dim" + std::to_string(numberOfDimensions) +".expected") );
+    CHECK( expected.load(testDirectory + "/data/optimisationProblem/blackBoxOptimisationBenchmark/bbob_attractiveSectorFunction_dim" + std::to_string(numberOfDimensions) + ".expected") );
 
     attractiveSectorFunction.setObjectiveValueTranslation(0);
     attractiveSectorFunction.setParameterTranslation(translation);
