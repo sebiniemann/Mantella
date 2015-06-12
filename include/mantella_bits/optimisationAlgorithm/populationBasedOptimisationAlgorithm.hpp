c50e4e6e6e588f0c226dfad219c96e57de7cba42
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-06-12 13:36:11 +0200

    Restricted U to std::is_floating_point

--- include/mantella_bits/optimisationAlgorithm/populationBasedOptimisationAlgorithm.hpp
+++ include/mantella_bits/optimisationAlgorithm/populationBasedOptimisationAlgorithm.hpp
@@ -2,7 +2,7 @@ namespace mant {
   template <typename T, typename U = double>
   class PopulationBasedOptimisationAlgorithm : public OptimisationAlgorithm<T> {
     static_assert(arma::is_supported_elem_type<T>::value, "The parameter type T must be supported by Armadillo C++, i.e. an arithmetic types, except bool.");
-    static_assert(std::is_arithmetic<U>::value, "U must be an arithmetic type.");
+    static_assert(std::is_floating_point<U>::value, "The codomain type U must be a floating point type.");
     
     public:
       template <typename V = T, typename std::enable_if<std::is_floating_point<V>::value, bool>::type = false>
