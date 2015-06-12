4fa254c5b18bfab8ef634d0ffe7836fda4f2fbb3
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-06-12 13:36:30 +0200

    Improved error message

--- include/mantella_bits/propertyAnalysis/activePropertyAnalysis.hpp
+++ include/mantella_bits/propertyAnalysis/activePropertyAnalysis.hpp
@@ -2,7 +2,7 @@ namespace mant {
   template <typename T, typename U = double>
   class ActivePropertyAnalysis : public PropertyAnalysis<T, U> {
     static_assert(arma::is_supported_elem_type<T>::value, "The parameter type T must be supported by Armadillo C++, i.e. an arithmetic types, except bool.");
-    static_assert(std::is_floating_point<U>::value, "U must be a floating point type.");
+    static_assert(std::is_floating_point<U>::value, "The codomain type U must be a floating point type.");
     
     public:
       using PropertyAnalysis<T, U>::PropertyAnalysis;
--- include/mantella_bits/propertyAnalysis/passivePropertyAnalysis/fitnessDistanceCorrelationAnalysis.hpp
+++ include/mantella_bits/propertyAnalysis/passivePropertyAnalysis/fitnessDistanceCorrelationAnalysis.hpp
@@ -2,7 +2,7 @@ namespace mant {
   template <typename T, typename U = double>
   class FitnessDistanceCorrelationAnalysis : public PassivePropertyAnalysis<T, U> {
     static_assert(arma::is_supported_elem_type<T>::value, "The parameter type T must be supported by Armadillo C++, i.e. an arithmetic types, except bool.");
-    static_assert(std::is_floating_point<U>::value, "U must be a floating point type.");
+    static_assert(std::is_floating_point<U>::value, "The codomain type U must be a floating point type.");
     
     public:
       using PassivePropertyAnalysis<T, U>::PassivePropertyAnalysis;
