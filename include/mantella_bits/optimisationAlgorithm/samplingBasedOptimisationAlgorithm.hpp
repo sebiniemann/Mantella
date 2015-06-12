3d6a5215e05433ece9753c91d62458e55eb0049a
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-06-12 13:35:04 +0200

    Restricted T to arma::is_supported_elem_type

--- include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm.hpp
+++ include/mantella_bits/optimisationAlgorithm/samplingBasedOptimisationAlgorithm.hpp
@@ -1,7 +1,7 @@
 namespace mant {
   template <typename T, typename U = double>
   class SamplingBasedOptimisationAlgorithm : public OptimisationAlgorithm<T, U> {
-    static_assert(std::is_arithmetic<T>::value, "T must be an arithmetic type.");
+    static_assert(arma::is_supported_elem_type<T>::value, "The parameter type T must be supported by Armadillo C++, i.e. an arithmetic types, except bool.");
     static_assert(std::is_arithmetic<U>::value, "U must be an arithmetic type.");
     
     public:
