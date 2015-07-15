c43a2a54d26e494878526ea019fb6cf50c57fcb8
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-07-15 13:41:21 +0200

    Fixed namespace

--- include/mantella_bits/helper/printable.hpp
+++ include/mantella_bits/helper/printable.hpp
@@ -3,16 +3,16 @@ namespace mant {
     public:
       virtual std::string toString() const noexcept = 0;
   };
-}
 
-inline std::string to_string(
-    const std::shared_ptr<Printable> printable) noexcept;
+  inline std::string to_string(
+      const std::shared_ptr<Printable> printable) noexcept;
 
-//
-// Implementation
-//
+  //
+  // Implementation
+  //
 
-inline std::string to_string(
-    const std::shared_ptr<Printable> printable) noexcept {
-  return printable->toString();
+  inline std::string to_string(
+      const std::shared_ptr<Printable> printable) noexcept {
+    return printable->toString();
+  }
 }
