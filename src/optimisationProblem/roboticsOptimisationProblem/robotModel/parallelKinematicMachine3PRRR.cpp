ada1055bc5d6d51fbd2a41a0a7a94cd5f9edad61
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-18 15:25:28 +0200

    Fixed CID 136473 Explicit null dereferenced

--- src/optimisationProblem/roboticsOptimisationProblem/robotModel/parallelKinematicMachine3PRRR.cpp
+++ src/optimisationProblem/roboticsOptimisationProblem/robotModel/parallelKinematicMachine3PRRR.cpp
@@ -57,7 +57,7 @@ namespace mant {
       assert(redundantJointsActuation.n_elem == numberOfRedundantJoints_);
       assert(!arma::any(redundantJointsActuation < 0) && !arma::any(redundantJointsActuation > 1));
 
-      arma::Cube<double> model(2, 3, 3);
+      arma::Cube<double>::fixed<2, 3, 3> model;
 
       const arma::Col<double>& endEffectorPosition = endEffectorPose.subvec(0, 1);
       const double& endEffectorAngle = endEffectorPose(2);
