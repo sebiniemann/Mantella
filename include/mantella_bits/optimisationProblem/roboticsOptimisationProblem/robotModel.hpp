080ab8b01464a0d8200f205771dcec6c08a1178e
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-18 11:10:29 +0200

    Removed minima/maximal*JointsActuation from robotModel

--- include/mantella_bits/optimisationProblem/roboticsOptimisationProblem/robotModel.hpp
+++ include/mantella_bits/optimisationProblem/roboticsOptimisationProblem/robotModel.hpp
@@ -16,22 +16,6 @@ namespace mant {
         explicit RobotModel(
             const arma::uword numberOfActiveJoints,
             const arma::uword numberOfRedundantJoint);
-
-        void setMinimalActiveJointsActuation(
-            const arma::Row<double> minimalActiveJointsActuation);
-        void setMaximalActiveJointsActuation(
-            const arma::Row<double> maximalActiveJointsActuation);
-
-        arma::Row<double> getMinimalActiveJointsActuation() const;
-        arma::Row<double> getMaximalActiveJointsActuation() const;
-
-        void setMinimalRedundantJointsActuation(
-            const arma::Row<double> minimalRedundantJointsActuation);
-        void setMaximalRedundantJointsActuation(
-            const arma::Row<double> maximalRedundantJointsActuation);
-
-        arma::Row<double> getMinimalRedundantJointsActuation() const;
-        arma::Row<double> getMaximalRedundantJointsActuation() const;
             
         arma::Cube<double> getModel(
             const arma::Col<double>& endEffectorPose,
@@ -48,12 +32,6 @@ namespace mant {
         virtual ~RobotModel() = default;
         
       protected:
-        arma::Row<double> minimalActiveJointsActuation_;
-        arma::Row<double> maximalActiveJointsActuation_;
-        
-        arma::Row<double> minimalRedundantJointsActuation_;
-        arma::Row<double> maximalRedundantJointsActuation_;
-      
         virtual arma::Cube<double> getModelImplementation(
             const arma::Col<double>& endEffectorPose,
             const arma::Row<double>& redundantJointsActuation) const = 0;
