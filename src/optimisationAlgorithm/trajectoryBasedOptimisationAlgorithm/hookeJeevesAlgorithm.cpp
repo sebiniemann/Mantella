c6840625934177743571575f51e0e2a52252ea24
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-18 23:40:31 +0200

    Using verify instead of "if(..) { throw ... }

--- src/optimisationAlgorithm/trajectoryBasedOptimisationAlgorithm/hookeJeevesAlgorithm.cpp
+++ src/optimisationAlgorithm/trajectoryBasedOptimisationAlgorithm/hookeJeevesAlgorithm.cpp
@@ -71,20 +71,16 @@ namespace mant {
 
   void HookeJeevesAlgorithm::setInitialStepSize(
       const arma::Col<double>& initialStepSize) {
-    } else if (arma::any(initialStepSize <= 0)) {
-      throw std::logic_error("The initial step size must be strict greater than 0.");
-    }
     verify(initialStepSize.n_elem == numberOfDimensions_, "The number of dimensions of the initial step size must match the number of dimensions of the optimisation problem.");
+    verify(arma::all(initialStepSize > 0), "The initial step size must be strict greater than 0.");
 
     initialStepSize_ = initialStepSize;
   }
 
   void HookeJeevesAlgorithm::setStepSizeDecrease(
       const arma::Col<double>& stepSizeDecrease) {
-    } else if(arma::any(stepSizeDecrease <= 0) || arma::any(stepSizeDecrease >= 1)) {
-      throw std::logic_error("The step size decrease must be strict greater than 0 and strict lower than 1.");
-    }
     verify(stepSizeDecrease.n_elem == numberOfDimensions_, "The number of dimensions of the step size decrease must match the number of dimensions of the optimisation problem.");
+    verify(arma::all(stepSizeDecrease > 0) && arma::all(stepSizeDecrease < 1), "The step size decrease must be within (0, 1)");
 
     stepSizeDecrease_ = stepSizeDecrease;
   }
