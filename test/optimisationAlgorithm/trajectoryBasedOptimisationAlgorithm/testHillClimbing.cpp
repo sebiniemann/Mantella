23bbcc51aed276cf8a93b8d712cd3762580d8392
dkiechle <kiechle@sra.uni-hannover.de>
2015-08-18 12:00:42 +0200

    Added some dummy tests

--- test/optimisationAlgorithm/trajectoryBasedOptimisationAlgorithm/testHillClimbing.cpp
+++ test/optimisationAlgorithm/trajectoryBasedOptimisationAlgorithm/testHillClimbing.cpp
@@ -8,8 +8,33 @@
 #include <mantella>
 
 TEST_CASE("HillClimbing", "") {
+  SECTION(".setMaximalStepSize") {
+    SECTION("Test default value"){
+      //TODO
+    }
+
+    SECTION("Test with parameter") {
+      //TODO
+    }
+  }
+
   SECTION(".optimise") {
     // TODO
+  }
+
+  SECTION("Exception tests") {
+    std::shared_ptr<mant::OptimisationProblem> optimisationProblem(new mant::bbob::SphereFunction(2));
+    mant::HillClimbing hillClimbing(optimisationProblem);
+
+    SECTION("Throws an exception, if the MaximalStepSize zero") {
+      CHECK_THROWS_AS(hillClimbing.setMaximalStepSize({0, 0}), std::logic_error);
+    }
+
+    SECTION("Throws an exception, if the size of MaximalStepSize is not equal to the number of dimension of the problem") {
+      CHECK_THROWS_AS(hillClimbing.setMaximalStepSize({100, 100, 100}), std::logic_error);
+      CHECK_THROWS_AS(hillClimbing.setMaximalStepSize({100}), std::logic_error);
+    }
+
   }
 
   SECTION("Returns the specified class name.") {
