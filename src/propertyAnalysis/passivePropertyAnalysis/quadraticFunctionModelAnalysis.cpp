026c14bf8d9380d4f6973e5a422b2c22dc8a3dd0
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-22 00:38:52 +0200

    Reworked quadratic function analysis (mostly reordering the internal coefficients)

--- src/propertyAnalysis/passivePropertyAnalysis/quadraticFunctionModelAnalysis.cpp
+++ src/propertyAnalysis/passivePropertyAnalysis/quadraticFunctionModelAnalysis.cpp
@@ -16,26 +16,25 @@ namespace mant {
   void QuadraticFunctionModelAnalysis::analyseImplementation() {
     assert(samples_.size() > 1);
     
-    arma::Mat<double> parameters(numberOfDimensions_ * (numberOfDimensions_ + 3) / 2 + 1, samples_.size());
+    arma::Mat<double> parameters(numberOfDimensions_ * (numberOfDimensions_ + 1) / 2 + numberOfDimensions_ + 1, samples_.size());
     arma::Col<double> objectiveValues(parameters.n_cols);
     arma::uword n = 0;
     for (const auto& sample : samples_) {
       const arma::Col<double>& parameter = sample.first;
 
-      parameters.submat(0, n, numberOfDimensions_ - 1, n) = parameter;
-      parameters.submat(numberOfDimensions_, n, 2 * numberOfDimensions_ - 1,  n) = arma::square(parameter);
-
-      arma::uword k = 2 * numberOfDimensions_;
+      arma::uword k = 0;
       for (arma::uword l = 0; l < numberOfDimensions_; ++l) {
-        for (arma::uword m = l + 1; m < numberOfDimensions_; ++m) {
+        for (arma::uword m = l; m < numberOfDimensions_; ++m) {
           parameters(k++, n) = parameter(l) * parameter(m);
         }
       }
+      
+      parameters.submat(k, n, k + numberOfDimensions_ - 1, n) = parameter;
+      parameters(k + numberOfDimensions_,  n) = 1;
 
       objectiveValues(n) = sample.second;
       ++n;
     }
-    parameters.row(parameters.n_rows - 1).fill(1.0);
     
     arma::Col<double> model;
     try {
@@ -44,21 +43,23 @@ namespace mant {
       model = arma::pinv(parameters * parameters.t()) * parameters * objectiveValues;
     }
     
-    quadraticCoefficients_.diag() = model.subvec(numberOfDimensions_, 2 * numberOfDimensions_ - 1);
-    
-    n = 2 * numberOfDimensions_;
+    n = 0;
     for (arma::uword k = 0; k < numberOfDimensions_; ++k) {
-      for (arma::uword l = k + 1; l < numberOfDimensions_; ++l) {
-        quadraticCoefficients_(k, l) = model(n++) / 2.0;
+      for (arma::uword l = k; l < numberOfDimensions_; ++l) {
+        if (k != l) {
+          quadraticCoefficients_(k, l) = model(n++) / 2.0;
+        } else {
+          quadraticCoefficients_(k, l) = model(n++);
+        }
       }
     }
     quadraticCoefficients_ = arma::symmatu(quadraticCoefficients_);
     
-    linearCoefficients_ = model.head(numberOfDimensions_);
-    errorTerm_ = model(model.n_elem - 1);
+    linearCoefficients_ = model.subvec(n, n + numberOfDimensions_ - 1);
+    errorTerm_ = model(n + numberOfDimensions_);
     
-    for (arma::uword n = 0; n < samples_.size(); ++n) {
-      residuals_(n) = objectiveValues(n) - arma::dot(parameters.col(n), model);
+    for (arma::uword k = 0; k < samples_.size(); ++k) {
+      residuals_(k) = objectiveValues(k) - arma::dot(parameters.col(k), model);
     }
   }
       
