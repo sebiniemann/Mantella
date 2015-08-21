01d0ab5e0acdad938d6aba8d4172044c39a7d41d
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-21 13:36:48 +0200

    Added QuadraticProportionalityAnalysis dummy

---
+++ src/propertyAnalysis/activePropertyAnalysis/quadraticProportionalityAnalysis.cpp
@@ -0,0 +1,11 @@
+#include <mantella_bits/propertyAnalysis/activePropertyAnalysis/quadraticProportionalityAnalysis.hpp>
+
+namespace mant {
+  void QuadraticProportionalityAnalysis::analyseImplementation() {
+  
+  }
+
+  std::string QuadraticProportionalityAnalysis::toString() const {
+    return "quadratic_proportionality_analysis";
+  }
+}
