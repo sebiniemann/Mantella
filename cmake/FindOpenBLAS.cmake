4cd2e0477866e66822e63a9a1cce80b8a56bfad1
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-21 17:22:17 +0200

    Added find_package(OpenBLAS) support

---
+++ cmake/FindOpenBLAS.cmake
@@ -0,0 +1,6 @@
+find_library(OPENBLAS_LIBRARIES openblas)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OpenBLAS DEFAULT_MSG OPENBLAS_LIBRARIES)
+
+mark_as_advanced(OPENBLAS_LIBRARIES)
