b640ce0a3b76f13d9dda526ad1e3d269f029a6de
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-21 14:37:56 +0200

    Added quadratic function model

--- include/mantella_bits/surrogateModel/quadraticFunctionModel.hpp
+++ include/mantella_bits/surrogateModel/quadraticFunctionModel.hpp
@@ -9,12 +9,27 @@
 namespace mant {
   class QuadraticFunctionModel : public SurrogateModel {
     public:
-      using SurrogateModel::SurrogateModel;
+      const arma::Mat<double> quadraticCoefficients_;
+      const arma::Col<double> linearCoefficients_;
+      const double errorTerm_;
     
+      explicit QuadraticFunctionModel(
+          const arma::Mat<double>& quadraticCoefficients,
+          const arma::Col<double>& linearCoefficients,
+          const double errorTerm);
+  
       std::string toString() const override;
   
     protected:
+      const arma::Mat<double> hessian_;
+    
       void modelImplementation() override;
+      
+      arma::Col<double> getJacobian(
+          const arma::Col<double>& parameter) const override;
+  
+      arma::Mat<double> getHessian(
+          const arma::Col<double>& parameter) const override;
       
       double getObjectiveValueImplementation(
           const arma::Col<double>& parameter) const override;
--- src/surrogateModel/quadraticFunctionModel.cpp
+++ src/surrogateModel/quadraticFunctionModel.cpp
@@ -1,13 +1,35 @@
 #include <mantella_bits/surrogateModel/quadraticFunctionModel.hpp>
 
 namespace mant {
+  QuadraticFunctionModel::QuadraticFunctionModel(
+      const arma::Mat<double>& quadraticCoefficients,
+      const arma::Col<double>& linearCoefficients,
+      const double errorTerm) 
+    : SurrogateModel(true, true),
+      quadraticCoefficients_(quadraticCoefficients),
+      linearCoefficients_(linearCoefficients),
+      errorTerm_(errorTerm),
+      hessian_(quadraticCoefficients_ + quadraticCoefficients_.t()) {
+    
+  }
+
   void QuadraticFunctionModel::modelImplementation() {
-  
+    // Nothing to do here
+  }
+      
+  arma::Col<double> QuadraticFunctionModel::getJacobian(
+      const arma::Col<double>& parameter) const {
+    return arma::dot(hessian_, parameter);
+  }
+
+  arma::Mat<double> QuadraticFunctionModel::getHessian(
+      const arma::Col<double>& parameter) const {
+    return hessian_;
   }
   
   double QuadraticFunctionModel::getObjectiveValueImplementation(
       const arma::Col<double>& parameter) const {
-    return 0.0;
+    return arma::dot(parameter.t() * quadraticCoefficients_, parameter) + arma::dot(parameter, linearCoefficients_) + errorTerm_;
   }
   
   std::string QuadraticFunctionModel::toString() const {
