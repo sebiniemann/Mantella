9aeafcc7cae42e3ad0606b9033cfa0c81791915a
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-27 16:38:45 +0200

    Fixed [-Wshorten-64-to-32] implicit conversion loses integer precision: 'unsigned long long' to 'int'

--- src/helper/setTheory.cpp
+++ src/helper/setTheory.cpp
@@ -38,24 +38,25 @@ namespace mant {
 
       partitions.push_back({firstSet, secondSet});
 
-      // TODO Why int?
       arma::Col<arma::uword> counter(n, arma::fill::zeros);
-      int index = counter.n_elem - 1;
+      arma::uword shiftedIndex = counter.n_elem;
 
-      int lastIndex = -1;
+      arma::uword lastIndex = 0;
       if(firstSet.n_elem == secondSet.size()) {
-        lastIndex = 0;
+        lastIndex = 1;
       }
 
-      while (index > lastIndex) {
         std::iter_swap(firstSet.begin() + index, secondSet.begin() + counter(index));
+      while (shiftedIndex > lastIndex) {
+        const arma::uword& index = shiftedIndex - 1;
+      
         partitions.push_back({firstSet, secondSet});
 
         if(counter(index) < secondSet.n_elem - 1) {
           counter.tail(counter.n_elem - index).fill(counter(index) + 1);
-          index = counter.n_elem - 1;
+          shiftedIndex = counter.n_elem;
         } else {
-          --index;
+          --shiftedIndex;
         }
       }
     }
