503dfaa0453358b63e36753eb096324a7e82c451
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-27 10:30:07 +0200

    Added polynomial surrogate models

--- include/mantella_bits/surrogateModel/polynomialFunctionModel.hpp
+++ include/mantella_bits/surrogateModel/polynomialFunctionModel.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
+// C++ standard library
+#include <string>
+
+// Armdillo
+#include <armadillo>
+
 // Mantella
+#include <mantella_bits/helper/unorderedContainer.hpp>
 #include <mantella_bits/surrogateModel.hpp>
+#include <mantella_bits/regressionFunction.hpp>
 
 namespace mant {
   class PolynomialFunctionModel : public SurrogateModel {
     public:
-      using SurrogateModel::SurrogateModel;
+      const std::shared_ptr<RegressionFunction> regressionFunction_;
+    
+      explicit PolynomialFunctionModel(
+          const std::unordered_map<arma::Col<double>, double, Hash, IsEqual>& samples,
+          const std::shared_ptr<RegressionFunction> regressionFunction);
+      
+      std::string toString() const;
+      
+    protected:
+      arma::Col<double> coefficients_;
+    
+      void modelImplementation() override;
+      
+      double getObjectiveValueImplementation(
+          const arma::Col<double>& parameter) const override;
   };
 }
