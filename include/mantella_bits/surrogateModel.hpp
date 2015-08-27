a86779d5752f79d1486c0c94c3b2bebf1f2692b5
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-08-27 10:29:46 +0200

    Surrogate models are now proper optimisation problems (including caching etc.)

--- include/mantella_bits/surrogateModel.hpp
+++ include/mantella_bits/surrogateModel.hpp
@@ -7,42 +7,22 @@
 #include <armadillo>
 
 // Mantella
-#include <mantella_bits/helper/printable.hpp>
 #include <mantella_bits/helper/unorderedContainer.hpp>
+#include <mantella_bits/optimisationProblem.hpp>
 
 namespace mant {
-  class SurrogateModel : public Printable {
+  class SurrogateModel : public OptimisationProblem {
     public:
-      const arma::uword numberOfDimensions_;
-    
       explicit SurrogateModel(
           const std::unordered_map<arma::Col<double>, double, Hash, IsEqual>& samples);
       
-      void setLowerBounds(
-        const arma::Col<double>& lowerBounds);
-      void setUpperBounds(
-        const arma::Col<double>& upperBounds);
-
-      arma::Col<double> getLowerBounds() const;
-      arma::Col<double> getUpperBounds() const;
-      
       void model();
       
-      double getObjectiveValue(
-          const arma::Col<double>& parameter) const;
-        
       virtual ~SurrogateModel() = default;
       
     protected:
       std::unordered_map<arma::Col<double>, double, Hash, IsEqual> samples_;
-      
-      arma::Col<double> lowerBounds_;
-      arma::Col<double> upperBounds_;
     
       virtual void modelImplementation() = 0;
-      
-      virtual double getObjectiveValueImplementation(
-          const arma::Col<double>& parameter) const = 0;
-    
   };
 }
--- src/surrogateModel.cpp
+++ src/surrogateModel.cpp
@@ -1,38 +1,14 @@
 #include <mantella_bits/surrogateModel.hpp>
 
-// C++ standard library
-#include <limits>
-
 namespace mant {
   SurrogateModel::SurrogateModel(
       const std::unordered_map<arma::Col<double>, double, Hash, IsEqual>& samples) 
-    : numberOfDimensions_ (samples.cbegin()->first.n_elem),
+    : OptimisationProblem(samples.cbegin()->first.n_elem),
       samples_(samples) {
       
   }
   
-  void SurrogateModel::setLowerBounds(
-    const arma::Col<double>& lowerBounds) {
-    lowerBounds_ = lowerBounds;
-  }
-  void SurrogateModel::setUpperBounds(
-    const arma::Col<double>& upperBounds) {
-    upperBounds_ = upperBounds;
-  }
-
-  arma::Col<double> SurrogateModel::getLowerBounds() const {
-    return lowerBounds_;
-  }
-  arma::Col<double> SurrogateModel::getUpperBounds() const {
-    return upperBounds_;
-  }
-  
   void SurrogateModel::model() {
     modelImplementation();
-  }
-  
-  double SurrogateModel::getObjectiveValue(
-      const arma::Col<double>& parameter) const {
-    return getObjectiveValueImplementation(parameter);
   }
 }
