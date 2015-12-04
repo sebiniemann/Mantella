eece3fd4c78af49bfb087b17d68f9dacb812af54
Wolff Bock von Wuelfingen <wlfbck@gmail.com>
2015-12-04 19:26:19 +0100

    changed test around a bit

--- Matlabtest/cmaesTest.m
+++ Matlabtest/cmaesTest.m
@@ -1,10 +1,13 @@
 global fitnessFunction;
 fitnessFunction = 1;
 iterations = [];
+dimensions = 10;
+startingpoint = 8 * rand(dimensions, 1) - 4;
+stepsize = 2;
 for i=1:24
 	disp(fitnessFunction);
 	for j=1:5
-		[xmin,fmin,counteval,stopflag,out,bestever] = cmaes('bbob',2*rand(10,1)-1,2);
+		[xmin,fmin,counteval,stopflag,out,bestever] = cmaes('bbob',startingpoint,stepsize);
 		disp(stopflag);
 		iterations(i,j) = counteval;
 	end;
