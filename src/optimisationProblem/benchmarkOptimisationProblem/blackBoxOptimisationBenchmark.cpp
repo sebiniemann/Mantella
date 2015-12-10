c49fff9f53431a5d54a0b7ec23e45ebc76d70f32
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-12-10 09:47:03 +0100

    Readded lowerBounds and upperBounds

--- src/optimisationProblem/benchmarkOptimisationProblem/blackBoxOptimisationBenchmark.cpp
+++ src/optimisationProblem/benchmarkOptimisationProblem/blackBoxOptimisationBenchmark.cpp
@@ -18,7 +18,8 @@ namespace mant {
     BlackBoxOptimisationBenchmark::BlackBoxOptimisationBenchmark(
         const arma::uword numberOfDimensions)
         : BenchmarkOptimisationProblem(numberOfDimensions) {
-      setBounds(arma::zeros<arma::Col<double>>(numberOfDimensions_) - 5.0, arma::zeros<arma::Col<double>>(numberOfDimensions_) + 5.0);
+      setLowerBounds(arma::zeros<arma::Col<double>>(numberOfDimensions_) - 5.0);
+      setUpperBounds(arma::zeros<arma::Col<double>>(numberOfDimensions_) + 5.0);
       // The objective value translation is randomly chosen from a Cauchy distribution with an approximate 50% chance to be within [-100, 100], rounded up to 2 decimal places.
       // The translation is further bounded to be between -1000 and 1000.
       setObjectiveValueTranslation(getRandomObjectiveValueTranslation());
