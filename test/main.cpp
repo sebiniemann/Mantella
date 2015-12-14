56edf7db8312e3e41b634feda7a5fdf6add85a03
Sebastian Niemann <niemann@sra.uni-hannover.de>
2015-12-14 16:32:39 +0100

    IOSync is not needed in tests

--- test/main.cpp
+++ test/main.cpp
@@ -18,6 +18,8 @@ int main(int argc, char* argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &nodeRank);
 #endif
 
+  std::ios_base::sync_with_stdio(false);
+
   // The last argument is used as location for the test data directory.
   // Reduced also the number of arguments, in order avoid conflicts with catch command line arguments handling.
   rootTestDataDirectory = argv[--argc];
