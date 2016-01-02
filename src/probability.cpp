23a79c8d5e8ffb87698ffac2f648742f0a3ba96d
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-01-02 15:09:37 +0100

    Fixed -Wfloat-equal (Travis Job 935.1)

--- src/probability.cpp
+++ src/probability.cpp
@@ -58,7 +58,7 @@ namespace mant {
     arma::Col<double> displacement = arma::normalise(arma::randn<arma::Col<double>>(parameter.n_elem)) * (maximalDistance - minimalDistance);
     displacement += arma::sign(displacement) * minimalDistance;
 
-    if (maximalDistance != 0 && arma::all(displacement == 0)) {
+    if (maximalDistance > 0 && arma::all(displacement == 0)) {
       displacement(std::uniform_int_distribution<arma::uword>(0, parameter.n_elem - 1)(Rng::getGenerator())) = (std::bernoulli_distribution(0.5)(Rng::getGenerator()) ? 1.0 : -1.0) * maximalDistance;
     }
 
