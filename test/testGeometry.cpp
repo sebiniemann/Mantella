38685a32ebfce5acbf9591069c96579467f867e3
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-01-04 07:09:43 +0100

    Applied changes to the tests and played a little with BDD

--- test/testGeometry.cpp
+++ test/testGeometry.cpp
@@ -55,102 +55,158 @@ TEST_CASE("rotationMatrix3D") {
   }
 }
 
-TEST_CASE("circleCircleIntersections") {
-  SECTION("Finds the intersection between two circles.") {
-    arma::Mat<double>::fixed<2, 2> expected;
-
-    expected = {
-      -0.74283680360129, 3.3309693303323, // First intersection.
-      -2.5522451636118, 1.8231290303235 // Second intersection
-    };
-    IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), expected);
-
-    expected = {
-      -1.7957464402521, -0.7476850918245, // First intersection.
-      -0.18041433590865, 0.97533581947522 // Second intersection
-    };
-    IS_EQUAL(mant::circleCircleIntersections({1.8, -2.5}, 4.0, {-3.0, 2.0}, 3.0), expected);
-
-    expected = {
-      -3.2, 4.1, // First intersection
-      -3.2, 4.1 // Second intersection.
-    };
-    IS_EQUAL(mant::circleCircleIntersections({-2.0, 5.0}, 1.5, {-6.0, 2.0}, 3.5), expected);
-  }
-
-  SECTION("Exception tests:") {
-    SECTION("Throws an exception, if both centres are on the same spot.") {
-      CHECK_THROWS_AS(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-2.0, 3.0}, 1.3), std::logic_error);
+SCENARIO("circleCircleIntersections") {
+  GIVEN("Two circles") {
+    WHEN("Their centers distance is within (*max(firstRadius, secondRadius)*, *firstRadius + secondRadius*)") {
+      THEN("There are two intersections") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Their centers distance is within (*abs(firstRadius - secondRadius), *max(firstRadius, secondRadius)*)") {
+      THEN("There are two intersections") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Their centers distance is (*max(firstRadius, secondRadius)*, *firstRadius + secondRadius*)") {
+      THEN("There are two intersections") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Their centers distance is exactly *firstRadius + secondRadius*") {
+      THEN("There is one intersection") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Their centers distance is exactly *abs(firstRadius - secondRadius)*") {
+      THEN("There is one intersection") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("The first circle is a dot (the radius is 0 and their distance is equal to *firstRadius*) on the second circle (which is not a dot)") {
+      THEN("There is one intersection") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Both circles are dots (both radii are 0) with an identical center") {
+      THEN("There is one intersection") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Their centers distance is greater than *firstRadius + secondRadius*") {    
+      THEN("There is no intersection") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
+    }
+    
+    WHEN("Their centers distance is less than *abs(firstRadius - secondRadius)*") {
+      THEN("There is no intersection") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
     }
 
-    SECTION("Throws an exception, if the circles are to far apart.") {
-      CHECK_THROWS_AS(mant::circleCircleIntersections({-8.0, 3.0}, 1.3, {-2.0, 3.0}, 1.3), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if the first circle is enclosed by second one.") {
-      CHECK_THROWS_AS(mant::circleCircleIntersections({-4.0, 2.8}, 0.3, {-4.0, 3.0}, 1.3), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if the second circle is enclosed by first one.") {
-      CHECK_THROWS_AS(mant::circleCircleIntersections({6.8, 1.4}, 2.0, {6.5, 1.2}, 0.1), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if any radius is 0 or negative.") {
-      CHECK_THROWS_AS(mant::circleCircleIntersections({1.8, -2.5}, 0.0, {1.8, 0.5}, 3.0), std::logic_error);
-      CHECK_THROWS_AS(mant::circleCircleIntersections({1.8, -2.5}, 4.0, {-3.0, 2.0}, -3.0), std::logic_error);
+    WHEN("Both circles are identical (same radius and center)") {    
+      THEN("An exception is thrown, as there are infinite intersections.") {
+        IS_EQUAL(mant::circleCircleIntersections({-2.0, 3.0}, 1.3, {-1.5, 2.4}, 1.2), {{-0.74283680360129, 3.3309693303323}, {-2.5522451636118, 1.8231290303235}
+        });
+      }
     }
   }
 }
 
-TEST_CASE("circleSphereIntersections") {
-  SECTION("Finds the intersection between a circle and a sphere.") {
-    arma::Mat<double>::fixed<3, 2> expected;
-
-    expected = {
-      0.0, -2.095, 1.1962336728, // First intersection
-      0.0, -2.095, -1.1962336728 // Second intersection
-    };
-    IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), expected);
-
-    expected = {
-      -2.5522451636, 1.8231290303, 0.0, // First intersection
-      -0.7428368036, 3.3309693303, 0.0 // Second intersection
-    };
-    IS_EQUAL(mant::circleSphereIntersections({-2.0, 3.0, 0.0}, 1.3, {0.0, 0.0, 1.0}, {-1.5, 2.4, 0.0}, 1.2), expected);
-
-    expected = {
-      -0.1804143359, 0.9753358195, 0.0, // First intersection
-      -1.7957464403, -0.7476850918, 0.0 // Second intersection
-    };
-    IS_EQUAL(mant::circleSphereIntersections({1.8, -2.5, 0.0}, 4.0, {0.0, 0.0, 1.0}, {-3.0, 2.0, 0.0}, 3.0), expected);
-
-    expected = {
-      -3.2, 4.1, 0.0, // First intersection
-      -3.2, 4.1, 0.0 // Second intersection.
-    };
-    IS_EQUAL(mant::circleSphereIntersections({-2.0, 5.0, 0.0}, 1.5, {0.0, 0.0, 1.0}, {-6.0, 2.0, 0.0}, 3.5), expected);
-  }
-
-  SECTION("Exception tests:") {
-    SECTION("Throws an exception, if both centres are on the same spot.") {
-      CHECK_THROWS_AS(mant::circleSphereIntersections({-2.0, 0.0, 0.0}, 1.2, {0.0, 0.0, 1.0}, {-2.0, 0.0, 0.0}, 1.5), std::logic_error);
+SCENARIO("circleSphereIntersections") {
+  GIVEN("A sphere and a circle") {
+    WHEN("The sphere intersects with the circle's plane") {
+      AND_WHEN("The sphere's intersection circle's center distance to the circle's center is within (*max(circleRadius, intersectionCircleRadius)*, *circleRadius + intersectionCircleRadius*)") {    
+        THEN("There are two intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728},
+            {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("The sphere's intersection circle's center distance to the circle's center is within (*abs(circleRadius - intersectionCircleRadius), max(circleRadius, intersectionCircleRadius)*)") {
+        THEN("There are two intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728},
+            {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("The sphere's intersection circle's center distance to the circle's center is exactly *circleRadius + intersectionCircleRadius*") {
+        THEN("There is one intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728},
+            {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("The sphere's intersection circle's center distance to the circle's center is exactly *abs(circleRadius - intersectionCircleRadius)*") {
+        THEN("There is one intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728},
+            {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("The sphere's intersection circle is a dot (the radius is 0 and their distance is equal to *circleRadius*) on the circle (which is not a dot)") {
+        THEN("There is one intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728},
+            {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("Both the sphere's intersection circle and the circle are dots (both radii are 0) with an identical center") {
+        THEN("There is one intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728},
+            {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("The sphere's intersection circle's center distance to the circle's center is greater than *circleRadius + intersectionCircleRadius*") {    
+        THEN("There is no intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728}, {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+      
+      AND_WHEN("The sphere's intersection circle's center distance to the circle's center is less than *abs(circleRadius - intersectionCircleRadius)*") {
+        THEN("There is no intersection") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728}, {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
+  
+      AND_WHEN("The sphere's intersection circle is identical (same radius and center) to the circle") {    
+        THEN("An exception is thrown, as there are infinite intersections.") {
+          IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728}, {0.0, -2.095, -1.1962336728}
+          });
+        }
+      }
     }
-
-    SECTION("Throws an exception, if the circle and sphere are to far apart.") {
-      CHECK_THROWS_AS(mant::circleSphereIntersections({-7.0, 3.0, 0.0}, 1.3, {0.0, 0.0, 1.0}, {-1.5, 2.4, 0.0}, 1.2), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if the circle is enclosed by the sphere.") {
-      CHECK_THROWS_AS(mant::circleSphereIntersections({-2.0, 3.0, 0.0}, 1.3, {0.0, 1.0, 1.0}, {-1.5, 2.4, 0.0}, 5.0), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if the sphere is enclosed by the circle.") {
-      CHECK_THROWS_AS(mant::circleSphereIntersections({-2.0, 3.0, 0.0}, 6.3, {0.0, 1.0, 1.0}, {-1.5, 2.4, 0.0}, 0.2), std::logic_error);
-    }
-
-    SECTION("Throws an exception, if any radius is 0 or negative.") {
-      CHECK_THROWS_AS(mant::circleSphereIntersections({-1.5, 3.4, 0.0}, 1.0, {0.0, 0.0, 1.0}, {-1.5, 2.4, 0.0}, 0.0), std::logic_error);
-      CHECK_THROWS_AS(mant::circleSphereIntersections({1.8, -2.5, 0.0}, -4.0, {0.0, 0.0, 1.0}, {-3.0, 2.0, 0.0}, 3.0), std::logic_error);
+  
+    WHEN("The sphere does not intersect with the circle's plane") {    
+      THEN("There is no intersection") {
+        IS_EQUAL(mant::circleSphereIntersections({0.0, -2.0, 0.0}, 1.2, {1.0, 0.0, 0.0}, {0.0, -3.0, 0.0}, 1.5), {{0.0, -2.095, 1.1962336728}, {0.0, -2.095, -1.1962336728}
+        });
+      }
     }
   }
 }
