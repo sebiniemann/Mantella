4a79b0f44ddb903530330842b3a0fcc6ddd2a7d1
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-03-10 09:22:15 +0100

    refactor: Reduced internal robot parameter visibility

--- include/mantella_bits/optimisationProblem/kinematicallyRedundantMachines/parallelKinematicMachine6PUPS.hpp
+++ include/mantella_bits/optimisationProblem/kinematicallyRedundantMachines/parallelKinematicMachine6PUPS.hpp
@@ -10,13 +10,14 @@ namespace mant {
   namespace krm {
     class ParallelKinematicMachine6PUPS : public KinematicallyRedundantMachines {
      public:
+      explicit ParallelKinematicMachine6PUPS();
+
+     protected:
       const arma::Mat<double>::fixed<3, 6> redundantJointsPosition_;
       const arma::Mat<double>::fixed<3, 6> redundantJointsAngles_;
       const arma::Row<double>::fixed<6> middleJointsMinimalLength_;
       const arma::Row<double>::fixed<6> middleJointsMaximalLength_;
       const arma::Mat<double>::fixed<3, 6> endEffectorJointsRelativePosition_;
-
-      explicit ParallelKinematicMachine6PUPS();
     };
   }
 }
