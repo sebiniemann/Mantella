33c69e933592e05e9653da14d634927641f4908f
Oskar Pusz <pusz@sra.uni-hannover.de>
2016-03-24 14:20:33 +0100

    Added test dummy

---
+++ test/testOrbitalMechanics.cpp
@@ -0,0 +1,24 @@
+// Armadillo
+#include <armadillo>
+
+// Catch
+#include <catch.hpp>
+#include "catchExtension.hpp"
+
+// Mantella
+#include <mantella>
+
+SCENARIO("lambert", "[orbitalMechanics][lambert]") {
+  GIVEN("") {
+
+    WHEN("") {
+      THEN("") {
+      }
+    }
+
+    WHEN("") {
+      THEN("Throw a std::logic_error") {
+      }
+    }
+  }
+}
