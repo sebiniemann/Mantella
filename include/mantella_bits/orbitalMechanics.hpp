57a35b6abb8e2496821aefce6ebfcdeed9939c2e
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-06-30 18:59:55 +0200

    feature: Added Stumpff function (#203)

---
+++ include/mantella_bits/orbitalMechanics.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// Armadillo
+#include <armadillo>
+
+namespace mant {
+  namespace itd {
+    double stumpffFunction(
+        const double parameter,
+        const arma::uword type);
+  }
+}
