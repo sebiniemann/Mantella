96c57a8ebe125d7a93a5e49a939a16540f4a9d40
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-07-25 00:21:17 +0200

    wip

--- include/mantella_bits/randomNumberGenerator.hpp
+++ include/mantella_bits/randomNumberGenerator.hpp
@@ -20,7 +20,9 @@ namespace mant {
     Rng& operator=(Rng&&) = delete;
 
     static std::mt19937_64& getGenerator() {
-      if (threadNumber() < generators_.size()) {
+      static std::array<std::mt19937_64, MAXIMAL_NUMBER_OF_THREADS> generators_;
+      
+      if (threadNumber() >= generators_.size()) {
         throw std::runtime_error("Rng.getGenerator: The thread number exceeded the maximal number of threads to support.");
       }
       
@@ -29,15 +31,16 @@ namespace mant {
 
     static void setSeed(
         const std::random_device::result_type seed) {
-      // Uses a different seed for each generator, deterministically based on the provided one.
-      for (arma::uword n = 0; n < generators_.size(); ++n) {
-        generators_.at(n).seed(seed + n);
+      #pragma omp parallel num_threads(MAXIMAL_NUMBER_OF_THREADS)
+      { 
+        #pragma omp for
+        for (arma::uword n = 0; n < MAXIMAL_NUMBER_OF_THREADS; ++n) {
+          // Uses a different seed for each generator, deterministically based on the provided one.
+          getGenerator().seed(seed + n);
+        }
       }
     }
 
     static std::random_device::result_type setRandomSeed();
-
-   protected:
-    static std::array<std::mt19937_64, MAXIMAL_NUMBER_OF_THREADS> generators_;
   };
 }
