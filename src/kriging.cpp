21231ee9e8d3c6e2a2cf629b55d94cef076570c9
Philipp Schiffmann <philippschiffmann93@gmail.com>
2016-08-29 15:21:07 +0200

    Fix Kriging.train: pass correct dimension parameter to arma::mean and arma::stddev

--- src/kriging.cpp
+++ src/kriging.cpp
@@ -59,8 +59,8 @@ namespace mant {
       ++n;
     }
 
-    meanParameter_ = arma::mean(parameters, 2);
-    standardDeviationParameter_ = arma::stddev(parameters, 2);
+    meanParameter_ = arma::mean(parameters, 1);
+    standardDeviationParameter_ = arma::stddev(parameters, 1);
 
     parameters.each_col() -= meanParameter_;
     parameters.each_col() /= standardDeviationParameter_;
