f24b50d13260c70774e73a97200224b641a5ef36
dkiechle <kiechle@sra.uni-hannover.de>
2016-09-16 11:28:35 +0200

    WIP

--- create_doc.py
+++ create_doc.py
@@ -19,63 +19,63 @@ for path, subdirs, files in os.walk("./include/"):
 				continue
 				
 			# Create folder there are not exists
-			if not os.path.exists(path.replace("./include/mantella_bits/", "./doc/", 1)):
-				os.makedirs(path.replace("./include/mantella_bits/", "./doc/", 1))
+			if not os.path.exists(path.replace("./include/mantella_bits", "./doc", 1)):
+				os.makedirs(path.replace("./include/mantella_bits", "./doc", 1))
 			
 			# Open .rst file for header file and added headline	
-			rstfile = open(path.replace("./include/mantella_bits/", "./doc/", 1) + file.split(".")[0] + ".rst", 'w')
+			rstfile = open(path.replace("./include/mantella_bits", "./doc", 1) + "/" +file.split(".")[0] + ".rst", 'w')
 			rstfile.write(file.split(".")[0] + "\n" + "="*len(file.split(".")[0]) + "\n")
 			
 			# Split comments
-			topic = re.findall(re.compile(ur'(.*?)(?=(?:\.\. code-block::|$))([ ]*\.\. code-block:: ((?:\w|\+)+)(?:.*?(?=:name:):name: (\w+)|).*?(?=\n   [^:])\n(.+?)(?=\n[^ \n]+)|)', re.DOTALL), comments)  
+			topic = re.findall(re.compile(ur'(.*?)(?=(?:[ ]*\.\. code-block::|$))(([ ]*)\.\. code-block:: ([^ ]+)(?:.*?(?=:name:):name: (\w+)|).*?(?=\n[ ]+[^:])(.*?)(?=(?:\n\3[^ ]|$)))', re.DOTALL), comments)  
 			
 			for selection in topic:
 				rstfile.write(selection[0]) #selection[0] = before code block
 				
-				if (selection[1] != ""): #selection[1] = code block 
-					if (selection[2] == "c++"): #selection[2] = codestyle
-					# Create .cpp file in include
-						if (selection[3] != ""): #selection[3] = code identifier
-							cppfile = open("./doc/examples/" + selection[3]+".cpp",'w')
-							cppfile.write(selection[4]) #selection[4] = code
+				if (selection[1] != ""): #selection[1] = code block
+					if "c++" in selection[3]: #selection[3] = codestyle
+						print "jes"
+						# Create .cpp file in include
+						if (selection[4] != ""): #selection[4] = code identifier
+							cppfile = open("./doc/examples/" + selection[4]+".cpp",'w')
+							cppfile.write(selection[5]) #selection[5] = code
 							cppfile.close()
-							
-			
-					# Save code block for compile and execute
-					codefile = open("./doc/code.cpp", 'w')
-					codefile.write(selection[4])
-					codefile.close()
+													
+						# Save code block for compile and execute
+						codefile = open("./doc/code.cpp", 'w')
+						codefile.write(selection[5])
+						codefile.close()
 					
-					# Compile generate code file
-					output = subprocess.Popen(['c++', '-std=c++11', '-march=native', '-O3', './doc/code.cpp', '-larmadillo', '-lmantella', '-o', './doc/executecode'], stderr=subprocess.PIPE)
-					output.wait()
+						# Compile generate code file
+						output = subprocess.Popen(['c++', '-std=c++11', '-march=native', '-O3', './doc/code.cpp', '-larmadillo', '-lmantella', '-o', './doc/executecode'], stderr=subprocess.PIPE)
+						output.wait()
 					
-					if (output.returncode != 0):
-						print "Failure during compilation: " + file +"\nWith exeption: \n\n" + output.stderr.read()
-						rstfile.close()
-						os.system("rm ./doc/code.cpp "  + path.replace("./include/mantella_bits/", "./doc/", 1) + file.split(".")[0] + ".rst")
-						if not os.listdir(path.replace("./include/mantella_bits/", "./doc/", 1)):
-							os.removedirs(path.replace("./include/mantella_bits/", "./doc/", 1))
-						exit()
+						if (output.returncode != 0):
+							print "Failure during compilation: " + file +"\nWith exeption: \n\n" + output.stderr.read()
+							rstfile.close()
+							os.system("rm ./doc/code.cpp "  + path.replace("./include/mantella_bits", "./doc", 1) + "/" + file.split(".")[0] + ".rst")
+							if not os.listdir(path.replace("./include/mantella_bits", "./doc", 1)):
+								os.removedirs(path.replace("./include/mantella_bits", "./doc", 1))
+							exit()
 					
-					# Execute generate code file	
-					output = subprocess.Popen(['./doc/executecode'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-					output.wait()
-					if (output.returncode != 0):
-						print "Failure during execution: " + file +"\nWith exeption: \n\n" + output.stderr.read()
-						rstfile.close()
-						os.system("rm ./doc/code.cpp ./doc/executecode " +  path.replace("./include/mantella_bits/", "./doc/", 1) + file.split(".")[0] + ".rst")
-						if not os.listdir(path.replace("./include/mantella_bits/", "./doc/", 1)):
-							os.removedirs(path.replace("./include/mantella_bits/", "./doc/", 1))
-						exit()
+						# Execute generate code file	
+						output = subprocess.Popen(['./doc/executecode'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+						output.wait()
+						if (output.returncode != 0):
+							print "Failure during execution: " + file +"\nWith exeption: \n\n" + output.stderr.read()
+							rstfile.close()
+							os.system("rm ./doc/code.cpp ./doc/executecode " +  path.replace("./include/mantella_bits", "./doc", 1) + "/" + file.split(".")[0] + ".rst")
+							if not os.listdir(path.replace("./include/mantella_bits", "./doc", 1)):
+								os.removedirs(path.replace("./include/mantella_bits", "./doc", 1))
+							exit()
 					
-					# Remove temp files
-					os.system("rm ./doc/code.cpp ./doc/executecode")
-					
-					# Write result from code file in .rst
-					rstfile.write(selection[1] + "\n.. code-block:: none\n")
-					for line in output.stdout.read().split("\n"):
-						rstfile.write("\n   " + line)
+						# Remove temp files
+						os.system("rm ./doc/code.cpp ./doc/executecode")
+									
+						# Write result from code file in .rst
+						rstfile.write(selection[1] + "\n.. code-block:: none\n")
+						for line in output.stdout.read().split("\n"):
+							rstfile.write("\n   " + line)
 				
 			# Save .rst file		
 			rstfile.close()
