d12a822c6efc1d09e5625902264fe99b34d67483
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-10-20 14:46:58 +0200

    wip

--- include/mantella_bits/optimisation_algorithm/hill_climbing.hpp
+++ include/mantella_bits/optimisation_algorithm/hill_climbing.hpp
@@ -65,7 +65,7 @@ TEST_CASE("hill_climbing", "[hill_climbing]") {
   
   SECTION("Next parameters functions") {
     CHECK(hill_climbing.next_parameters_functions.size() == 1);
-    CHECK(std::get<1>(hill_climbing.next_parameters_functions.at(0)) == "Draws all parameters randomly and uniformly from a hyper-sphere (with hollow center), centred at *best_found_paramter*, with outer radius *maximal_stepsize* and inner radius *minimal_stepsize*.");
+    CHECK(std::get<1>(hill_climbing.next_parameters_functions.at(0)) == "Hill climbing next parameters");
     
     hill_climbing.active_dimensions = {0, 2};
     hill_climbing.minimal_stepsize = 0.2;
