5fbcaa7a82531c897144a1ab607c40fbe8ad0c0c
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-10-27 16:21:08 +0200

    wip

--- benchmark/Mantella/benchmark.cpp
+++ benchmark/Mantella/benchmark.cpp
@@ -5,7 +5,20 @@
 // Mantella
 #include <mantella0>
 
-int main() {  
+template <unsigned N> struct faux_unroll {
+  template <typename F> static void call(F const& f) {
+    f();
+    faux_unroll<N-1>::call(f);
+  }
+};
+
+template <> struct faux_unroll<0u> {
+  template <typename F> static void call(F const&) {}
+};
+
+int main() {
+  // 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40
+  
   constexpr std::size_t number_of_dimensions = 5;
   const std::array<std::unique_ptr<mant::problem<double, number_of_dimensions>>, 5> problems = {
     std::unique_ptr<mant::problem<double, number_of_dimensions>>(new mant::ackley_function<double, number_of_dimensions>),
@@ -25,7 +38,7 @@ int main() {
   
   // TODO Explain the statistic reason behind this magic looking number (effect power, test power, ...)
   constexpr std::size_t number_of_repetitions = 2600;
-  std::array<std::array<std::array<mant::optimise_result<double, number_of_dimensions>, 2 * problems.size()>, number_of_repetitions>, optimisers.size()> benchmark;
+  std::array<std::array<std::array<mant::optimise_result<double, number_of_dimensions>, problems.size()>, number_of_repetitions>, optimisers.size()> benchmark;
   for (std::size_t n = 0; n < benchmark.size(); ++n) {
     std::cout << "n: " << n << " of " << benchmark.size() << std::endl;
     const auto& optimiser = optimisers.at(n);
@@ -34,25 +47,21 @@ int main() {
     for (std::size_t k = 0; k < repetitions.size(); ++k) {
       auto& results = repetitions.at(k);
       
-      // TODO Why 10 parameters, should this be the same number for each algorithm?
-      std::vector<std::array<double, number_of_dimensions>> parameters(number_of_dimensions + 1);
-      for (auto& parameter : parameters) {
+      std::vector<std::array<double, number_of_dimensions>> initial_parameters(number_of_dimensions + 1);
+      for (auto& parameter : initial_parameters) {
         std::generate(
           parameter.begin(), std::next(parameter.begin(), optimiser->active_dimensions.size()),
           std::bind(
             std::uniform_real_distribution<double>(0.0, 1.0),
             std::ref(mant::random_number_generator())));
       }
-          
-      for (std::size_t l = 0; l < results.size() / 2; ++l) {
-        // TODO Why 1e-5? Should this be the same value for each problem?
-        optimiser->acceptable_objective_value = 1e-5;
-        results.at(2 * l) = optimiser->optimisation_function(*problems.at(l), parameters);
-        optimiser->acceptable_objective_value = -std::numeric_limits<double>::infinity();
-        // TODO Why 1000? Should this be the same number for each problem?
-        optimiser->maximal_number_of_evaluations = 1000;
-        results.at(2 * l + 1) = optimiser->optimisation_function(*problems.at(l), parameters);
-      }
+
+      std::transform(
+        problems.cbegin(), problems.cend(),
+        results.begin(),
+        [&optimiser, &initial_parameters](const auto& problem) {
+          return optimiser->optimisation_function(*problem, initial_parameters);
+        });
     }
   }
 
