b86d58d3f7d475a563dda640fa17792bfa273618
dkiechle <kiechle@sra.uni-hannover.de>
2016-11-10 18:07:23 +0100

    doc: Added auto column widths (#344)

--- include/mantella0_bits/optimise_result.hpp
+++ include/mantella0_bits/optimise_result.hpp
@@ -12,7 +12,6 @@ Optimisation result
   Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
 
   .. list-table:: Template parameters
-    :widths: 27 73
 
     * - T
         
@@ -26,7 +25,7 @@ Optimisation result
         Must be within ``[1, std::numeric_limits<std::size_t>::max()]``.
 
   .. list-table:: Member variables
-    :widths: 27 73
+
   
     * - parameter
     
