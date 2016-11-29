e2e4341218e4d4e2d06531224528354ba2df5432
Philipp Schiffmann <philippschiffmann93@gmail.com>
2016-11-29 15:10:24 +0100

    Add unit test for `mant::evaluate`

--- include/mantella0_bits/evaluate.hpp
+++ include/mantella0_bits/evaluate.hpp
@@ -49,12 +49,12 @@ evaluate
 
 template <typename T, std::size_t N>
 T evaluate(
-    const problem& problem,
+    const problem<T, N>& problem,
     const std::array<T, N>& parameter);
 
-template <typename T>
+template <typename T, std::size_t N>
 std::vector<T> evaluate(
-    const problem& problem,
+    const problem<T, N>& problem,
     const std::vector<std::array<T, N>>& parameters);
 
 //
@@ -63,14 +63,14 @@ std::vector<T> evaluate(
 
 template <typename T, std::size_t N>
 T evaluate(
-    const problem& problem,
+    const problem<T, N>& problem,
     const std::array<T, N>& parameter) {
   return problem.objective_function(parameter);
 }
 
-template <typename T>
+template <typename T, std::size_t N>
 std::vector<T> evaluate(
-    const problem& problem,
+    const problem<T, N>& problem,
     const std::vector<std::array<T, N>>& parameters) {
   std::vector<T> objective_values(parameters.size());
   std::transform(
@@ -89,6 +89,17 @@ std::vector<T> evaluate(
 
 #if defined(MANTELLA_BUILD_TESTS)
 TEST_CASE("evaluate", "[evaluate]") {
+  mant::problem<double, 2> problem;
+  problem.objective_function = [](const auto& parameter) {
+    return parameter.at(0) * parameter.at(1);
+  };
 
+  const auto result = evaluate(problem, {{1.0, 2.0}, {3.0, 4.0}, {5.0, 6.0}});
+
+  CHECK(result.at(0) == 2.0);
+  CHECK(result.at(1) == 12.0);
+  CHECK(result.at(2) == 30.0);
+
+  CHECK(evaluate(problem, {3.0, 7.0}) == problem.objective_function({{3.0, 7.0}}));
 }
 #endif
